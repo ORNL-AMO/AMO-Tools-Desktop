950d09376f2ebc30b53cbd8e261b6e4d56d7d9c6
rmroot <mroot@intertech.com>
2017-01-30 13:00:23 -0600

    bootstrap, font-awesome and glyphicons added to application styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,15 @@
-import { Component } from '@angular/core';
-import { ElectronService } from './electron.service';
+import { Component, ViewEncapsulation, ViewContainerRef } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../assets/styles/application.scss', './app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'app works!';
-
-  constructor(private _electronService: ElectronService){
-
-  }
-
-  toggleTools(){
-    this._electronService.toggleDevTools();
+  private viewContainerRef: ViewContainerRef;
+  constructor(viewContainerRef: ViewContainerRef){
+    this.viewContainerRef = viewContainerRef;
   }
 }
