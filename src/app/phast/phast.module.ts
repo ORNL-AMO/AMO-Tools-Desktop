ecec069e3489a976d800f54711ca12a3c2dc3c6d
Mlwhitmer <whitmerlm@gmail.com>
2017-02-15 07:58:41 -0500

    Sankey is added. It's a little broken, a little ugly, and a little big.

--- src/app/phast/phast.module.ts
+++ src/app/phast/phast.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PhastComponent } from './phast.component';
 import { PhastBannerComponent } from './phast-banner/phast-banner.component';
+import { SankeyComponent } from './sankey/sankey.component';
+import { D3Service } from 'd3-ng2-service';
 
 @NgModule({
   declarations: [
     PhastComponent,
-    PhastBannerComponent
+    PhastBannerComponent,
+    SankeyComponent
   ],
   exports: [
   ],
   imports: [
     RouterModule
   ],
-  providers: []
+  providers: [D3Service]
 })
 
 export class PhastModule { }
