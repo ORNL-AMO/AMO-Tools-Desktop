a4142f172d9ac7cbbd0e4b3b01baa7829195cccb
Kyle Beanblossom <kdbeanblossom1@gmail.com>
2017-02-17 15:56:43 -0500

    Updated main.js to allow installer to work correctly.

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ require('electron-reload')(__dirname);
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
+const {autoUpdater} = require('electron');
+const feedURL = '~/updates-folder';
+
 
 require('dotenv').config();
 let win = null;
@@ -13,18 +16,14 @@ app.on('ready', function () {
   // Initialize the window to our specified dimensions
   win = new BrowserWindow({width: 1000, height: 600});
   win.maximize();
+
   // Specify entry point
-  if(process.env.PACKAGE === 'true'){
     win.loadURL(url.format({
       pathname: path.join(__dirname, 'dist/index.html'),
       protocol: 'file',
       slashes: true
     }));
-  }else {
-    win.loadURL(process.env.HOST);
-    // Show dev tools
-    win.webContents.openDevTools();
-  }
+
 
   // Remove window once app is closed
   win.on('closed', function () {
