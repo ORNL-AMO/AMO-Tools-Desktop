e223ec8b56ae659ebd6a2dcb935d7c5662075389
rmroot <mroot@intertech.com>
2017-02-20 14:28:40 -0600

    psat motor step view

--- src/app/psat/motor/motor.component.ts
+++ src/app/psat/motor/motor.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-motor',
   templateUrl: './motor.component.html',
   styleUrls: ['./motor.component.css']
 })
 export class MotorComponent implements OnInit {
+  @Output('continue')
+  continue = new EventEmitter<string>();
 
-  constructor() { }
+  motorForm: any;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.motorForm = this.initForm();
+  }
+
+  initForm(){
+    return this.formBuilder.group({
+      'frequency': [''],
+      'horsePower': [''],
+      'motorRPM': [''],
+      'efficiencyClass': [''],
+      'voltage': [''],
+      'fullLoadAmps': [''],
+      'sizeMargin': ['']
+    })
+  }
+
+  saveContinue(){
+    //TODO: Save Logic
+
+    this.continue.emit('field-data');
+  }
+
+  back(){
+    this.continue.emit('pump-fluid');
   }
 
 }
