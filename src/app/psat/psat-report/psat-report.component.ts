b2d4e6c91bbfae1c5f5d64ec4fac72754ffc1e65
rmroot <mroot@intertech.com>
2017-03-05 14:09:47 -0600

    styling updates for psat

--- src/app/psat/psat-report/psat-report.component.ts
+++ src/app/psat/psat-report/psat-report.component.ts
@@ -15,10 +15,40 @@ export class PsatReportComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.setOutputs();
   }
 
   closeAssessment() {
     this.closeReport.emit(true);
   }
 
+  //TEMP FOR DEMO
+  setOutputs() {
+    this.assessment.psat.outputs = {
+      pump_efficiency: 0,
+      motor_rated_power: 0,
+      motor_shaft_power: 0,
+      pump_shaft_power: 0,
+      motor_efficiency: 0,
+      motor_power_factor: 0,
+      motor_current: 0,
+      motor_power: 0,
+      annual_energy: 0,
+      annual_cost: 0
+    }
+    this.assessment.psat.adjustments.forEach(adjustment => {
+      adjustment.outputs = {
+        pump_efficiency: 0,
+        motor_rated_power: 0,
+        motor_shaft_power: 0,
+        pump_shaft_power: 0,
+        motor_efficiency: 0,
+        motor_power_factor: 0,
+        motor_current: 0,
+        motor_power: 0,
+        annual_energy: 0,
+        annual_cost: 0
+      }
+    })
+  }
 }
--- src/app/shared/models/psat.ts
+++ src/app/shared/models/psat.ts
@@ -1,5 +1,6 @@
 export interface PSAT {
   inputs?: PsatInputs,
+  outputs?: PsatOutputs,
   adjustments?: PSAT[],
   selected?: boolean
   name?: string,
@@ -8,40 +9,43 @@ export interface PSAT {
 
 }
 
-export interface PsatInputs{
+export interface PsatInputs {
   pump_style?: any,
-    pump_specified?: any,
-    pump_rated_speed?: any,
-    drive?: any,
-    kinematic_viscosity?: any,
-    specific_gravity?: any,
-    stages?: any,
-    fixed_speed?: any,
-    line_frequency?: any,
-    motor_rated_power?: any,
-    motor_rated_speed?: any,
-    efficiency_class?: any,
-    efficiency?: any,
-    motor_rated_voltage?: any,
-    load_estimation_method?: any,
-    motor_rated_flc?: any,
-    full_load_amps?: any,
-    margin?: any,
-    operating_fraction?: any,
-    flow_rate?: any,
-    head?: any,
-    motor_field_power?: any,
-    motor_field_current?: any,
-    motor_field_voltage?: any,
-    cost_kw_hour?: any
+  pump_specified?: any,
+  pump_rated_speed?: any,
+  drive?: any,
+  kinematic_viscosity?: any,
+  specific_gravity?: any,
+  stages?: any,
+  fixed_speed?: any,
+  line_frequency?: any,
+  motor_rated_power?: any,
+  motor_rated_speed?: any,
+  efficiency_class?: any,
+  efficiency?: any,
+  motor_rated_voltage?: any,
+  load_estimation_method?: any,
+  motor_rated_flc?: any,
+  full_load_amps?: any,
+  margin?: any,
+  operating_fraction?: any,
+  flow_rate?: any,
+  head?: any,
+  motor_field_power?: any,
+  motor_field_current?: any,
+  motor_field_voltage?: any,
+  cost_kw_hour?: any
 }
 
 export interface PsatOutputs {
-
+  pump_efficiency?: any,
+  motor_rated_power?: any,
+  motor_shaft_power?: any,
+  pump_shaft_power?: any,
+  motor_efficiency?: any,
+  motor_power_factor?: any,
+  motor_current?: any,
+  motor_power?: any,
+  annual_energy?: any,
+  annual_cost?: any
 }
-// export interface Adjustment {
-//   psat: PSAT,
-//   name: string,
-//   savings?: number,
-//   optimizationRating?: number
-// }
