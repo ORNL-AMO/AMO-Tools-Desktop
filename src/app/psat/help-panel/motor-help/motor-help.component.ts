a55640b30d14faf95e75ccad84db4ee6a305abef
rmroot <mroot@intertech.com>
2017-03-06 09:47:28 -0600

    psat help panel components

---
+++ src/app/psat/help-panel/motor-help/motor-help.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-motor-help',
+  templateUrl: './motor-help.component.html',
+  styleUrls: ['./motor-help.component.css']
+})
+export class MotorHelpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/psat/help-panel/pump-fluid-help/pump-fluid-help.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pump-fluid-help',
+  templateUrl: './pump-fluid-help.component.html',
+  styleUrls: ['./pump-fluid-help.component.css']
+})
+export class PumpFluidHelpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
