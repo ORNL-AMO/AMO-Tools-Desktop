5dcac1c9b9115b71b0b482c44765f2ce023f3cf2
rmroot <mroot@intertech.com>
2017-03-09 11:17:43 -0600

    water-cooling setup

---
+++ src/app/phast/losses/water-cooling-losses/water-cooling-losses-form/water-cooling-losses-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-water-cooling-losses-form',
+  templateUrl: './water-cooling-losses-form.component.html',
+  styleUrls: ['./water-cooling-losses-form.component.css']
+})
+export class WaterCoolingLossesFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
