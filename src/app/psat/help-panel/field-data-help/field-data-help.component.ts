81e257f91a01562f07827cf8756c0f41c9bb7a79
rmroot <mroot@intertech.com>
2017-03-09 15:47:32 -0600

    context help help panel idea

--- src/app/psat/help-panel/field-data-help/field-data-help.component.ts
+++ src/app/psat/help-panel/field-data-help/field-data-help.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-field-data-help',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./field-data-help.component.css']
 })
 export class FieldDataHelpComponent implements OnInit {
+  @Input()
+  currentField: string;
 
+  defaultOpen: boolean = false;
+  operatingFractionOpen: boolean = false;
+  costKwHrOpen: boolean = false;
+  flowRateOpen: boolean = false;
+  headOpen: boolean = false;
+  loadEstimatedMethodOpen: boolean = false;
+  motorKWOpen: boolean = false;
+  voltageOpen: boolean = false;
   constructor() { }
 
   ngOnInit() {
+    console.log(this.currentField)
+  }
+
+  toggleOpen(bool: boolean){
+    return !bool;
   }
 
 }
