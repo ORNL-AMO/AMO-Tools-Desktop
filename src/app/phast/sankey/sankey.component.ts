87c340cc289d8037563b67ce883aa8bc49df3047
Mlwhitmer <whitmerlm@gmail.com>
2017-03-10 19:41:12 -0500

    modified:   src/app/phast/sankey/sankey.component.ts

--- src/app/phast/sankey/sankey.component.ts
+++ src/app/phast/sankey/sankey.component.ts
@@ -6,9 +6,10 @@ declare var d3: any;
 
 var svg;
 
-var width, height;
+var width = 1650,
+    height = 1200;
 
-const portionAmount = 30000;
+const baseWidth = 1650;
 
 @Component({
   selector: 'app-sankey',
@@ -33,40 +34,34 @@ export class SankeyComponent implements OnInit{
 
   maxSankey(){
     svg.attr("transform", "scale(2)");
-    d3.select("svg")
-      .attr('width', width*2)
-      .attr('height', height*2);
   }
   minSankey(){
-    svg.attr("transform", " scale(1)");
-    d3.select("svg")
-      .attr('width', width)
-      .attr('height', height);
+    svg.attr("transform", " scale(.8)");
   }
 
   makeSankey(){
 
     //Remove  all Sankeys
-    d3.select('body').selectAll('svg').remove();
+    d3.select('app-sankey-diagram').selectAll('svg').remove();
 
     var nodes = [
 
-      /*0*/{ name: "Input", value: 6000000, proportion: 0, x: 150, y:(height/2), input: true, usefulOutput: false, inter: false, top: false},
-      /*1*/{ name: "inter1", value: 0, proportion: 0, x: 350, y: (height/2), input: false, usefulOutput: false, inter: true, top: true},
-      /*2*/{ name: "Flue_Gas_Losses", value: 1500000, proportion: 0, x: 460, y: (height/2), input: false, usefulOutput: false, inter: false, top: true},
-      /*3*/{ name: "inter2", value: 0, proportion: 0, x: 460, y: (height/2), input: false, usefulOutput: false, inter: true, top: false},
-      /*4*/{ name: "Atmosphere_Losses", value: 500000, proportion: 0, x: 540, y: (height/2), input: false, usefulOutput: false, inter: false, top: false},
-      /*5*/{ name: "inter3", value: 0, proportion: 0, x: 540, y: (height/2), input: false, usefulOutput: false, inter: true, top: true},
-      /*6*/{ name: "Other_Losses", value: 400000, proportion: 0, x: 660, y: (height/2), input: false, usefulOutput: false, inter: false, top: true},
-      /*7*/{ name: "inter4", value: 0, proportion: 0, x: 660, y: (height/2), input: false, usefulOutput: false, inter: true, top: false},
-      /*8*/{ name: "Water_Cooling_Losses", value: 800000, proportion: 0, x: 820, y: (height/2), input: false, usefulOutput: false, inter: false, top: false},
-      /*9*/{ name: "inter5", value: 0, proportion: 0, x: 810, y: (height/2), input: false, usefulOutput: false, inter: true, top: true},
-      /*10*/{ name: "Wall_Losses", value: 400000, proportion: 0, x: 930, y: (height/2), input: false, usefulOutput: false, inter: false, top: true},
-      /*11*/{ name: "inter6", value: 0, proportion: 0, x: 940, y: (height/2), input: false, usefulOutput: false, inter: true, top: false},
-      /*12*/{ name: "Opening_Losses", value: 100000, proportion: 0, x: 1070, y: (height/2), input: false, usefulOutput: false, inter: false, top: true},
-      /*13*/{ name: "inter7", value: 0, proportion: 0, x: 1000, y: (height/2), input: false, usefulOutput: false, inter: true, top: false},
-      /*14*/{ name: "Fixture_Losses", value: 100000, proportion: 0, x: 1120, y: (height/2), input: false, usefulOutput: false, inter: false, top: false},
-      /*15*/{ name: "Useful_Output", value: 0, proportion: 0, x: 1150, y: (height/2), input: false, usefulOutput: true, inter: false, top: true}
+      /*0*/{ name: "Input", value: 300, proportionX: (150/baseWidth), x: 0 ,y:0, input: true, usefulOutput: false, inter: false, top: false},
+      /*1*/{ name: "inter1", value: 0, proportionX: (450/baseWidth), x: 0, y: 0, input: false, usefulOutput: false, inter: true, top: true},
+      /*2*/{ name: "Flue_Gas_Losses", value: 70, proportionX: (620/baseWidth), x: 0, y: 0, input: false, usefulOutput: false, inter: false, top: true},
+      /*3*/{ name: "inter2", value: 0, proportionX: (500/baseWidth), x: 0, y: 0, input: false, usefulOutput: false, inter: true, top: false},
+      /*4*/{ name: "Atmosphere_Losses", value: 40, proportionX: (650/baseWidth), x: 0, y: 0, input: false, usefulOutput: false, inter: false, top: false},
+      /*5*/{ name: "inter3", value: 0, proportionX: (630/baseWidth), x: 0, y: 0, input: false, usefulOutput: false, inter: true, top: true},
+      /*6*/{ name: "Other_Losses", value: 20, proportionX: (770/baseWidth), x: 0, y: 0, input: false, usefulOutput: false, inter: false, top: true},
+      /*7*/{ name: "inter4", value: 0, proportionX: (740/baseWidth), x: 0, y: 0, input: false, usefulOutput: false, inter: true, top: false},
+      /*8*/{ name: "Water_Cooling_Losses", value: 30, proportionX: (890/baseWidth), x: 0, y: 0, input: false, usefulOutput: false, inter: false, top: false},
+      /*9*/{ name: "inter5", value: 0, proportionX: (900/baseWidth), x: 0, y: 0, input: false, usefulOutput: false, inter: true, top: true},
+      /*10*/{ name: "Wall_Losses", value: 40,  proportionX: (1030/baseWidth), x: 0, y: 0, input: false, usefulOutput: false, inter: false, top: true},
+      /*11*/{ name: "inter6", value: 0, proportionX: (1070/baseWidth), x: 0, y: 0, input: false, usefulOutput: false, inter: true, top: false},
+      /*12*/{ name: "Opening_Losses", value: 10, proportionX: (1200/baseWidth), x: 0, y: 0, input: false, usefulOutput: false, inter: false, top: true},
+      /*13*/{ name: "inter7", value: 0, proportionX: (1130/baseWidth), x: 0, y: 0, input: false, usefulOutput: false, inter: true, top: false},
+      /*14*/{ name: "Fixture_Losses", value: 10,  proportionX: (1270/baseWidth), x: 0, y: 0, input: false, usefulOutput: false, inter: false, top: false},
+      /*15*/{ name: "Useful_Output", value: 0,  proportionX: (1350/baseWidth), x: 0, y: 0, input: false, usefulOutput: true, inter: false, top: true}
     ];
     var links = [
       //linking to the first interNode
@@ -95,75 +90,73 @@ export class SankeyComponent implements OnInit{
     ];
 
 
-    svg = d3.select('body').append('svg')
-      .call( calcSankey)
-      .attr('width', width)
-      .attr('height', height)
-      .style("border", "1px solid black")
-      .append("g");
+    svg = d3.select('app-sankey-diagram').append('svg')
+          .call(calcSankey)
+          .attr("viewBox", "0 0 " + width + " " + height)
+          .attr("preserveAspectRatio", "xMidYMid meet")
+          .style("border", "1px solid black")
+          .append("g");
+
+    var tShiftVal;
 
     function calcSankey() {
-      var alterVal = 0, shiftVal = 0, maxWidth;
-      //Change 800
-      height = (portionValue(nodes[0].value, portionAmount)*2) + 800;
-      nodes.forEach(function (d, i) {
+      var alterVal = 0, shiftVal;
 
+      nodes.forEach(function (d, i) {
+        d.y = (height/2 - nodes[0].value/2);
         if (d.inter) {
-          d.y = (height/2 - (portionValue(nodes[0].value, portionAmount)/2));
           //Reset height
           if (i == 1) {
             //First interNode
             d.value = nodes[i - 1].value;
-            d.proportion = portionValue(d.value, portionAmount);
-            shiftVal = (nodes[0].x + nodes[0].proportion) - d.x;
+            d.x = (width*d.proportionX);
+            shiftVal = (nodes[0].x + nodes[0].value) - d.x;
+            width += shiftVal;
+            tShiftVal = shiftVal;
           }
           else {
             //Previous node.val - interNode.value
             d.value = (nodes[i - 2].value - nodes[i - 1].value);
-            d.proportion = portionValue(d.value, portionAmount);
             if (d.top) {
               d.y = d.y + alterVal;
             }
             else {
-              alterVal += (nodes[i - 2].proportion - d.proportion);
+              alterVal += (nodes[i - 2].value - d.value);
               d.y = (d.y + alterVal);
             }
           }
-          d.x += shiftVal;
+          d.x = (width*d.proportionX) + shiftVal;
         }
-        //Triangle for all other nodes then the source
         else {
           if(!d.input) {
-            d.y = (height/2 - (portionValue(nodes[0].value, portionAmount)/2));
             if (d.usefulOutput) {
-              //Set the output node in relation to where the links will end up
               d.y = d.y + alterVal;
               d.value = (nodes[i - 2].value - nodes[i - 1].value);
-              d.proportion = portionValue(d.value, portionAmount);
             }
             else {
-              d.proportion = portionValue(d.value, portionAmount);
               if (d.top) {
-                d.y -= nodes[i - 1].proportion - alterVal;
+                d.y -= nodes[i - 1].value - alterVal;
               }
               else {
-                d.y += (nodes[i - 1].proportion * 2) + alterVal;
+                d.y += (nodes[i - 1].value * 2) + alterVal;
               }
             }
-            d.x += shiftVal;
-            if (d.usefulOutput) {
-              //Total Width of the Sankey plus text and input box
-              maxWidth = d.x + 200;
-            }
+            d.x = (width*d.proportionX) + shiftVal;
           }
           else{
-            d.proportion = portionValue(d.value, portionAmount);
-            d.y = (height/2 - (portionValue(nodes[0].value, portionAmount)/2));
+            d.x = (width*d.proportionX);
           }
         }
       });
-      console.log("height: " + height);
-      width = maxWidth;
+      console.log("Shift: " + tShiftVal);
+      updateDisplay(tShiftVal);
+    }
+
+    function updateDisplay(addition){
+      console.log(d3.select('app-sankey-diagram').selectAll('svg'));
+      console.log("ratio: " + (baseWidth/width));
+      d3.select('app-sankey-diagram').selectAll('svg')
+        .attr("viewBox", "0 0 " + width+400 + " " + height);
     }
 
     function makeLinks(d){
@@ -171,25 +164,25 @@ export class SankeyComponent implements OnInit{
       var points = [];
 
       if(nodes[d.source].input){
-        points.push([nodes[d.source].x, (nodes[d.target].y+( nodes[d.target].proportion/2))]);
-        points.push([nodes[d.target].x, (nodes[d.target].y+(nodes[d.target].proportion/2))]);
+        points.push([nodes[d.source].x, (nodes[d.target].y+( nodes[d.target].value/2))]);
+        points.push([nodes[d.target].x, (nodes[d.target].y+(nodes[d.target].value/2))]);
       }
       //If it links up with an inter or usefulOutput then go strait tot the interNode
       else if(nodes[d.target].inter || nodes[d.target].usefulOutput){
-        points.push([(nodes[d.source].x - 5), (nodes[d.target].y+( nodes[d.target].proportion/2))]);
-        points.push([nodes[d.target].x, (nodes[d.target].y+(nodes[d.target].proportion/2))]);
+        points.push([(nodes[d.source].x - 5), (nodes[d.target].y+( nodes[d.target].value/2))]);
+        points.push([nodes[d.target].x, (nodes[d.target].y+(nodes[d.target].value/2))]);
       }
       else {
         //Curved linkes
         if(nodes[d.target].top) {
-          points.push([(nodes[d.source].x-5 ), (nodes[d.source].y+(nodes[d.target].proportion/2))]);
-          points.push([(nodes[d.source].x + 30), (nodes[d.source].y+(nodes[d.target].proportion/2))]);
-          points.push([(nodes[d.target].x ),(nodes[d.target].y + (nodes[d.target].proportion / 2))]);
+          points.push([(nodes[d.source].x-5 ), (nodes[d.source].y+(nodes[d.target].value/2))]);
+          points.push([(nodes[d.source].x + 30), (nodes[d.source].y+(nodes[d.target].value/2))]);
+          points.push([(nodes[d.target].x ),(nodes[d.target].y + (nodes[d.target].value / 2))]);
         }
         else {
-          points.push([(nodes[d.source].x-5), ((nodes[d.source].y+nodes[d.source].proportion)-(nodes[d.target].proportion/2))]);
-          points.push([(nodes[d.source].x + 30), (((nodes[d.source].y+nodes[d.source].proportion)-(nodes[d.target].proportion/2)))]);
-          points.push([(nodes[d.target].x ),(nodes[d.target].y - (nodes[d.target].proportion / 2))]);
+          points.push([(nodes[d.source].x-5), ((nodes[d.source].y+nodes[d.source].value)-(nodes[d.target].value/2))]);
+          points.push([(nodes[d.source].x + 30), (((nodes[d.source].y+nodes[d.source].value)-(nodes[d.target].value/2)))]);
+          points.push([(nodes[d.target].x ),(nodes[d.target].y - (nodes[d.target].value / 2))]);
         }
       }
 
@@ -203,10 +196,6 @@ export class SankeyComponent implements OnInit{
       else{
         return "";
       }
-    }
-
-    function portionValue(value, proportion){
-      return value/proportion;
     }
 
     var color = d3.scaleLinear()
@@ -224,21 +213,21 @@ export class SankeyComponent implements OnInit{
         .attr("x1", nodes[link_data.source].x)
         .attr("y1", function() {
           if (nodes[link_data.target].inter || nodes[link_data.target].usefulOutput){
-            return (nodes[link_data.target].y + (nodes[link_data.target].proportion/2));
+            return (nodes[link_data.target].y + (nodes[link_data.target].value/2));
           }
           else{
             if(nodes[link_data.target].top) {
               return nodes[link_data.source].y;
             }
             else{
-              return (nodes[link_data.source].y+nodes[link_data.source].proportion);
+              return (nodes[link_data.source].y+nodes[link_data.source].value);
             }
           }
         })
         .attr("x2", nodes[link_data.target].x)
         .attr("y2", function(){
           if (nodes[link_data.target].inter || nodes[link_data.target].usefulOutput) {
-            return (nodes[link_data.target].y + (nodes[link_data.target].proportion/2));
+            return (nodes[link_data.target].y + (nodes[link_data.target].value/2));
           }
           else{
             return nodes[link_data.target].y;
@@ -293,21 +282,21 @@ export class SankeyComponent implements OnInit{
           .attr("x1", nodes[link_data.source].x)
           .attr("y1", function() {
             if (nodes[link_data.target].inter || nodes[link_data.target].usefulOutput){
-              return (nodes[link_data.target].y + (nodes[link_data.target].proportion/2));
+              return (nodes[link_data.target].y + (nodes[link_data.target].value/2));
             }
             else{
               if(nodes[link_data.target].top) {
                 return nodes[link_data.source].y;
               }
               else{
-                return (nodes[link_data.source].y+nodes[link_data.source].proportion);
+                return (nodes[link_data.source].y+nodes[link_data.source].value);
               }
             }
           })
           .attr("x2", nodes[link_data.target].x)
           .attr("y2", function(){
             if (nodes[link_data.target].inter || nodes[link_data.target].usefulOutput) {
-              return (nodes[link_data.target].y + (nodes[link_data.target].proportion/2));
+              return (nodes[link_data.target].y + (nodes[link_data.target].value/2));
             }
             else{
               return nodes[link_data.target].y;
@@ -345,7 +334,7 @@ export class SankeyComponent implements OnInit{
       })
       .style("fill", "none")
       .style("stroke-width", function(d){
-        return nodes[d.target].proportion;
+        return nodes[d.target].value;
       })
       .attr('marker-end', function(d){
         return getEndMarker(d);
@@ -370,7 +359,7 @@ export class SankeyComponent implements OnInit{
           return d.x - 70;
         }
         else if(d.usefulOutput){
-          return d.x + (nodes[15].proportion*.8) + 50;
+          return d.x + (nodes[15].value*.8) + 50;
         }
         else {
           return d.x;
@@ -378,8 +367,7 @@ export class SankeyComponent implements OnInit{
       })
       .attr("dy", function(d){
         if(d.input || d.usefulOutput){
-          console.log("d.y: " + d.y);
-          return d.y + (d.proportion/2);
+          return d.y + (d.value/2);
         }
         else {
           if (d.top) {
@@ -406,7 +394,7 @@ export class SankeyComponent implements OnInit{
               return node_val.x - 120;
             }
             else if (node_val.usefulOutput) {
-              return d.x + (nodes[15].proportion*.8);
+              return d.x + (nodes[15].value*.8);
             }
             else {
               return node_val.x - 50;
@@ -414,7 +402,7 @@ export class SankeyComponent implements OnInit{
           })
           .attr("y", function () {
             if (node_val.input || node_val.usefulOutput) {
-              return (node_val.y + (node_val.proportion / 2)) + 10;
+              return (node_val.y + (node_val.value / 2)) + 10;
             }
             else if (node_val.top) {
               return node_val.y - 80;
@@ -430,14 +418,17 @@ export class SankeyComponent implements OnInit{
           .data(nodes)
           .attr("type", "text")
           .attr("id", node_val.name)
-          .attr("value", node_val.value)
+          .attr("value", function(){
+            var format = d3.format(",");
+            return format(node_val.value);
+          })
           .style("width", "100px")
           .on("change", function(){
             if(isNaN(parseFloat(this.value))){
               nodes[i].value = 0;
             }
             else{
-              nodes[i].value = parseFloat(this.value);
+              nodes[i].value = parseFloat(this.value.replace(new RegExp(",", "g"), ""));
             }
             calcSankey();
             updateColors();
@@ -447,7 +438,7 @@ export class SankeyComponent implements OnInit{
               })
               .style("stroke-width", function(d){
                 //returns a links width equal to the target's value
-                return nodes[d.target].proportion;
+                return nodes[d.target].value;
               })
               .attr("marker-end", function (d) {
                 return getEndMarker(d);
@@ -462,7 +453,7 @@ export class SankeyComponent implements OnInit{
                   return d.x - 70;
                 }
                 else if(d.usefulOutput){
-                  return d.x + (nodes[15].proportion*.8) + 50;
+                  return d.x + (nodes[15].value*.8) + 50;
                 }
                 else {
                   return d.x;
@@ -470,7 +461,7 @@ export class SankeyComponent implements OnInit{
               })
               .attr("dy", function(d){
                 if(d.input || d.usefulOutput){
-                  return d.y + (d.proportion/2);
+                  return d.y + (d.value/2);
                 }
                 else {
                   if (d.top) {
@@ -491,18 +482,19 @@ export class SankeyComponent implements OnInit{
     function changePlaceHolders(){
       svg.selectAll("input")
         .attr("value", function(d,i){
+          var format = d3.format(",");
           if(i == 8){
-            return nodes[15].value;
+            return format(nodes[15].value);
           }
           else {
-            return nodes[i * 2].value;
+            return format(nodes[i * 2].value);
           }
         });
       svg.selectAll("foreignObject")
         .data(nodes)
         .attr("x", function (d, i) {
           if(i == 8){
-            return nodes[15].x + (nodes[15].proportion*.8);
+            return nodes[15].x + (nodes[15].value*.8);
           }
           else if(nodes[i * 2].input){
             return nodes[i * 2].x - 120;
@@ -513,10 +505,10 @@ export class SankeyComponent implements OnInit{
         })
         .attr("y", function (d, i) {
           if (nodes[i].input){
-            return (nodes[i * 2].y + (nodes[i * 2].proportion / 2)) + 10;
+            return (nodes[i * 2].y + (nodes[i * 2].value / 2)) + 10;
           }
           else if(i == 8 ){
-            return (nodes[15].y + (nodes[15].proportion / 2)) + 10;
+            return (nodes[15].y + (nodes[15].value / 2)) + 10;
           }
           else{
             if (nodes[i * 2].top) {
