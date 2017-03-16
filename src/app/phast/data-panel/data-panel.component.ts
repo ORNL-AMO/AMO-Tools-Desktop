6b86d164059fe1326a5b9882a9fa31baf6469f7d
Mlwhitmer <whitmerlm@gmail.com>
2017-03-16 13:22:00 -0400

    modified:   src/app/phast/data-panel/data-panel.component.html 	modified:   src/app/phast/data-panel/data-panel.component.ts 	modified:   src/app/phast/sankey/sankey.component.css 	modified:   src/app/phast/sankey/sankey.component.html 	modified:   src/app/phast/sankey/sankey.component.ts

--- src/app/phast/data-panel/data-panel.component.ts
+++ src/app/phast/data-panel/data-panel.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-data-panel',
   templateUrl: './data-panel.component.html',
   styleUrls: ['./data-panel.component.css']
 })
+
 export class DataPanelComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
