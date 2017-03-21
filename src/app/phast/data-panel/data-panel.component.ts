067c16292747928a47d93d81febc17baf06a8ed7
Mlwhitmer <whitmerlm@gmail.com>
2017-03-21 09:17:25 -0400

    modified:   main.js 	modified:   src/app/phast/data-panel/data-panel.component.html 	modified:   src/app/phast/data-panel/data-panel.component.ts 	new file:   src/app/phast/data-panel/sankey-diagram/sankey-diagram.component.css 	new file:   src/app/phast/data-panel/sankey-diagram/sankey-diagram.component.html 	new file:   src/app/phast/data-panel/sankey-diagram/sankey-diagram.component.spec.ts 	new file:   src/app/phast/data-panel/sankey-diagram/sankey-diagram.component.ts 	modified:   src/app/phast/losses/wall-losses/wall-losses.component.html 	modified:   src/app/phast/sankey/sankey.component.html 	modified:   src/app/phast/sankey/sankey.component.ts 	modified:   src/app/psat/help-panel/field-data-help/field-data-help.component.html 	modified:   src/app/psat/help-panel/motor-help/motor-help.component.html

--- src/app/phast/data-panel/data-panel.component.ts
+++ src/app/phast/data-panel/data-panel.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit} from '@angular/core';
 
+import {SankeyComponent} from '../sankey/sankey.component';
+
 @Component({
   selector: 'app-data-panel',
   templateUrl: './data-panel.component.html',
   styleUrls: ['./data-panel.component.css']
 })
 
+
+
 export class DataPanelComponent implements OnInit {
+
+  private sankey = new SankeyComponent();
 
   constructor() {
   }
 
   ngOnInit() {
+    this.sankey.makeSankey("app-sankey-diagram");
   }
 
 }
---
+++ src/app/phast/data-panel/sankey-diagram/sankey-diagram.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sankey-diagram',
+  templateUrl: './sankey-diagram.component.html',
+  styleUrls: ['./sankey-diagram.component.css']
+})
+export class SankeyDiagramComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
