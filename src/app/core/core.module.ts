6b94a9d71925322d269e870c0ca3ab7b33ad130c
rmroot <mroot@intertech.com>
2017-03-29 14:49:04 -0500

    added converter dependencies

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -14,7 +14,7 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { AssessmentService } from '../assessment/assessment.service';
 import { UpdateModalComponent } from '../update-modal/update-modal.component';
-import { ConvertUnitsService } from '../shared/convert-units/convert-units.service';
+
 @NgModule({
   declarations: [
     CoreComponent,
@@ -34,8 +34,7 @@ import { ConvertUnitsService } from '../shared/convert-units/convert-units.servi
     NgxElectronModule
   ],
   providers: [
-    AssessmentService,
-    ConvertUnitsService
+    AssessmentService
   ]
 })
 
