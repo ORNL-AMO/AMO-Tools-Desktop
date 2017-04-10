176447504af5007a43acd7a34e5c31f042198295
rmroot <mroot@intertech.com>
2017-04-10 14:14:58 -0500

    fixed data binding for select menus

--- src/app/calculator/pumps/nema-energy-efficiency/nema-energy-efficiency.component.ts
+++ src/app/calculator/pumps/nema-energy-efficiency/nema-energy-efficiency.component.ts
@@ -17,13 +17,10 @@ export class NemaEnergyEfficiencyComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private psatService: PsatService) { }
 
   ngOnInit() {
-    debugger
     if (!this.psat) {
       this.nemaForm = this.psatService.initForm();
-      console.log(this.nemaForm);
     } else {
-      this.nemaForm = this.psatService.getFormFromPsat(this.psat);
-      console.log(this.nemaForm);
+      this.nemaForm = this.psatService.getFormFromPsat(this.psat.inputs);
     }
   }
 }
