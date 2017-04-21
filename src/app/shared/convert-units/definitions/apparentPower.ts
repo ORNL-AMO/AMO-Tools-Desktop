df0e0c552871e3a42a9861c5bc3da4979952fdd7
rmroot <mroot@intertech.com>
2017-04-21 15:37:21 -0500

    added definitions

---
+++ src/app/shared/convert-units/definitions/apparentPower.ts
@@ -0,0 +1,46 @@
+export const apparentPower = {
+
+    apparentPower: {
+        VA: {
+            name: {
+                singular: 'Volt-Ampere'
+                , plural: 'Volt-Amperes'
+            }
+            , to_anchor: 1
+        }
+        , mVA: {
+            name: {
+                singular: 'Millivolt-Ampere'
+                , plural: 'Millivolt-Amperes'
+            }
+            , to_anchor: .001
+        }
+        , kVA: {
+            name: {
+                singular: 'Kilovolt-Ampere'
+                , plural: 'Kilovolt-Amperes'
+            }
+            , to_anchor: 1000
+        }
+        , MVA: {
+            name: {
+                singular: 'Megavolt-Ampere'
+                , plural: 'Megavolt-Amperes'
+            }
+            , to_anchor: 1000000
+        }
+        , GVA: {
+            name: {
+                singular: 'Gigavolt-Ampere'
+                , plural: 'Gigavolt-Amperes'
+            }
+            , to_anchor: 1000000000
+        }
+    }
+    , _anchors: {
+        metric: {
+            unit: 'VA'
+            , ratio: 1
+        }
+    }
+};
