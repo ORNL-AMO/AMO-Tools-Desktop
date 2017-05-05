73008a98d8e337c0276aa4c5a1b3320827ed147a
rmroot <mroot@intertech.com>
2017-05-05 12:29:36 -0500

    fix for prod build

--- src/jsRequireScript.js
+++ src/jsRequireScript.js
@@ -1,5 +1,5 @@
-var phastAddon = require('../node_modules/amo-tools-suite/build/Release/phast.node');
-var psatAddon = require('../node_modules/amo-tools-suite/build/Release/psat.node');
+// var phastAddon = require('../node_modules/amo-tools-suite/build/Release/phast.node');
+// var psatAddon = require('../node_modules/amo-tools-suite/build/Release/psat.node');
 //var converter = require('../node_modules/convert-units');
 //var electron = require('electron');
 //var d3 = require("d3");
