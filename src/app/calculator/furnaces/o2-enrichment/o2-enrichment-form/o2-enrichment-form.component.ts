711cd531fc21c2eecc79cda96c171b16424358f1
qpk <kaminskimp@ornl.gov>
2017-07-05 10:43:52 -0400

    Initial commit for furnace calculator

--- src/app/calculator/furnaces/o2-enrichment/o2-enrichment-form/o2-enrichment-form.component.ts
+++ src/app/calculator/furnaces/o2-enrichment/o2-enrichment-form/o2-enrichment-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-o2-enrichment-form',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./o2-enrichment-form.component.css']
 })
 export class O2EnrichmentFormComponent implements OnInit {
-
+  @Input()
+  furnaceForm: any;
+  @Output('calculate')
+  calculate = new EventEmitter<boolean>();
   constructor() { }
 
   ngOnInit() {
   }
-
+  calc() {
+    if (this.furnaceForm.valid) {
+      this.calculate.emit(true);
+    }
+  }
 }
