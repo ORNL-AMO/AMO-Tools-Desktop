673f17a6b7507ceef616395b10b857fdc6b6826f
rmroot <mroot@intertech.com>
2017-07-10 09:05:23 -0500

    calculations working, need layout updates

--- src/app/calculator/furnaces/o2-enrichment/o2-enrichment-form/o2-enrichment-form.component.ts
+++ src/app/calculator/furnaces/o2-enrichment/o2-enrichment-form/o2-enrichment-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-
+import { O2Enrichment, O2EnrichmentOutput } from '../../../../shared/models/phast/o2Enrichment';
 @Component({
   selector: 'app-o2-enrichment-form',
   templateUrl: './o2-enrichment-form.component.html',
@@ -7,16 +7,16 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class O2EnrichmentFormComponent implements OnInit {
   @Input()
-  furnaceForm: any;
+  o2Enrichment: O2Enrichment;
   @Output('calculate')
   calculate = new EventEmitter<boolean>();
+  @Input()
+  o2EnrichmentResults: O2EnrichmentOutput;
   constructor() { }
 
   ngOnInit() {
   }
   calc() {
-    if (this.furnaceForm.valid) {
-      this.calculate.emit(true);
-    }
+    this.calculate.emit(true);
   }
 }
