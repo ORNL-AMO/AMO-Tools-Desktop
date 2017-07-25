d7ff3676e154e5070ed1789a33c781e1cbc0d503
rmroot <mroot@intertech.com>
2017-07-25 10:40:16 -0500

    energy use working

--- src/app/calculator/furnaces/energy-use/energy-use-form/energy-use-form.component.ts
+++ src/app/calculator/furnaces/energy-use/energy-use-form/energy-use-form.component.ts
@@ -14,17 +14,79 @@ export class EnergyUseFormComponent implements OnInit {
   flowCalculationResults: FlowCalculationsOutput;
   @Output('changeField')
   changeField = new EventEmitter<string>();
+
+  sectionOptions: any = [
+    {
+      name: 'Sharp Edge',
+      value: 0
+    }, {
+      name: 'Square Edge',
+      value: 1
+    },
+    {
+      name: 'Venturi',
+      value: 2
+    }
+  ]
+
+  gasTypeOptions: any;
+
+  tmpGasTypeOptions: any = [
+    {
+      name: 'Air',
+      value: 0
+    }, {
+      name: 'Ammonia Dissociated',
+      value: 1
+    }, {
+      name: 'Argon',
+      value: 2
+    }, {
+      name: 'Butane',
+      value: 3
+    }, {
+      name: 'Endothermic Ammonia',
+      value: 4
+    }, {
+      name: 'Exothermic Cracked (Lean)',
+      value: 5
+    }, {
+      name: 'Exothermic Cracked (Rich)',
+      value: 6
+    }, {
+      name: 'Helium',
+      value: 7
+    }, {
+      name: 'Hydrogen',
+      value: 8
+    }, {
+      name: 'Natural Gas',
+      value: 9
+    }, {
+      name: 'Nitrogen',
+      value: 10
+    }, {
+      name: 'Other',
+      value: 11
+    }, {
+      name: 'Oxygen',
+      value: 12
+    }, {
+      name: 'Propane',
+      value: 13
+    }
+  ]
+
   constructor(private suiteDbService: SuiteDbService, private phastService: PhastService) { }
 
   ngOnInit() {
+    this.gasTypeOptions = this.suiteDbService.selectGasFlueGasMaterials();
     this.calculate();
   }
 
   calculate() {
     this.flowCalculationResults = this.phastService.flowCalculations(this.flowCalculations);
-    console.log(this.flowCalculationResults);
   }
-
 
   focusField(str: string) {
     this.changeField.emit(str);
