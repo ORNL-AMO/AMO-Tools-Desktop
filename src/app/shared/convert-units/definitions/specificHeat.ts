fccbc6e46998c9c21f24f6c0923d18688cafc7da
Gia Jacob <mkaminski69@gmail.com>
2017-09-13 12:01:12 -0400

    new units initial commit

--- src/app/shared/convert-units/definitions/specificHeat.ts
+++ src/app/shared/convert-units/definitions/specificHeat.ts
@@ -1,36 +1,51 @@
 export const specificHeat = {
   // Specific Heat
   // 1 kJ/(kg- K) = 1 kJ/(kg- C) = 0.239 Btu/(lb- F) = 0.239 kcal/(kg-C)
-  specificHeat: {
-    kJkgC: {
+      metric: {
+      kJkgC: {
+        name: {
+          singular: 'Kilojoule per Kilogram Celsius'
+          , plural: 'Kilojoules per Kilogram Celsius',
+          display: '(kJ/kg-C)'
+        }
+        , to_anchor: 1
+      }
+      , kJkgK: {
+        name: {
+          singular: 'Kilojoule per Kilogram Kelvin'
+          , plural: 'Kilojoules per Kilogram Kelvin',
+          display: '(kJ/kg-K)'
+        }
+        , to_anchor: 1
+      }
+      , kcalkgC: {
+        name: {
+          singular: 'Kilocalorie per Kilogram Celsius'
+          , plural: 'Kilocalories per Kilogram Celsius',
+          display: '(kcal/kg-C)'
+        }
+        , to_anchor: 1 / 0.238845897
+      }
+    },
+
+  imperial: {
+    btulblF: {
       name: {
-        singular: 'Kilojoule per Kilogram Celsius'
-        , plural: 'Kilojoules per Kilogram Celsius',
-        display: '(kJ/kg-C)'
+        singular: 'British Thermal Unit per Pound Fahrenheit'
+        , plural: 'British Thermal Units per Pound Fahrenheit',
+        display: '(Btu/lb-F)'
       }
       , to_anchor: 1
-    }
-    , kJkgK: {
-      name: {
-        singular: 'Kilojoule per Kilogram Kelvin'
-        , plural: 'Kilojoules per Kilogram Kelvin',
-        display: '(kJ/kg-K)'
-      }
-      , to_anchor: 1
-    }
-    , kcalkgC: {
-      name: {
-        singular: 'Kilocalorie per Kilogram Celsius'
-        , plural: 'Kilocalories per Kilogram Celsius',
-        display: '(kcal/kg-C)'
-      }
-      , to_anchor: 0.238845897
-    }
-  },
-  _anchors: {
-    metric: {
-      unit: 'kJ/kgC'
-      , ratio: 1
     }
   }
-};
+  , _anchors: {
+    metric: {
+      unit: 'kJ/kgC'
+      , ratio: 0.238845897
+    }
+    , imperial: {
+      unit: 'Btu/lb-F'
+      , ratio: 1 / 0.238845897
+    }
+  }
+}
