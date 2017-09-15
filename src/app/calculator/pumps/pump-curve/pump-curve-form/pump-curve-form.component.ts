d4a54922051ef996c94f6e3ca4681af52bc8a757
rmroot <mroot@intertech.com>
2017-09-15 14:36:03 -0500

    pump curve mostly working

--- src/app/calculator/pumps/pump-curve/pump-curve-form/pump-curve-form.component.ts
+++ src/app/calculator/pumps/pump-curve/pump-curve-form/pump-curve-form.component.ts
@@ -15,7 +15,7 @@ export class PumpCurveFormComponent implements OnInit {
   @Output('emitCalculate')
   emitCalculate = new EventEmitter<boolean>();
 
-  //selectedFormView: string = 'Equation';
+  selectedFormView: string = 'Equation';
 
   options: Array<string> = [
     'Diameter',
@@ -38,7 +38,55 @@ export class PumpCurveFormComponent implements OnInit {
     this.pumpCurveForm.dataRows.push(tmpRow)
   }
 
-  calculate(){
+  calculate() {
     this.emitCalculate.emit(true);
+  }
+
+  setView(){
+    this.pumpCurveForm.selectedFormView = this.selectedFormView;
+  }
+
+  estimateHead() {
+    if (this.pumpCurveForm.selectedFormView == 'Data') {
+      let tmpArr = new Array<any>();
+      this.pumpCurveForm.dataRows.forEach(val => {
+        tmpArr.push([val.flow, val.head]);
+      })
+      let results = regression.polynomial(tmpArr, { order: this.pumpCurveForm.dataOrder, precision: 10 });
+      let newVal = results.predict(this.pumpCurveForm.exploreFlow);
+      this.pumpCurveForm.exploreHead = newVal[1];
+    } else if (this.pumpCurveForm.selectedFormView == 'Equation') {
+      let result = 0;
+      result = this.pumpCurveForm.headConstant + this.pumpCurveForm.headFlow * this.pumpCurveForm.exploreFlow + this.pumpCurveForm.headFlow2 * Math.pow(this.pumpCurveForm.exploreFlow, 2) + this.pumpCurveForm.headFlow3 * Math.pow(this.pumpCurveForm.exploreFlow, 3) + this.pumpCurveForm.headFlow4 * Math.pow(this.pumpCurveForm.exploreFlow, 4) + this.pumpCurveForm.headFlow5 * Math.pow(this.pumpCurveForm.exploreFlow, 5) + this.pumpCurveForm.headFlow6 * Math.pow(this.pumpCurveForm.exploreFlow, 6);
+      this.pumpCurveForm.exploreHead = result;
+    }
+  }
+
+  estimateFlow() {
+    if (this.pumpCurveForm.selectedFormView == 'Data') {
+      let tmpArr = new Array<any>();
+      this.pumpCurveForm.dataRows.forEach(val => {
+        tmpArr.push([val.head, val.flow]);
+      })
+      let results = regression.polynomial(tmpArr, { order: this.pumpCurveForm.dataOrder, precision: 10 });
+      let newVal = results.predict(this.pumpCurveForm.exploreHead);
+      this.pumpCurveForm.exploreFlow = newVal[1];
+    } else {
+      let tmpArr = this.genHeadData();
+      let regResult = regression.polynomial(tmpArr, { order: this.pumpCurveForm.dataOrder, precision: 10 });
+      let newVal = regResult.predict(this.pumpCurveForm.exploreHead);
+      this.pumpCurveForm.exploreFlow = newVal[1];
+    }
+  }
+
+  genHeadData() {
+    let tmpArr = new Array<any>();
+    for (let i = 0; i < this.pumpCurveForm.maxFlow; i = i + 10) {
+      let result = this.pumpCurveForm.headConstant + this.pumpCurveForm.headFlow * i + this.pumpCurveForm.headFlow2 * Math.pow(i, 2) + this.pumpCurveForm.headFlow3 * Math.pow(i, 3) + this.pumpCurveForm.headFlow4 * Math.pow(i, 4) + this.pumpCurveForm.headFlow5 * Math.pow(i, 5) + this.pumpCurveForm.headFlow6 * Math.pow(i, 6);
+      if (result > 0) {
+        tmpArr.push([result, i]);
+      }
+    }
+    return tmpArr;
   }
 }
