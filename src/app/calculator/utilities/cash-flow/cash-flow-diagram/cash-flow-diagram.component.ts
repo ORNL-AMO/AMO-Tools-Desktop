5230dca830e836968d079cc5b00c33a4433f5c6a
Gia Jacob <mkaminski69@gmail.com>
2017-09-28 15:07:30 -0400

    Cash Flow Diagram Calculator

---
+++ src/app/calculator/utilities/cash-flow/cash-flow-diagram/cash-flow-diagram.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cash-flow-diagram',
+  templateUrl: './cash-flow-diagram.component.html',
+  styleUrls: ['./cash-flow-diagram.component.css']
+})
+export class CashFlowDiagramComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/calculator/utilities/cash-flow/cash-flow-form/cash-flow-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cash-flow-form',
+  templateUrl: './cash-flow-form.component.html',
+  styleUrls: ['./cash-flow-form.component.css']
+})
+export class CashFlowFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
