5a36e8bcb3c39b17dcf71c5c75f48e8f15f7bc80
rmroot <mroot@intertech.com>
2017-10-06 12:28:15 -0500

    updates

--- src/app/phast/phast-report/report-graphs/report-graphs.component.ts
+++ src/app/phast/phast-report/report-graphs/report-graphs.component.ts
@@ -20,28 +20,158 @@ export class ReportGraphsComponent implements OnInit {
   @Input()
   assessment: Assessment;
 
-  baseLineResults: PhastResults;
-  modificationResults: Array<PhastResults>;
+  selectedPhast1: PHAST;
+  selectedPhast2: PHAST;
+
+  resultsArray: Array<any>;
   modExists: boolean = false;
   showResultsCats: ShowResultsCategories;
+  pieLabels: any;
+  baselineResults: PhastResults;
+  colors: Array<string> = [
+    '#BA4A00',
+    '#CA6F1E',
+    '#F39C12',
+    '#F1C40F',
+    '#DC7633',
+    '#E74C3C',
+    '#F9E79F',
+    '#909497',
+    '#D2B4DE',
+  ]
   constructor(private phastService: PhastService, private phastResultsService: PhastResultsService) { }
 
   ngOnInit() {
-    this.modificationResults = new Array<PhastResults>();
+    this.resultsArray = new Array<any>();
     this.showResultsCats = this.phastResultsService.getResultCategories(this.settings);
     if (this.phast.losses) {
-      this.baseLineResults = this.phastResultsService.getResults(this.phast, this.settings);
+      this.baselineResults = this.phastResultsService.getResults(this.phast, this.settings);
+      this.resultsArray.push({ name: 'Baseline', data: this.baselineResults })
+      this.selectedPhast1 = this.resultsArray[0];
       if (this.phast.modifications) {
         if (this.phast.modifications.length != 0) {
           this.modExists = true;
           this.phast.modifications.forEach(mod => {
             let tmpResults = this.phastResultsService.getResults(mod.phast, this.settings);
-            this.modificationResults.push(tmpResults);
+            this.resultsArray.push({ name: mod.phast.name, data: tmpResults });
           })
+          this.selectedPhast2 = this.resultsArray[1];
         }
       }
     } else {
-      this.baseLineResults = this.phastResultsService.initResults();
+      this.baselineResults = this.phastResultsService.initResults();
+      this.resultsArray.push({ name: 'Baseline', data: this.baselineResults })
+    }
+    this.pieLabels = new Array();
+    this.getPieLabels(this.baselineResults, this.showResultsCats);
+  }
+
+
+  getPieLabels(phastResults: PhastResults, resultCats: ShowResultsCategories) {
+    let i = 0;
+    if (phastResults.totalWallLoss) {
+      this.pieLabels.push({
+        name: 'Wall Losses',
+        color: this.colors[i]
+      })
+      i++;
+    }
+    if (phastResults.totalAtmosphereLoss) {
+      this.pieLabels.push({
+        name: 'Atmosphere Losses',
+        color: this.colors[i]
+      })
+      i++;
+    }
+    if (phastResults.totalOtherLoss) {
+      this.pieLabels.push({
+        name: 'Other Losses',
+        color: this.colors[i]
+      })
+      i++;
+    }
+    if (phastResults.totalCoolingLoss) {
+      this.pieLabels.push({
+        name: 'Cooling Losses',
+        color: this.colors[i]
+      })
+      i++;
+    }
+    if (phastResults.totalOpeningLoss) {
+      this.pieLabels.push({
+        name: 'Opening Losses',
+        color: this.colors[i]
+      })
+      i++;
+    }
+    if (phastResults.totalFixtureLoss) {
+      this.pieLabels.push({
+        label: 'Fixture Losses',
+        color: this.colors[i]
+      })
+      i++;
+    }
+    if (phastResults.totalLeakageLoss) {
+      this.pieLabels.push({
+        name: 'Leakage Losses',
+        color: this.colors[i]
+      })
+      i++;
+    }
+    if (phastResults.totalExtSurfaceLoss) {
+      this.pieLabels.push({
+        name: 'Extended Surface Losses',
+        color: this.colors[i]
+      })
+      i++;
+    }
+    if (phastResults.totalChargeMaterialLoss) {
+      this.pieLabels.push({
+        name: 'Charge Material Losses',
+        color: this.colors[i]
+      })
+      i++;
+    }
+    if (resultCats.showFlueGas && phastResults.totalFlueGas) {
+      this.pieLabels.push({
+        name: 'Flue Gas Losses',
+        color: this.colors[i]
+      })
+      i++;
+    }
+    if (resultCats.showAuxPower && phastResults.totalAuxPower) {
+      this.pieLabels.push({
+        name: 'Auxiliary Power Losses',
+        color: this.colors[i]
+      })
+      i++;
+    }
+    if (resultCats.showSlag && phastResults.totalSlag) {
+      this.pieLabels.push({
+        name: 'Slag Losses',
+        color: this.colors[i]
+      })
+      i++;
+    }
+    if (resultCats.showExGas && phastResults.totalExhaustGasEAF) {
+      this.pieLabels.push({
+        name: 'Exhaust Gas',
+        color: this.colors[i]
+      })
+      i++;
+    }
+    if (resultCats.showEnInput2 && phastResults.totalExhaustGas) {
+      this.pieLabels.push({
+        name: 'Exhaust Gas',
+        color: this.colors[i]
+      })
+      i++;
+    }
+    if (phastResults.totalSystemLosses && resultCats.showSystemEff) {
+      this.pieLabels.push({
+        name: 'System Losses',
+        color: this.colors[i]
+      })
     }
   }
 }
