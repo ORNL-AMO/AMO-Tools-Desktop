5371275277bf6bd97bd0441e1dc5d6faf4cb1e9a
pshires <pshires1@vols.utk.edu>
2017-10-09 15:11:01 -0400

    hopeful commit, not working.

--- src/app/calculator/utilities/combined-heat-power/combined-heat-power-form/combined-heat-power-form.component.ts
+++ src/app/calculator/utilities/combined-heat-power/combined-heat-power-form/combined-heat-power-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { CombinedHeatPowerCalculations, CombinedHeatPowerCalculationsOutput } from '../CombinedHeatPowerComponent';
 
 @Component({
   selector: 'app-combined-heat-power-form',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./combined-heat-power-form.component.css']
 })
 export class CombinedHeatPowerFormComponent implements OnInit {
+  @Input()
+  combinedHeatPowerCalculations: CombinedHeatPowerCalculations;
+  @Input()
+  combinedHeatPowerCalculationResults: CombinedHeatPowerCalculationsOutput;
+  @Output('changeField')
+  changeField = new EventEmitter<string>();
+  @Output('emitCalculate')
+  emitCalculate = new EventEmitter<boolean>();
 
+  calculationOptions: any = [
+    {
+      name: 'option 1',
+      value: 0,
+    }, {
+      name: 'option 2',
+      value: 1,
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  calculate() {
+    this.emitCalculate.emit(true);
+  }
+
+  focusField(str: string) {
+    this.changeField.emit(str);
+  }
+
+  setCalculationOption() {
+    this.calculate();
   }
 
 }
