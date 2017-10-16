c08b99642cc25496efbc3ba0c968a304b21198a5
rmroot <mroot@intertech.com>
2017-10-16 15:56:23 -0500

    some functionality for selecting mods

--- src/app/phast/phast-report/executive-summary/executive-summary.component.ts
+++ src/app/phast/phast-report/executive-summary/executive-summary.component.ts
@@ -5,6 +5,8 @@ import { Settings } from '../../../shared/models/settings';
 import { Assessment } from '../../../shared/models/assessment';
 import { PhastResultsService } from '../../phast-results.service';
 import { ExecutiveSummaryService } from '../executive-summary.service';
+import * as _ from 'lodash';
+import { ReportRollupService } from '../../../report-rollup/report-rollup.service';
 @Component({
   selector: 'app-executive-summary',
   templateUrl: './executive-summary.component.html',
@@ -15,22 +17,41 @@ export class ExecutiveSummaryComponent implements OnInit {
   settings: Settings;
   @Input()
   phast: PHAST;
-
+  @Input()
+  assessment: Assessment;
+  @Input()
+  inPhast: boolean; 
+  
   baseline: ExecutiveSummary;
 
   modifications: Array<ExecutiveSummary>;
-
-  constructor(private executiveSummaryService: ExecutiveSummaryService) { }
+  phastMods: Array<any>;
+  selectedModificationIndex: number = 0;
+  maxAnnualSavings
+  constructor(private executiveSummaryService: ExecutiveSummaryService, private reportRollupService: ReportRollupService) { }
 
   ngOnInit() {
     this.baseline = this.executiveSummaryService.getSummary(this.phast, false, this.settings, this.phast);
     this.modifications = new Array<ExecutiveSummary>();
     if (this.phast.modifications) {
+      this.phastMods = this.phast.modifications;
       this.phast.modifications.forEach(mod => {
         let tmpSummary = this.executiveSummaryService.getSummary(mod.phast, true, this.settings, this.phast, this.baseline);
         this.modifications.push(tmpSummary);
       })
+      this.initMaxAnnualSavings();
     }
   }
 
+  initMaxAnnualSavings() {
+    let min = _.minBy(this.modifications, 'annualCost');
+    if (min) {
+      this.selectedModificationIndex = _.findIndex(this.modifications, min);
+    }
+  }
+  useModification() {
+    this.reportRollupService.updateSelectedPhasts(this.assessment, this.selectedModificationIndex);
+  }
+
 }
+
