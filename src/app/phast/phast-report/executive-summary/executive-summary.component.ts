bb7403115ed88dd426a3487c6441d149db67b6d2
rmroot <mroot@intertech.com>
2017-10-16 14:26:01 -0500

    summary component for phast init

--- src/app/phast/phast-report/executive-summary/executive-summary.component.ts
+++ src/app/phast/phast-report/executive-summary/executive-summary.component.ts
@@ -4,7 +4,7 @@ import { PHAST, PhastResults, ExecutiveSummary } from '../../../shared/models/ph
 import { Settings } from '../../../shared/models/settings';
 import { Assessment } from '../../../shared/models/assessment';
 import { PhastResultsService } from '../../phast-results.service';
-
+import { ExecutiveSummaryService } from '../executive-summary.service';
 @Component({
   selector: 'app-executive-summary',
   templateUrl: './executive-summary.component.html',
@@ -20,80 +20,17 @@ export class ExecutiveSummaryComponent implements OnInit {
 
   modifications: Array<ExecutiveSummary>;
 
-  constructor(private phastResultsService: PhastResultsService, private phastService: PhastService) { }
+  constructor(private executiveSummaryService: ExecutiveSummaryService) { }
 
   ngOnInit() {
-    this.baseline = this.getSummary(this.phast, false);
+    this.baseline = this.executiveSummaryService.getSummary(this.phast, false, this.settings, this.phast);
     this.modifications = new Array<ExecutiveSummary>();
     if (this.phast.modifications) {
       this.phast.modifications.forEach(mod => {
-        let tmpSummary = this.getSummary(mod.phast, true);
+        let tmpSummary = this.executiveSummaryService.getSummary(mod.phast, true, this.settings, this.phast, this.baseline);
         this.modifications.push(tmpSummary);
       })
     }
-  }
-
-  getSummary(phast: PHAST, isMod: boolean): ExecutiveSummary {
-    let tmpResultsSummary = this.initSummary();
-    let tmpPhastResults = this.phastResultsService.getResults(phast, this.settings);
-    tmpResultsSummary.annualEnergyUsed = this.calcAnnualEnergy(tmpPhastResults);
-    tmpResultsSummary.energyPerMass = this.calcEnergyPer(phast);
-    tmpResultsSummary.annualCost = this.calcAnnualCost(tmpResultsSummary.annualEnergyUsed);
-    if (isMod) {
-      tmpResultsSummary.annualCostSavings = this.baseline.annualCost - tmpResultsSummary.annualCost;
-      tmpResultsSummary.annualEnergySavings = this.baseline.annualEnergyUsed - tmpResultsSummary.annualEnergyUsed;
-      tmpResultsSummary.percentSavings = Number(Math.round(((((tmpResultsSummary.annualCostSavings) * 100) / this.baseline.annualCost) * 100) / 100).toFixed(0));
-      tmpResultsSummary.implementationCosts = phast.implementationCost;
-      if (tmpResultsSummary.annualCostSavings > 0) {
-        tmpResultsSummary.paybackPeriod = (phast.implementationCost / tmpResultsSummary.annualCostSavings) * 12;
-      }
-    }
-    return tmpResultsSummary;
-  }
-
-  calcPercentSavings() {
-
-  }
-
-  calcAnnualEnergy(results: PhastResults): number {
-    //gross heat * operating hours
-    let tmpAnnualEnergy = results.grossHeatInput * this.phast.operatingHours.hoursPerYear;
-    return tmpAnnualEnergy;
-  }
-
-  calcEnergyPer(phast: PHAST): number {
-    //Energy Intensity for Charge Materials
-    let sumFeedRate = this.phastService.sumChargeMaterialFeedRate(phast.losses.chargeMaterials);
-    let calculatedEnergyUsed = this.phastService.sumHeatInput(phast.losses, this.settings);
-    let calculatedEnergyIntensity = (calculatedEnergyUsed / sumFeedRate) || 0;
-    return calculatedEnergyIntensity;
-  }
-
-  calcAnnualCost(annualEnergyUsed: number): number {
-    //gross heat * operating hours * cost
-    let tmpAnnualCost = 0;
-    if (this.settings.energySourceType == 'Fuel') {
-      tmpAnnualCost = annualEnergyUsed * this.phast.operatingCosts.fuelCost;
-    } else if (this.settings.energySourceType == 'Electricity') {
-      tmpAnnualCost = annualEnergyUsed * this.phast.operatingCosts.electricityCost;
-    } else if (this.settings.energySourceType == 'Steam') {
-      tmpAnnualCost = annualEnergyUsed * this.phast.operatingCosts.steamCost;
-    }
-    return tmpAnnualCost;
-  }
-
-  initSummary(): ExecutiveSummary {
-    let tmpSummary: ExecutiveSummary = {
-      percentSavings: 0,
-      annualEnergyUsed: 0,
-      energyPerMass: 0,
-      annualEnergySavings: 0,
-      annualCost: 0,
-      annualCostSavings: 0,
-      implementationCosts: 0,
-      paybackPeriod: 0
-    }
-    return tmpSummary;
   }
 
 }
