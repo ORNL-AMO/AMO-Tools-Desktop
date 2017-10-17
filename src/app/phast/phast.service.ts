c30dda8b4e167505814954b363d7c3c5c3fb38e5
rmroot <mroot@intertech.com>
2017-10-17 10:42:53 -0500

    issue #760

--- src/app/phast/phast.service.ts
+++ src/app/phast/phast.service.ts
@@ -530,6 +530,12 @@ export class PhastService {
     if (losses.wallLosses) {
       grossHeatRequired += this.sumWallLosses(losses.wallLosses, settings);
     }
+    if (losses.energyInputExhaustGasLoss) {
+      grossHeatRequired += this.sumEnergyInputExhaustGas(losses.energyInputExhaustGasLoss, settings);
+    }
+    if (losses.energyInputEAF) {
+      grossHeatRequired += this.sumEnergyInputEAF(losses.energyInputEAF, settings);
+    }
     return grossHeatRequired;
   }
 
@@ -606,7 +612,7 @@ export class PhastService {
       let tmpResult = this.energyInputEAF(loss, settings);
       sum.heatDelivered += tmpResult.heatDelivered;
     })
-    return sum;
+    return sum.heatDelivered;
   }
 
   sumExhaustGasEAF(losses: ExhaustGasEAF[], settings: Settings): number {
@@ -754,15 +760,15 @@ export class PhastService {
       materials.forEach(val => {
         if (val.chargeMaterialType == 'Solid') {
           if (val.solidChargeMaterial.thermicReactionType == 1) {
-            sumAdditionalHeat += ((val.solidChargeMaterial.chargeFeedRate * val.solidChargeMaterial.reactionHeat * val.solidChargeMaterial.chargeReacted)/100);
+            sumAdditionalHeat += ((val.solidChargeMaterial.chargeFeedRate * val.solidChargeMaterial.reactionHeat * val.solidChargeMaterial.chargeReacted) / 100);
           }
         } else if (val.chargeMaterialType == 'Liquid') {
           if (val.liquidChargeMaterial.thermicReactionType == 1) {
-            sumAdditionalHeat += ((val.liquidChargeMaterial.chargeFeedRate * val.liquidChargeMaterial.reactionHeat * val.liquidChargeMaterial.percentReacted)/100);
+            sumAdditionalHeat += ((val.liquidChargeMaterial.chargeFeedRate * val.liquidChargeMaterial.reactionHeat * val.liquidChargeMaterial.percentReacted) / 100);
           }
         } else if (val.chargeMaterialType == 'Gas') {
           if (val.gasChargeMaterial.thermicReactionType == 1) {
-            sumAdditionalHeat += ((val.gasChargeMaterial.feedRate * val.gasChargeMaterial.reactionHeat * val.gasChargeMaterial.percentReacted)/100);
+            sumAdditionalHeat += ((val.gasChargeMaterial.feedRate * val.gasChargeMaterial.reactionHeat * val.gasChargeMaterial.percentReacted) / 100);
           }
         }
       })
@@ -770,8 +776,8 @@ export class PhastService {
     return sumAdditionalHeat;
   }
 
-  sumGrossHeat(losses: Losses){
-    
+  sumGrossHeat(losses: Losses) {
+
   }
 
   // sumAuxiliaryEquipment(phast: PHAST, results: Array<any>) {
