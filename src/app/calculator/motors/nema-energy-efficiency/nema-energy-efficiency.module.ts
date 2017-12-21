9aeaf22b957e7c3e88b468bd28067e5f6ddc1b25
Gia Jacob <mkaminski69@gmail.com>
2017-12-21 11:34:40 -0500

    PHAST Operating Hours User Validation & PSAT Help for Input Fields

--- src/app/calculator/motors/nema-energy-efficiency/nema-energy-efficiency.module.ts
+++ src/app/calculator/motors/nema-energy-efficiency/nema-energy-efficiency.module.ts
@@ -7,6 +7,7 @@ import { SharedModule } from '../../../shared/shared.module';
 import { NemaEnergyEfficiencyGraphComponent } from './nema-energy-efficiency-graph/nema-energy-efficiency-graph.component';
 import { NemaEnergyEfficiencyComponent } from './nema-energy-efficiency.component';
 import { NemaEnergyEfficiencyFormComponent } from './nema-energy-efficiency-form/nema-energy-efficiency-form.component';
+import { NemaEnergyEfficiencyHelpComponent } from './nema-energy-efficiency-help/nema-energy-efficiency-help.component';
 
 @NgModule({
   imports: [
@@ -18,7 +19,8 @@ import { NemaEnergyEfficiencyFormComponent } from './nema-energy-efficiency-form
   declarations: [
     NemaEnergyEfficiencyComponent,
     NemaEnergyEfficiencyFormComponent,
-    NemaEnergyEfficiencyGraphComponent
+    NemaEnergyEfficiencyGraphComponent,
+    NemaEnergyEfficiencyHelpComponent
   ],
   exports: [
     NemaEnergyEfficiencyComponent
