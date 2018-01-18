7973d00d9d39cde2eb55b0fdc2b1272a5c6c6489
Mark Root <mroot@intertech.com>
2018-01-18 13:13:40 -0600

    fix saving

--- src/app/phast/system-basics/system-basics.component.ts
+++ src/app/phast/system-basics/system-basics.component.ts
@@ -75,7 +75,7 @@ export class SystemBasicsComponent implements OnInit {
       this.showSettingsModal();
     } else if (this.settings.energySourceType != this.newSettings.energySourceType ||
       this.settings.furnaceType != this.newSettings.furnaceType ||
-      this.settings.energyResultUnit != this.newSettings.energyResultUnit,
+      this.settings.energyResultUnit != this.newSettings.energyResultUnit ||
       this.settings.customFurnaceName != this.newSettings.customFurnaceName) {
       this.updateData(false);
     }
