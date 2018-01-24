b7f53064e7e911916d560b07c6031cddae6a788c
Mark Root <mroot@intertech.com>
2018-01-24 09:04:25 -0600

    gas-leakage updates

--- src/app/phast/explore-phast-opportunities/explore-phast-opportunities-form/explore-leakage-form/explore-leakage-form.component.ts
+++ src/app/phast/explore-phast-opportunities/explore-phast-opportunities-form/explore-leakage-form/explore-leakage-form.component.ts
@@ -18,9 +18,86 @@ export class ExploreLeakageFormComponent implements OnInit {
   settings: Settings;
   @Input()
   exploreModIndex: number;
+  showOpening: Array<boolean>;
+  showLeakage: boolean = false;
+  openingAreaError1: Array<string>;
+  openingAreaError2: Array<string>;
   constructor() { }
 
   ngOnInit() {
+    this.initData();
+  }
+
+  initData() {
+    this.showOpening = new Array();
+    this.openingAreaError1 = new Array<string>();
+    this.openingAreaError2 = new Array<string>();
+    let index: number = 0;
+    this.phast.losses.leakageLosses.forEach(loss => {
+      let check: boolean = this.initOpening(loss.openingArea, this.phast.modifications[this.exploreModIndex].phast.losses.leakageLosses[index].openingArea);
+      if (!this.showOpening && check) {
+        this.showLeakage = check;
+      }
+      this.showOpening.push(check);
+      this.openingAreaError1.push(null);
+      this.openingAreaError2.push(null);
+      index++;
+    })
+  }
+
+
+  initOpening(area1: number, area2: number) {
+    if (area1 != area2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  toggleLeakage() {
+    if (this.showLeakage == false) {
+      let index: number = 0;
+      this.phast.losses.leakageLosses.forEach(loss => {
+        let baselineArea: number = loss.openingArea;
+        this.phast.modifications[this.exploreModIndex].phast.losses.leakageLosses[index].openingArea = baselineArea;
+        index++;
+      });
+      this.initData();
+    }
+  }
+
+  toggleOpening(index: number, baselineArea: number) {
+    if(this.showOpening[index] == false){
+      this.phast.modifications[this.exploreModIndex].phast.losses.leakageLosses[index].openingArea = baselineArea;      this.calculate();
+    }
+  }
+
+  focusField(str: string) {
+    //this.changeField.emit(str);
+  }
+
+  checkOpening(num: number, openingArea: number, index: number) {
+    if (openingArea < 0) {
+      if (num == 1) {
+        this.openingAreaError1[index] = 'Opening Area must be equal or greater than 0';
+      } else if (num == 2) {
+        this.openingAreaError2[index] = 'Opening Area must be equal or greater than 0';
+      }
+    } else {
+      if (num == 1) {
+        this.openingAreaError1[index] = null;
+      } else if (num == 2) {
+        this.openingAreaError2[index] = null;
+      }
+    }
+  }
+
+  focusOut() {
+
+  }
+
+  calculate(){
+    //this.emitCalculate.emit(true)
   }
 
 }
