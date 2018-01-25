ca89c5099e11dd9df1a5f40be9c570ad0e0104fe
pshires <pshires1@vols.utk.edu>
2018-01-25 13:43:13 -0500

    trouble with explore opportunities

--- src/app/psat/motor/motor.component.ts
+++ src/app/psat/motor/motor.component.ts
@@ -37,12 +37,15 @@ export class MotorComponent implements OnInit {
   efficiencyClasses: Array<string> = [
     'Standard Efficiency',
     'Energy Efficient',
+    'Premium',
     // When the user chooses specified, they need a place to put the efficiency value
     'Specified'
   ];
 
   horsePowers: Array<number> = [5,7.5,10,15,20,25,30,40,50,60,75,100,125,150,200,250,300,350,400,450,500,600,700,800,900,1000,1250,1750,2000,2250,2500,3000,3500,4000,4500,5000,5500,6000,7000,8000,9000,10000,11000,12000,13000,14000,15000,16000,17000,18000,19000,20000,22500,25000,27500,30000,35000,40000,45000,50000];
+  horsePowersPremium: Array<number> = [5,7.5,10,15,20,25,30,40,50,60,75,100,125,150,200,250,300,350,400,450,500];
   kWatts: Array<number> = [3,3.7,4,4.5,5.5,6,7.5,9.2,11,13,15,18.5,22,26,30,37,45,55,75,90,110,132,150,160,185,200,225,250,280,300,315,335,355,400,450,500,560,630,710,800,900,1000,1250,1500,1750,2000,2250,2500,3000,3500,4000,4500,5000,5500,6000,7000,8000,9000,10000,11000,12000,13000,14000,15000,16000,17000,18000,19000,20000,22500,25000,27500,30000,35000,40000];
+  kWattsPremium: Array<number> = [3,3.7,4,4.5,5.5,6,7.5,9.2,11,13,15,18.5,22,26,30,37,45,55,75,90,110,132,150,160,185,200,225,250,280,300,315,335,355];
 
   frequencies: Array<string> = [
     '50 Hz',
@@ -74,6 +77,7 @@ export class MotorComponent implements OnInit {
 
     this.helpPanelService.currentField.next('lineFrequency');
     //init alert meessages
+    this.modifyPowerArrays();
     this.checkEfficiency(true);
     this.checkFLA(true);
     this.checkMotorRpm(true);
@@ -107,6 +111,32 @@ export class MotorComponent implements OnInit {
     }
     else {
       this.isFirstChange = false;
+    }
+  }
+
+  modifyPowerArrays() {
+    if (this.psatForm.controls.efficiencyClass.value === 'Premium') {
+      if (this.settings.powerMeasurement === 'hp') {
+        if (this.psatForm.controls.horsePower.value > 500) {
+          this.psatForm.patchValue({
+            'horsePower': this.horsePowersPremium[this.horsePowersPremium.length - 1]
+          });
+        }
+        this.options = this.horsePowersPremium;
+      } else {
+        if (this.psatForm.controls.horsePower.value > 355) {
+          this.psatForm.patchValue({
+            'horsePower': this.kWattsPremium[this.kWattsPremium.length - 1]
+          });
+        }
+        this.options = this.kWattsPremium;
+      }
+    } else {
+      if (this.settings.powerMeasurement === 'hp') {
+        this.options = this.horsePowers;
+      } else {
+        this.options = this.kWatts;
+      }
     }
   }
 
@@ -290,6 +320,7 @@ export class MotorComponent implements OnInit {
   }
 
   checkRatedPower(bool?: boolean) {
+    this.modifyPowerArrays();
     if (!bool) {
       this.startSavePolling();
     }
@@ -369,6 +400,7 @@ export class MotorComponent implements OnInit {
   }
 
   startSavePolling() {
+    this.modifyPowerArrays();
     this.checkForm(this.psatForm);
     if (this.counter) {
       clearTimeout(this.counter);
