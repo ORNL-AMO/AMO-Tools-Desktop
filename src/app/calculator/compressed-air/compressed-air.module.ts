bf32ed6d32579b2823582f1b1410aa1c842bf590
Mark Root <mroot@intertech.com>
2018-02-06 09:08:40 -0600

    updates

--- src/app/calculator/compressed-air/compressed-air.module.ts
+++ src/app/calculator/compressed-air/compressed-air.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CompressedAirComponent } from './compressed-air.component';
 import { BagMethodComponent } from './bag-method/bag-method.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule
   ],
   declarations: [
     CompressedAirComponent,
