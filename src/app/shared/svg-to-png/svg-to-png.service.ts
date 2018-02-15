860c02a97495932421a8d33e0c732dac0fe8d8b7
Mark Root <mroot@intertech.com>
2018-02-15 15:35:14 -0600

    dllink type

--- src/app/shared/svg-to-png/svg-to-png.service.ts
+++ src/app/shared/svg-to-png/svg-to-png.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, ElementRef } from '@angular/core';
+import { ImportExportService } from '../import-export/import-export.service';
+import { WindowRefService } from '../../indexedDb/window-ref.service';
 @Injectable()
 export class SvgToPngService {
 
-  constructor() { }
+  constructor(private importExportService: ImportExportService, private windowRefService: WindowRefService) { }
 
   //saves an svg chart as a png given a chart 
   // element containing an svg and a file name
@@ -14,9 +16,24 @@ export class SvgToPngService {
         svg = element.nativeElement.children[i];
       }
     }
-    svg.style = "background-color: #fff";
-    exportFunc.saveSvgAsPng(svg, fn.trim());
-    svg.style = "background-color: none";
+    //    debugger
+
+    exportFunc.svgAsPngUri(svg, {}, (data) => {
+      // console.log(data);
+      let doc = this.windowRefService.getDoc();
+      let dlLink = doc.createElement("a");
+      dlLink.setAttribute("type", "image");
+      dlLink.setAttribute("href", data);
+      dlLink.setAttribute("download", "testImg");
+      console.log(dlLink);
+      dlLink.click()
+      //this.importExportService.createFile(data);
+      //this.importExportService.downloadImage(data);
+      // svg.style = "background-color: #fff";
+      // exportFunc.saveSvgAsPng(svg, fn.trim());
+      // svg.style = "background-color: none";
+    })
+
   }
 
 
