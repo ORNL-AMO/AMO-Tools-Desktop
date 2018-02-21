162b77af59b7e3d732421afc7aa92ff9f4a77afc
Dmitry Howard <dhoward@intertech.com>
2018-02-21 11:24:21 -0600

    merging with develop

--- src/app/phast/phast-report/report-graphs/report-graphs-print/report-graphs-print.component.ts
+++ src/app/phast/phast-report/report-graphs/report-graphs-print/report-graphs-print.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PhastService } from '../../../phast.service';
-import { PHAST, PhastResults, ShowResultsCategories } from '../../../../shared/models/phast/phast';
+import { PHAST, Losses, PhastResults, ShowResultsCategories, Modification } from '../../../../shared/models/phast/phast';
 import { Settings } from '../../../../shared/models/settings';
 import { Assessment } from '../../../../shared/models/assessment';
 import { PhastResultsService } from '../../../phast-results.service';
 import { graphColors } from '../graphColors';
 import { PhastReportService } from '../../phast-report.service';
+
+//debug
+import { LossTab } from '../../../tabs';
+import { LossesService } from '../../../losses/losses.service';
+
 
 
 @Component({
@@ -41,14 +46,31 @@ export class ReportGraphsPrintComponent implements OnInit {
     assessmentName: string;
     energyUnit: string;
 
-    constructor() { }
+
+    //debug
+    lossesTabs: Array<LossTab>;
+    _modifications: Array<Modification>;
+
+
+    constructor(private lossesService: LossesService) { }
 
     ngOnInit() {
+        //debug
+        this.lossesTabs = this.lossesService.lossesTabs;
+        this._modifications = new Array<Modification>();
+        
+
+        //real version
         this.assessmentName = this.assessment.name.replace(/\s/g, '');
         this.sankeyPhastOptions = new Array<any>();
         this.sankeyPhastOptions.push({ name: 'Baseline', phast: this.phast });
         this.sankeyBaseline = this.sankeyPhastOptions[0];
         if (this.phast.modifications) {
+
+            //debug
+            this._modifications = (JSON.parse(JSON.stringify(this.phast.modifications)));
+
+            //real version
             if (this.phast.modifications.length > 0) {
                 this.modExists = true;
             }
@@ -57,5 +79,21 @@ export class ReportGraphsPrintComponent implements OnInit {
             });
         }
         this.energyUnit = this.settings.energyResultUnit;
+
+        //debug
+        this.printAllNotes();
+    }
+
+
+    printAllNotes() {
+
+        
+        for (let i = 0; i < this.lossesTabs.length; i++) {
+            let loss = this.lossesTabs[i].tabName;
+            let note = this._modifications[i].notes.chargeNotes;
+            console.log("loss " + loss + " = " + note);
+
+            
+        }
     }
 }
