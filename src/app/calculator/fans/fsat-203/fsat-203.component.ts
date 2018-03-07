9261b2624ab65dc40d47e5e1468b8280aba4cf14
Mark Root <mroot@intertech.com>
2018-03-07 09:29:04 -0600

    installer 3 updates

--- src/app/calculator/fans/fsat-203/fsat-203.component.ts
+++ src/app/calculator/fans/fsat-203/fsat-203.component.ts
@@ -56,6 +56,7 @@ export class Fsat203Component implements OnInit {
 
   calculate() {
     if (this.planeDataDone && this.basicsDone && this.gasDone && this.shaftPowerDone) {
+      console.log(this.inputs);
       this.results = this.fsatService.fan203(this.inputs);
     } else {
       this.results = {
