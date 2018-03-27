5ae26c3ba59637435104197a16665dd4aeb97fc7
Mark Root <mroot@intertech.com>
2018-03-27 13:36:53 -0500

    aux power losses tab and compare

--- src/app/phast/losses/losses-tabs/losses-tabs.module.ts
+++ src/app/phast/losses/losses-tabs/losses-tabs.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LossesTabsComponent } from './losses-tabs.component';
 import { ChargeMaterialTabComponent } from './charge-material-tab/charge-material-tab.component';
 import { AtmosphereTabComponent } from './atmosphere-tab/atmosphere-tab.component';
+import { AuxiliaryPowerTabComponent } from './auxiliary-power-tab/auxiliary-power-tab.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LossesTabsComponent, ChargeMaterialTabComponent, AtmosphereTabComponent],
+  declarations: [LossesTabsComponent, ChargeMaterialTabComponent, AtmosphereTabComponent, AuxiliaryPowerTabComponent],
   exports: [LossesTabsComponent]
 })
 export class LossesTabsModule { }
