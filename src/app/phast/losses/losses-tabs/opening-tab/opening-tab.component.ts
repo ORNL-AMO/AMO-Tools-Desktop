edfd51ba4747d32ec09707c939f86a99e101a8d0
Mark Root <mroot@intertech.com>
2018-03-28 14:46:25 -0500

    opening tab compare

--- src/app/phast/losses/losses-tabs/opening-tab/opening-tab.component.ts
+++ src/app/phast/losses/losses-tabs/opening-tab/opening-tab.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { LossesService } from '../../losses.service';
+import { PHAST } from '../../../../shared/models/phast/phast';
+import { FormGroup } from '@angular/forms';
+import { OpeningLossesService } from '../../opening-losses/opening-losses.service';
+import { OpeningLossesCompareService } from '../../opening-losses/opening-losses-compare.service';
+import { OpeningLoss } from '../../../../shared/models/phast/losses/openingLoss';
+
 
 @Component({
   selector: 'app-opening-tab',
@@ -6,10 +13,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./opening-tab.component.css']
 })
 export class OpeningTabComponent implements OnInit {
+  @Input()
+  phast: PHAST;
 
-  constructor() { }
+  numLosses: number = 0;
+  inputError: boolean;
+  missingData: boolean;
+  isDifferent: boolean;
+  badgeClass: Array<string>;
+  constructor(private lossesService: LossesService, private openingLossesService: OpeningLossesService, private openingLossesCompareService: OpeningLossesCompareService) { }
 
   ngOnInit() {
+    this.setNumLosses();
+    this.lossesService.updateTabs.subscribe(val => {
+      this.setNumLosses();
+      this.missingData = this.checkMissingData();
+      this.isDifferent = this.checkDifferent();
+      this.setBadgeClass();
+    })
+
+    this.openingLossesCompareService.inputError.subscribe(val => {
+      this.inputError = val;
+      this.setBadgeClass();
+    })
   }
 
+  setBadgeClass() {
+    if (this.missingData) {
+      this.badgeClass = ['missing-data'];
+    } else if (this.inputError) {
+      this.badgeClass = ['input-error'];
+    } else if (this.isDifferent) {
+      this.badgeClass = ['loss-different'];
+    } else {
+      this.badgeClass = ['success'];
+    }
+  }
+
+  setNumLosses() {
+    if (this.phast.losses) {
+      if (this.phast.losses.openingLosses) {
+        this.numLosses = this.phast.losses.openingLosses.length;
+      }
+    }
+  }
+  checkMissingData(): boolean {
+    let testVal = false;
+    if (this.openingLossesCompareService.baselineOpeningLosses) {
+      this.openingLossesCompareService.baselineOpeningLosses.forEach(loss => {
+        if (this.checkLossValid(loss) == false) {
+          testVal = true;
+        }
+      })
+    }
+    if (this.openingLossesCompareService.modifiedOpeningLosses) {
+      this.openingLossesCompareService.modifiedOpeningLosses.forEach(loss => {
+        if (this.checkLossValid(loss) == false) {
+          testVal = true;
+        }
+      })
+    }
+    return testVal;
+  }
+
+
+  checkLossValid(loss: OpeningLoss) {
+      let tmpForm: FormGroup = this.openingLossesService.getFormFromLoss(loss);
+      if (tmpForm.status == 'VALID') {
+        return true;
+      } else {
+        return false;
+      }
+  }
+
+  checkDifferent() {
+    if (this.openingLossesCompareService.baselineOpeningLosses && this.openingLossesCompareService.modifiedOpeningLosses) {
+      return this.openingLossesCompareService.compareAllLosses();
+    }
+  }
 }
--- src/app/phast/losses/opening-losses/opening-losses-compare.service.ts
+++ src/app/phast/losses/opening-losses/opening-losses-compare.service.ts
@@ -7,83 +7,67 @@ export class OpeningLossesCompareService {
   baselineOpeningLosses: OpeningLoss[];
   modifiedOpeningLosses: OpeningLoss[];
 
-  //used to hold behavior subjects for each modification
-  differentArray: Array<any>;
+  inputError: BehaviorSubject<boolean>;
+  constructor() {
+    this.inputError = new BehaviorSubject<boolean>(false);
+  }
 
-  constructor() { }
-
-
-  initCompareObjects() {
-    this.differentArray = new Array();
-    if (this.baselineOpeningLosses && this.modifiedOpeningLosses) {
-      if (this.baselineOpeningLosses.length == this.modifiedOpeningLosses.length) {
-        let numLosses = this.baselineOpeningLosses.length;
-        for (let i = 0; i < numLosses; i++) {
-          this.differentArray.push({
-            lossIndex: i,
-            different: this.initDifferentObject()
-          })
-        }
-        this.checkOpeningLosses();
+  compareAllLosses(): boolean {
+    let index = 0;
+    let numLoss = this.baselineOpeningLosses.length;
+    let isDiff: boolean = false;
+    for (index; index < numLoss; index++) {
+      if (this.compareLoss(index) == true) {
+        isDiff = true;
       }
     }
+    return isDiff;
   }
 
-  addObject(num: number) {
-    this.differentArray.push({
-      lossIndex: num,
-      different: this.initDifferentObject()
-    })
+  compareLoss(index: number): boolean {
+    return (
+      this.compareNumberOfOpenings(index) ||
+      this.compareEmissivity(index) ||
+      this.compareThickness(index) ||
+      this.compareAmbientTemperature(index) ||
+      this.compareInsideTemperature(index) ||
+      this.comparePercentTimeOpen(index) ||
+      this.compareViewFactor(index) ||
+      this.compareOpeningType(index) ||
+      this.compareLengthOfOpening(index) ||
+      this.compareHeightOfOpening(index)
+    )
   }
 
-
-  checkOpeningLosses() {
-    if (this.baselineOpeningLosses && this.modifiedOpeningLosses) {
-      if (this.baselineOpeningLosses.length != 0 && this.modifiedOpeningLosses.length != 0) {
-        for (let lossIndex = 0; lossIndex < this.differentArray.length; lossIndex++) {
-          //numberOfOpenings
-          this.differentArray[lossIndex].different.numberOfOpenings.next(this.compare(this.baselineOpeningLosses[lossIndex].numberOfOpenings, this.modifiedOpeningLosses[lossIndex].numberOfOpenings));
-          //emissivity
-          this.differentArray[lossIndex].different.emissivity.next(this.compare(this.baselineOpeningLosses[lossIndex].emissivity, this.modifiedOpeningLosses[lossIndex].emissivity));
-          //thickness
-          this.differentArray[lossIndex].different.thickness.next(this.compare(this.baselineOpeningLosses[lossIndex].thickness, this.modifiedOpeningLosses[lossIndex].thickness));
-          //ambientTemperature
-          this.differentArray[lossIndex].different.ambientTemperature.next(this.compare(this.baselineOpeningLosses[lossIndex].ambientTemperature, this.modifiedOpeningLosses[lossIndex].ambientTemperature));
-          //insideTemperature
-          this.differentArray[lossIndex].different.insideTemperature.next(this.compare(this.baselineOpeningLosses[lossIndex].insideTemperature, this.modifiedOpeningLosses[lossIndex].insideTemperature));
-          //percentTimeOpen
-          this.differentArray[lossIndex].different.percentTimeOpen.next(this.compare(this.baselineOpeningLosses[lossIndex].percentTimeOpen, this.modifiedOpeningLosses[lossIndex].percentTimeOpen));
-          //viewFactor
-          this.differentArray[lossIndex].different.viewFactor.next(this.compare(this.baselineOpeningLosses[lossIndex].viewFactor, this.modifiedOpeningLosses[lossIndex].viewFactor));
-          //openingType
-          this.differentArray[lossIndex].different.openingType.next(this.compare(this.baselineOpeningLosses[lossIndex].openingType, this.modifiedOpeningLosses[lossIndex].openingType));
-          //lengthOfOpening
-          this.differentArray[lossIndex].different.lengthOfOpening.next(this.compare(this.baselineOpeningLosses[lossIndex].lengthOfOpening, this.modifiedOpeningLosses[lossIndex].lengthOfOpening));
-          //heightOfOpening
-          this.differentArray[lossIndex].different.heightOfOpening.next(this.compare(this.baselineOpeningLosses[lossIndex].heightOfOpening, this.modifiedOpeningLosses[lossIndex].heightOfOpening));
-        }
-      } else {
-        this.disableAll();
-      }
-    }
-    else if ((this.baselineOpeningLosses && !this.modifiedOpeningLosses) || (!this.baselineOpeningLosses && this.modifiedOpeningLosses)) {
-      this.disableAll();
-    }
+  compareNumberOfOpenings(index: number): boolean {
+    return this.compare(this.baselineOpeningLosses[index].numberOfOpenings, this.modifiedOpeningLosses[index].numberOfOpenings);
   }
-
-  disableAll() {
-    for (let lossIndex = 0; lossIndex < this.differentArray.length; lossIndex++) {
-      this.differentArray[lossIndex].different.numberOfOpenings.next(false);
-      this.differentArray[lossIndex].different.emissivity.next(false);
-      this.differentArray[lossIndex].different.thickness.next(false);
-      this.differentArray[lossIndex].different.ambientTemperature.next(false);
-      this.differentArray[lossIndex].different.insideTemperature.next(false);
-      this.differentArray[lossIndex].different.percentTimeOpen.next(false);
-      this.differentArray[lossIndex].different.viewFactor.next(false);
-      this.differentArray[lossIndex].different.openingType.next(false);
-      this.differentArray[lossIndex].different.lengthOfOpening.next(false);
-      this.differentArray[lossIndex].different.heightOfOpening.next(false);
-    }
+  compareEmissivity(index: number): boolean {
+    return this.compare(this.baselineOpeningLosses[index].emissivity, this.modifiedOpeningLosses[index].emissivity);
+  }
+  compareThickness(index: number): boolean {
+    return this.compare(this.baselineOpeningLosses[index].thickness, this.modifiedOpeningLosses[index].thickness);
+  }
+  compareAmbientTemperature(index: number): boolean {
+    return this.compare(this.baselineOpeningLosses[index].ambientTemperature, this.modifiedOpeningLosses[index].ambientTemperature);
+  }
+  compareInsideTemperature(index: number): boolean {
+    return this.compare(this.baselineOpeningLosses[index].insideTemperature, this.modifiedOpeningLosses[index].insideTemperature);
+  }
+  comparePercentTimeOpen(index: number): boolean {
+    return this.compare(this.baselineOpeningLosses[index].percentTimeOpen, this.modifiedOpeningLosses[index].percentTimeOpen);
+  }
+  compareViewFactor(index: number): boolean {
+    return this.compare(this.baselineOpeningLosses[index].viewFactor, this.modifiedOpeningLosses[index].viewFactor);
+  }
+  compareOpeningType(index: number): boolean {
+    return this.compare(this.baselineOpeningLosses[index].openingType, this.modifiedOpeningLosses[index].openingType);
+  }
+  compareLengthOfOpening(index: number): boolean {
+    return this.compare(this.baselineOpeningLosses[index].lengthOfOpening, this.modifiedOpeningLosses[index].lengthOfOpening);
+  }
+  compareHeightOfOpening(index: number): boolean {
+    return this.compare(this.baselineOpeningLosses[index].heightOfOpening, this.modifiedOpeningLosses[index].heightOfOpening);
   }
 
   compare(a: any, b: any) {
@@ -101,32 +85,4 @@ export class OpeningLossesCompareService {
     }
   }
 
-  initDifferentObject(): OpeningLossDifferent {
-    let tmpDifferent: OpeningLossDifferent = {
-      numberOfOpenings: new BehaviorSubject<boolean>(null),
-      emissivity: new BehaviorSubject<boolean>(null),
-      thickness: new BehaviorSubject<boolean>(null),
-      ambientTemperature: new BehaviorSubject<boolean>(null),
-      insideTemperature: new BehaviorSubject<boolean>(null),
-      percentTimeOpen: new BehaviorSubject<boolean>(null),
-      viewFactor: new BehaviorSubject<boolean>(null),
-      openingType: new BehaviorSubject<boolean>(null),
-      lengthOfOpening: new BehaviorSubject<boolean>(null),
-      heightOfOpening: new BehaviorSubject<boolean>(null),
-    }
-    return tmpDifferent;
-  }
-}
-
-export interface OpeningLossDifferent {
-  numberOfOpenings: BehaviorSubject<boolean>,
-  emissivity: BehaviorSubject<boolean>,
-  thickness: BehaviorSubject<boolean>,
-  ambientTemperature: BehaviorSubject<boolean>,
-  insideTemperature: BehaviorSubject<boolean>,
-  percentTimeOpen: BehaviorSubject<boolean>,
-  viewFactor: BehaviorSubject<boolean>,
-  openingType: BehaviorSubject<boolean>,
-  lengthOfOpening: BehaviorSubject<boolean>,
-  heightOfOpening: BehaviorSubject<boolean>,
 }
