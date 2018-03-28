d95a72ddec6590b710ba7d313cbe1813a7f4ceac
Mark Root <mroot@intertech.com>
2018-03-28 15:35:46 -0500

    slag updates

--- src/app/phast/losses/losses-tabs/slag-tab/slag-tab.component.ts
+++ src/app/phast/losses/losses-tabs/slag-tab/slag-tab.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { LossesService } from '../../losses.service';
+import { PHAST } from '../../../../shared/models/phast/phast';
+import { FormGroup } from '@angular/forms';
+import { SlagService } from '../../slag/slag.service';
+import { SlagCompareService } from '../../slag/slag-compare.service';
+import { Slag } from '../../../../shared/models/phast/losses/slag';
 
 @Component({
   selector: 'app-slag-tab',
@@ -6,10 +12,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slag-tab.component.css']
 })
 export class SlagTabComponent implements OnInit {
+  @Input()
+  phast: PHAST;
 
-  constructor() { }
+  numLosses: number = 0;
+  inputError: boolean;
+  missingData: boolean;
+  isDifferent: boolean;
+  badgeClass: Array<string>;
+  constructor(private lossesService: LossesService, private slagService: SlagService, private slagCompareService: SlagCompareService) { }
 
   ngOnInit() {
+    this.setNumLosses();
+    this.lossesService.updateTabs.subscribe(val => {
+      this.setNumLosses();
+      this.missingData = this.checkMissingData();
+      this.isDifferent = this.checkDifferent();
+      this.setBadgeClass();
+    })
+
+    this.slagCompareService.inputError.subscribe(val => {
+      this.inputError = val;
+      this.setBadgeClass();
+    })
+  }
+
+  setBadgeClass() {
+    if (this.missingData) {
+      this.badgeClass = ['missing-data'];
+    } else if (this.inputError) {
+      this.badgeClass = ['input-error'];
+    } else if (this.isDifferent) {
+      this.badgeClass = ['loss-different'];
+    } else {
+      this.badgeClass = ['success'];
+    }
+  }
+
+  setNumLosses() {
+    if (this.phast.losses) {
+      if (this.phast.losses.slagLosses) {
+        this.numLosses = this.phast.losses.slagLosses.length;
+      }
+    }
+  }
+  checkMissingData(): boolean {
+    let testVal = false;
+    if (this.slagCompareService.baselineSlag) {
+      this.slagCompareService.baselineSlag.forEach(loss => {
+        if (this.checkLossValid(loss) == false) {
+          testVal = true;
+        }
+      })
+    }
+    if (this.slagCompareService.modifiedSlag) {
+      this.slagCompareService.modifiedSlag.forEach(loss => {
+        if (this.checkLossValid(loss) == false) {
+          testVal = true;
+        }
+      })
+    }
+    return testVal;
+  }
+
+
+  checkLossValid(loss: Slag) {
+      let tmpForm: FormGroup = this.slagService.getFormFromLoss(loss);
+      if (tmpForm.status == 'VALID') {
+        return true;
+      } else {
+        return false;
+      }
+  }
+
+  checkDifferent() {
+    if (this.slagCompareService.baselineSlag && this.slagCompareService.modifiedSlag) {
+      return this.slagCompareService.compareAllLosses();
+    }
   }
 
 }
