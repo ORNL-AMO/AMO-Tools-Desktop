f61f474a635d8980b864b6c16e13203184ee774a
Mark Root <mroot@intertech.com>
2018-03-29 09:47:44 -0500

    wall compare tab

--- src/app/phast/losses/losses-tabs/wall-tab/wall-tab.component.ts
+++ src/app/phast/losses/losses-tabs/wall-tab/wall-tab.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { LossesService } from '../../losses.service';
+import { PHAST } from '../../../../shared/models/phast/phast';
+import { FormGroup } from '@angular/forms';
+import { WallLossesService } from '../../wall-losses/wall-losses.service';
+import { WallLossCompareService } from '../../wall-losses/wall-loss-compare.service';
+import { WallLoss } from '../../../../shared/models/phast/losses/wallLoss';
 
 @Component({
   selector: 'app-wall-tab',
@@ -6,10 +12,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wall-tab.component.css']
 })
 export class WallTabComponent implements OnInit {
+  @Input()
+  phast: PHAST;
 
-  constructor() { }
+  numLosses: number = 0;
+  inputError: boolean;
+  missingData: boolean;
+  isDifferent: boolean;
+  badgeClass: Array<string>;
+  constructor(private lossesService: LossesService, private wallLossesService: WallLossesService, private wallLossCompareService: WallLossCompareService) { }
 
   ngOnInit() {
+    this.setNumLosses();
+    this.lossesService.updateTabs.subscribe(val => {
+      this.setNumLosses();
+      this.missingData = this.checkMissingData();
+      this.isDifferent = this.checkDifferent();
+      this.setBadgeClass();
+    })
+
+    this.wallLossCompareService.inputError.subscribe(val => {
+      this.inputError = val;
+      this.setBadgeClass();
+    })
+  }
+
+  setBadgeClass() {
+    if (this.missingData) {
+      this.badgeClass = ['missing-data'];
+    } else if (this.inputError) {
+      this.badgeClass = ['input-error'];
+    } else if (this.isDifferent) {
+      this.badgeClass = ['loss-different'];
+    } else {
+      this.badgeClass = ['success'];
+    }
+  }
+
+  setNumLosses() {
+    if (this.phast.losses) {
+      if (this.phast.losses.wallLosses) {
+        this.numLosses = this.phast.losses.wallLosses.length;
+      }
+    }
+  }
+  checkMissingData(): boolean {
+    let testVal = false;
+    if (this.wallLossCompareService.baselineWallLosses) {
+      this.wallLossCompareService.baselineWallLosses.forEach(loss => {
+        if (this.checkLossValid(loss) == false) {
+          testVal = true;
+        }
+      })
+    }
+    if (this.wallLossCompareService.modifiedWallLosses) {
+      this.wallLossCompareService.modifiedWallLosses.forEach(loss => {
+        if (this.checkLossValid(loss) == false) {
+          testVal = true;
+        }
+      })
+    }
+    return testVal;
+  }
+
+
+  checkLossValid(loss: WallLoss) {
+    let tmpForm: FormGroup = this.wallLossesService.getWallLossForm(loss);
+    if (tmpForm.status == 'VALID') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  checkDifferent() {
+    if (this.wallLossCompareService.baselineWallLosses && this.wallLossCompareService.modifiedWallLosses) {
+      return this.wallLossCompareService.compareAllLosses();
+    }
   }
 
 }
