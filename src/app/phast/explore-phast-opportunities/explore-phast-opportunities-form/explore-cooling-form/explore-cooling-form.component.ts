82cbdf32931223c2db1687d75c08b8bc5b0ba979
Mark Root <mroot@intertech.com>
2018-04-27 14:40:47 -0500

    init cooling

--- src/app/phast/explore-phast-opportunities/explore-phast-opportunities-form/explore-cooling-form/explore-cooling-form.component.ts
+++ src/app/phast/explore-phast-opportunities/explore-phast-opportunities-form/explore-cooling-form/explore-cooling-form.component.ts
@@ -19,24 +19,88 @@ export class ExploreCoolingFormComponent implements OnInit {
   @Input()
   exploreModIndex: number;
 
-  baselineLosses: Array<LiquidCoolingLoss | GasCoolingLoss>;
-  modifiedLosses: Array<LiquidCoolingLoss | GasCoolingLoss>;
+  baselineLosses: Array<{ loss: LiquidCoolingLoss | GasCoolingLoss, type: string }>;
+  modifiedLosses: Array<{ loss: LiquidCoolingLoss | GasCoolingLoss, type: string }>;
+  showCooling: boolean = false;
+  showFlowRate: Array<boolean>;
+  liquidFlowError1: Array<string>;
+  liquidFlowError2: Array<string>;
+  gasFlowError1: Array<string>;
+  gasFlowError2: Array<string>;
+
   constructor() { }
 
   ngOnInit() {
+    this.baselineLosses = new Array();
+    this.modifiedLosses = new Array();
+    this.showFlowRate = new Array<boolean>();
+    this.liquidFlowError1 = new Array<string>();
+    this.liquidFlowError2 = new Array<string>();
+    this.gasFlowError1 = new Array<string>();
+    this.gasFlowError2 = new Array<string>();
+
     let index = 0;
     this.phast.losses.coolingLosses.forEach(loss => {
-      if(loss.coolingLossType == 'Liquid'){
-        this.baselineLosses.push(loss.liquidCoolingLoss);
-      }else if(loss.coolingLossType == 'Gas'){
-        this.baselineLosses.push(loss.gasCoolingLoss);
+      this.showFlowRate.push(false);
+      this.liquidFlowError1.push(null);
+      this.liquidFlowError2.push(null);
+      this.gasFlowError1.push(null);
+      this.gasFlowError2.push(null);
+      if (loss.coolingLossType == 'Liquid') {
+        console.log(loss.liquidCoolingLoss)
+        this.baselineLosses.push({ loss: loss.liquidCoolingLoss, type: 'Liquid' });
+      } else if (loss.coolingLossType == 'Gas') {
+        this.baselineLosses.push({ loss: loss.gasCoolingLoss, type: 'Gas' });
       }
-      if(this.phast.modifications[this.exploreModIndex].phast.losses.coolingLosses[index].coolingLossType == 'Liquid'){
-        this.modifiedLosses.push(loss.liquidCoolingLoss)
-      }else if(this.phast.modifications[this.exploreModIndex].phast.losses.coolingLosses[index].coolingLossType == 'Gas'){
-        this.modifiedLosses.push(loss.gasCoolingLoss);
+      if (this.phast.modifications[this.exploreModIndex].phast.losses.coolingLosses[index].coolingLossType == 'Liquid') {
+        this.modifiedLosses.push({ loss: this.phast.modifications[this.exploreModIndex].phast.losses.coolingLosses[index].liquidCoolingLoss, type: 'Liquid' })
+      } else if (this.phast.modifications[this.exploreModIndex].phast.losses.coolingLosses[index].coolingLossType == 'Gas') {
+        this.modifiedLosses.push({ loss: this.phast.modifications[this.exploreModIndex].phast.losses.coolingLosses[index].gasCoolingLoss, type: 'Gas' });
       }
     })
   }
 
+
+  toggleCooling() {
+
+  }
+
+  toggleFeedRate() {
+
+  }
+
+
+  checkLiquidFlow() {
+
+  }
+
+  checkGasFlow() {
+
+  }
+
+
+
+
+
+
+
+
+  focusField(str: string) {
+    this.changeField.emit(str);
+    //   this.changeTab.emit( {
+    //     tabName: 'Charge Material',
+    //     step: 1,
+    //     next: 2,
+    //     componentStr: 'charge-material',
+    //     showAdd: true  
+    // });
+  }
+
+  focusOut() {
+
+  }
+
+  calculate() {
+    this.emitCalculate.emit(true)
+  }
 }
