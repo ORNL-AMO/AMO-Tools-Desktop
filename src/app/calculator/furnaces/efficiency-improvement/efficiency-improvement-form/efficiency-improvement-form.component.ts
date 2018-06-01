1bdefad10508fe8c441743e553e970bf2cbdecc4
Dmitry Howard <dhoward@intertech.com>
2018-06-01 13:15:34 -0500

    Added phast warnings for combustion air temperature

--- src/app/calculator/furnaces/efficiency-improvement/efficiency-improvement-form/efficiency-improvement-form.component.ts
+++ src/app/calculator/furnaces/efficiency-improvement/efficiency-improvement-form/efficiency-improvement-form.component.ts
@@ -18,6 +18,11 @@ export class EfficiencyImprovementFormComponent implements OnInit {
   @Input()
   settings: Settings;
 
+  error = {
+    currentCombAirTemp: null,
+    newCombAirTemp: null
+  };
+
   constructor() { }
 
   ngOnInit() {
@@ -25,7 +30,23 @@ export class EfficiencyImprovementFormComponent implements OnInit {
 
 
   calc() {
-    this.calculate.emit(true);
+    this.error.currentCombAirTemp = null;
+    this.error.newCombAirTemp = null;
+    let canCalculate: boolean = true;
+
+    if (this.efficiencyImprovementInputs.currentCombustionAirTemp > this.efficiencyImprovementInputs.currentFlueGasTemp) {
+      this.error.currentCombAirTemp = 'Combustion air temperature must be less than flue gas temperature';
+      canCalculate = false;
+    }
+
+    if (this.efficiencyImprovementInputs.newCombustionAirTemp > this.efficiencyImprovementInputs.newFlueGasTemp) {
+      this.error.newCombAirTemp = 'Combustion air temperature must be less than flue gas temperature';
+      canCalculate = false;
+    }
+
+    if (canCalculate) {
+      this.calculate.emit(true);
+    }
   }
 
   focusField(str: string) {
