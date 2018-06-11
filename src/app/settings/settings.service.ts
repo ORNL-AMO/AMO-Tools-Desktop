81083859609b25dcc151fcc0b9a2dc24342fe462
Rappoport <1br@mb0088756.ornl.gov>
2018-06-11 10:01:42 -0400

    Added more metric/imperial unit conversions: gas density, static pressure, inlet/outlet pressure, flow rate

--- src/app/settings/settings.service.ts
+++ src/app/settings/settings.service.ts
@@ -183,7 +183,7 @@ export class SettingsService {
         phastRollupElectricityUnit: 'MMBtu',
         phastRollupSteamUnit: 'MMBtu',
         densityMeasurement: 'lbscf',
-        fanFlowRate: 'ft3/h',
+        fanFlowRate: 'ft3/min',
         fanPressureMeasurement: 'inH2o'
         // currentMeasurement: 'A',
         // viscosityMeasurement: 'cST',
@@ -209,7 +209,7 @@ export class SettingsService {
         phastRollupElectricityUnit: 'GJ',
         phastRollupSteamUnit: 'GJ',
         densityMeasurement: 'kgNm3',
-        fanFlowRate: 'm3/h',
+        fanFlowRate: 'm3/s',
         fanPressureMeasurement: 'Pa'
         // currentMeasurement: 'A',
         // viscosityMeasurement: 'cST',
