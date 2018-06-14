c84f00d8e85b16b98412b9b03526d54fcebfcce6
Dmitry Howard <dhoward@intertech.com>
2018-06-14 13:28:55 -0500

    Saving work

--- src/app/fsat/fsat-sankey/fsat-sankey.component.ts
+++ src/app/fsat/fsat-sankey/fsat-sankey.component.ts
@@ -1,6 +1,22 @@
 import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { Settings } from '../../shared/models/settings';
 import { FSAT } from '../../shared/models/fans';
+import { ConvertUnitsService } from '../../shared/convert-units/convert-units.service';
+
+
+var svg;
+
+// use these values to alter label font position and size
+const width = 2650,
+  height = 1400,
+  labelFontSize = 28,
+  labelPadding = 4,
+  reportFontSize = 34,
+  reportPadding = 4,
+  topLabelPositionY = 150,
+  bottomLabelPositionY = 1250,
+  topReportPositionY = 125,
+  bottomReportPositionY = 1250;
 
 @Component({
   selector: 'app-fsat-sankey',
@@ -24,12 +40,28 @@ export class FsatSankeyComponent implements OnInit {
   isBaseline: boolean;
 
   @ViewChild('ngChart') ngChart: ElementRef;
+  width: number;
+  height: number;
 
-  
+  firstChange: boolean = true;
+  baseSize: number = 300;
+  minSize: number = 3;
 
-  constructor() { }
+  title: string;
+  unit: string;
+  titlePlacement: string;
+
+  energyInput: number;
+  motorLosses: number;
+  driveLosses: number;
+  fanLosses: number;
+  usefulOutput: number;
+
+  constructor(private convertUnitsService: ConvertUnitsService) { }
 
   ngOnInit() {
+    console.log('this.fsat = ');
+    console.log(this.fsat);
   }
 
 }
