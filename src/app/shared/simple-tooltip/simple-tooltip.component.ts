c25be880572306ffb931ef92257977fe322b91a5
Dmitry Howard <dhoward@intertech.com>
2018-06-18 14:54:23 -0500

    Tooltip functions have been added

--- src/app/shared/simple-tooltip/simple-tooltip.component.ts
+++ src/app/shared/simple-tooltip/simple-tooltip.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-simple-tooltip',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./simple-tooltip.component.css']
 })
 export class SimpleTooltipComponent implements OnInit {
+  @Input()
+  message: string;
+  @Input()
+  hover: boolean;
+  @Input()
+  display: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    if (this.message === null || this.message === undefined) {
+      this.message = "This is a placeholder tool tip.";
+    }
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+  }
+
 
 }
