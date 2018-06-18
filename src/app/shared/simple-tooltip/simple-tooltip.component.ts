80224cccadd3f2a8c85d55797392d761e2dec1f4
Dmitry Howard <dhoward@intertech.com>
2018-06-18 13:11:43 -0500

    Saving work

---
+++ src/app/shared/simple-tooltip/simple-tooltip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-tooltip',
+  templateUrl: './simple-tooltip.component.html',
+  styleUrls: ['./simple-tooltip.component.css']
+})
+export class SimpleTooltipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
