08e53d7487a564a890bed841b147a82ba7a5c879
Dmitry Howard <dhoward@intertech.com>
2018-06-18 12:04:07 -0500

    Working on getting good data for pie charts

--- src/app/fsat/fsat-report/fsat-report.module.ts
+++ src/app/fsat/fsat-report/fsat-report.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FsatReportComponent } from './fsat-report.component';
 import { InputSummaryComponent } from './input-summary/input-summary.component';
 import { ResultsSummaryComponent } from './results-summary/results-summary.component';
+import { FsatReportGraphsComponent } from './fsat-report-graphs/fsat-report-graphs.component';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     SharedModule
   ],
-  declarations: [FsatReportComponent, InputSummaryComponent, ResultsSummaryComponent],
+  declarations: [FsatReportComponent, InputSummaryComponent, ResultsSummaryComponent, FsatReportGraphsComponent],
   exports: [FsatReportComponent]
 })
 export class FsatReportModule { }
