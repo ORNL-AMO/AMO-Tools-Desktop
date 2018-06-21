9a35e84df6b286dc1e5dcf6d1798a7125b465f54
Mark Root <mroot@intertech.com>
2018-06-21 12:25:27 -0500

    init setup for 1579 fans

--- src/app/fsat/fsat-banner/fsat-banner.component.ts
+++ src/app/fsat/fsat-banner/fsat-banner.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Assessment } from '../../shared/models/assessment';
 import { FsatService } from '../fsat.service';
+import { Router } from '@angular/router';
+import { AssessmentService } from '../../assessment/assessment.service';
 
 @Component({
   selector: 'app-fsat-banner',
@@ -12,7 +14,7 @@ export class FsatBannerComponent implements OnInit {
   assessment: Assessment;
 
   mainTab: string;
-  constructor(private fsatService: FsatService) { }
+  constructor(private fsatService: FsatService, private router: Router, private assessmentService: AssessmentService) { }
 
   ngOnInit() {
     this.fsatService.mainTab.subscribe(val => {
@@ -23,5 +25,17 @@ export class FsatBannerComponent implements OnInit {
   changeTab(str: string) {
     if(str == 'assessment' || str == 'system-setup' || str == 'report')
     this.fsatService.mainTab.next(str);
+  }
+  
+  goHome(){
+    this.assessmentService.workingDirectoryId.next(undefined);
+    this.assessmentService.dashboardView.next('landing-screen');
+    this.router.navigateByUrl('/dashboard');
+  }
+
+  goToFolder(){
+    this.assessmentService.workingDirectoryId.next(this.assessment.directoryId);
+    this.assessmentService.dashboardView.next('assessment-dashboard');
+    this.router.navigateByUrl('/dashboard');
   }
 }
