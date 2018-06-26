9bc06f78d9e73cd986fd933307cd18850c5529ac
Rappoport <brappop@umich.edu>
2018-06-26 16:49:33 -0400

    Added tooltip for download button for bar chart in PSAT

--- src/app/psat/psat-report/psat-report-graphs/psat-bar-chart/psat-bar-chart.component.ts
+++ src/app/psat/psat-report/psat-report-graphs/psat-bar-chart/psat-bar-chart.component.ts
@@ -45,6 +45,12 @@ export class PsatBarChartComponent implements OnInit {
   barData2: Array<any>;
   chartData: Array<any>;
 
+  //booleans for tooltip
+  hoverBtnExport: boolean = false;
+  displayExportTooltip: boolean = false;
+  hoverBtnGridLines: boolean = false;
+  displayGridLinesTooltip: boolean = false;
+
   constructor(private windowRefService: WindowRefService, private svgToPngService: SvgToPngService) { }
 
   ngOnInit() {
@@ -78,6 +84,54 @@ export class PsatBarChartComponent implements OnInit {
       }
     }
   }
+
+  // ========== export/gridline tooltip functions ==========
+  // if you get a large angular error, make sure to add SimpleTooltipComponent to the imports of the calculator's module
+  // for example, check motor-performance-graph.module.ts
+  initTooltip(btnType: string) {
+
+    if (btnType == 'btnExportChart') {
+      this.hoverBtnExport = true;
+    }
+    else if (btnType == 'btnGridLines') {
+      this.hoverBtnGridLines = true;
+    }
+    setTimeout(() => {
+      this.checkHover(btnType);
+    }, 700);
+  }
+
+  hideTooltip(btnType: string) {
+
+    if (btnType == 'btnExportChart') {
+      this.hoverBtnExport = false;
+      this.displayExportTooltip = false;
+    }
+    else if (btnType == 'btnGridLines') {
+      this.hoverBtnGridLines = false;
+      this.displayGridLinesTooltip = false;
+    }
+  }
+
+  checkHover(btnType: string) {
+    if (btnType == 'btnExportChart') {
+      if (this.hoverBtnExport) {
+        this.displayExportTooltip = true;
+      }
+      else {
+        this.displayExportTooltip = false;
+      }
+    }
+    else if (btnType == 'btnGridLines') {
+      if (this.hoverBtnGridLines) {
+        this.displayGridLinesTooltip = true;
+      }
+      else {
+        this.displayGridLinesTooltip = false;
+      }
+    }
+  }
+  // ========== end tooltip functions ==========
 
   setBarLabels() {
     this.labels = new Array<string>();
@@ -210,7 +264,7 @@ export class PsatBarChartComponent implements OnInit {
         unload: true,
         columns: [this.barData1, this.barData2]
       });
-      
+
     }
   }
 
