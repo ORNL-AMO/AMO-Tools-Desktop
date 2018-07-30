1c71baf1b7414dd442a68a243231389a14feee6b
Mark Root <mroot@intertech.com>
2018-07-30 16:28:33 -0500

    help for turbine calc

--- src/app/calculator/steam/turbine/turbine-help/turbine-help.component.ts
+++ src/app/calculator/steam/turbine/turbine-help/turbine-help.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { Settings } from '../../../../shared/models/settings';
+import { TurbineRanges, TurbineService } from '../turbine.service';
+import { SteamService } from '../../steam.service';
 
 @Component({
   selector: 'app-turbine-help',
@@ -11,9 +13,55 @@ export class TurbineHelpComponent implements OnInit {
   currentField: string;
   @Input()
   settings: Settings;
-  constructor() { }
+  @Input()
+  outletQuantity: number;
+  @Input()
+  inletQuantity: number;
+  @Input()
+  turbineProperty: number;
+
+  rangeValues: TurbineRanges;
+  constructor(private turbineService: TurbineService, private steamService: SteamService) { }
 
   ngOnInit() {
+    this.getRanges();
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.outletQuantity) {
+      this.getRanges();
+    }
+    if (changes.inletQuantity) {
+      this.getRanges();
+    }
+  }
+
+
+  getRanges() {
+    this.rangeValues = this.turbineService.getRangeValues(this.settings, this.inletQuantity, this.outletQuantity);
+  }
+
+  getDisplayUnit(unit: string) {
+    if (unit) {
+      return this.steamService.getDisplayUnit(unit);
+    } else {
+      return unit;
+    }
+  }
+
+  getOptionDisplayUnit(quantity: number) {
+    let displayUnit: string;
+    if (quantity == 0) {
+      displayUnit = this.getDisplayUnit(this.settings.steamTemperatureMeasurement);
+      return displayUnit;
+    } else if (quantity == 1) {
+      displayUnit = this.getDisplayUnit(this.settings.steamSpecificEnthalpyMeasurement);
+      return displayUnit;
+    } else if (quantity == 2) {
+      displayUnit = this.getDisplayUnit(this.settings.steamSpecificEntropyMeasurement);
+      return displayUnit;
+    } else if (quantity == 3) {
+      return displayUnit;
+    }
+  }
 }
