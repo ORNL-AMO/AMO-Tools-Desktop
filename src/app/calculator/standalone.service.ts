95bfad9455c2334877920ecb8c51ef3f64efec16
Mark Root <mroot@intertech.com>
2018-08-07 14:57:01 -0500

    updates to system capacity

--- src/app/calculator/standalone.service.ts
+++ src/app/calculator/standalone.service.ts
@@ -54,7 +54,7 @@ export class StandaloneService {
   }
 
   static airSystemCapacity(input: AirSystemCapacityInput): AirSystemCapacityOutput {
-    console.log(input);
+\\
     return standaloneAddon.airSystemCapacity(input);
   }
 
