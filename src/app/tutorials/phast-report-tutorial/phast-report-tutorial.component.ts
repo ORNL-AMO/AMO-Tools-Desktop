8dd7e1bce82310fb87928b0a7d92a541ef5e9f8b
Mark Root <mroot@intertech.com>
2018-08-16 15:32:13 -0500

    phast tutorials

--- src/app/tutorials/phast-report-tutorial/phast-report-tutorial.component.ts
+++ src/app/tutorials/phast-report-tutorial/phast-report-tutorial.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-phast-report-tutorial',
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./phast-report-tutorial.component.css']
 })
 export class PhastReportTutorialComponent implements OnInit {
+  @Output('closeTutorial')
+  closeTutorial = new EventEmitter<boolean>();
 
+  showItem: Array<boolean> = [true, false, false, false, false, false, false];
+
+  index: number = 0;
+  //dontShow: boolean = true;
+  show: boolean = true;
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.next();
+    }, 1000)
+ }
+
+  next() {
+    this.showItem[this.index] = false;
+    this.index++;
+    this.showItem[this.index] = true;
   }
 
+  prev() {
+    this.showItem[this.index] = false;
+    this.index--;
+    this.showItem[this.index] = true;
+  }
+  close() {
+    // if(this.dontShow){
+    //   // this.sendDontShow();
+    // }
+    this.closeTutorial.emit(true);
+  }
+
+  // sendDontShow(){
+  //   this.settingsService.setDontShow.next(this.dontShow);
+  // }
 }
