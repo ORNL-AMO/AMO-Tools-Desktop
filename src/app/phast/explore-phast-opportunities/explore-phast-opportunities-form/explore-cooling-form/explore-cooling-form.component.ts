ceda20ceafdbee3c3ea19daf11161b8f45128fe0
Mark Root <mroot@intertech.com>
2018-09-11 16:21:41 -0500

    updates to cooling

--- src/app/phast/explore-phast-opportunities/explore-phast-opportunities-form/explore-cooling-form/explore-cooling-form.component.ts
+++ src/app/phast/explore-phast-opportunities/explore-phast-opportunities-form/explore-cooling-form/explore-cooling-form.component.ts
@@ -3,6 +3,7 @@ import { PHAST } from '../../../../shared/models/phast/phast';
 import { Settings } from '../../../../shared/models/settings';
 import { LiquidCoolingLoss, GasCoolingLoss } from '../../../../shared/models/phast/losses/coolingLoss';
 import { LossTab } from '../../../tabs';
+import { GasCoolingWarnings, LiquidCoolingWarnings, CoolingLossesService } from '../../../losses/cooling-losses/cooling-losses.service';
 @Component({
   selector: 'app-explore-cooling-form',
   templateUrl: './explore-cooling-form.component.html',
@@ -27,11 +28,9 @@ export class ExploreCoolingFormComponent implements OnInit {
   showCooling: boolean = false;
   showFlowRate: Array<boolean>;
   showTemp: Array<boolean>;
-  flowError1: Array<string>;
-  flowError2: Array<string>;
-  tempError1: Array<string>;
-  tempError2: Array<string>;
-  constructor() { }
+  baselineWarnings: Array<{ flowWarning: string, tempWarning: string }>;
+  modificationWarnings: Array<{ flowWarning: string, tempWarning: string }>;
+  constructor(private coolingLossesService: CoolingLossesService) { }
 
   ngOnInit() {
     this.initData();
@@ -46,32 +45,34 @@ export class ExploreCoolingFormComponent implements OnInit {
     }
   }
 
-
   initData() {
+    this.baselineWarnings
     this.baselineLosses = new Array();
     this.modifiedLosses = new Array();
     this.showFlowRate = new Array<boolean>();
-    this.flowError1 = new Array<string>();
-    this.flowError2 = new Array<string>();
-    this.tempError1 = new Array<string>();
-    this.tempError2 = new Array<string>();
+    this.baselineWarnings = new Array<{ flowWarning: string, tempWarning: string }>();
+    this.modificationWarnings = new Array<{ flowWarning: string, tempWarning: string }>();
     this.showTemp = new Array<boolean>();
 
     let index = 0;
     this.phast.losses.coolingLosses.forEach(loss => {
-      this.flowError1.push(null);
-      this.flowError2.push(null);
-      this.tempError1.push(null);
-      this.tempError2.push(null);
       if (loss.coolingLossType == 'Liquid') {
         this.baselineLosses.push({ loss: loss.liquidCoolingLoss, type: 'Liquid', name: loss.name });
+        let tmpWarnings: { flowWarning: string, tempWarning: string } = this.getLiquidWarnings(loss.liquidCoolingLoss);
+        this.baselineWarnings.push(tmpWarnings)
       } else if (loss.coolingLossType == 'Gas') {
         this.baselineLosses.push({ loss: loss.gasCoolingLoss, type: 'Gas', name: loss.name });
+        let tmpWarnings: { flowWarning: string, tempWarning: string } = this.getGasWarnings(loss.gasCoolingLoss);
+        this.baselineWarnings.push(tmpWarnings)
       }
       if (this.phast.modifications[this.exploreModIndex].phast.losses.coolingLosses[index].coolingLossType == 'Liquid') {
         this.modifiedLosses.push({ loss: this.phast.modifications[this.exploreModIndex].phast.losses.coolingLosses[index].liquidCoolingLoss, type: 'Liquid', name: this.phast.modifications[this.exploreModIndex].phast.losses.coolingLosses[index].name })
+        let tmpWarnings: { flowWarning: string, tempWarning: string } = this.getLiquidWarnings(this.phast.modifications[this.exploreModIndex].phast.losses.coolingLosses[index].liquidCoolingLoss);
+        this.modificationWarnings.push(tmpWarnings)
       } else if (this.phast.modifications[this.exploreModIndex].phast.losses.coolingLosses[index].coolingLossType == 'Gas') {
         this.modifiedLosses.push({ loss: this.phast.modifications[this.exploreModIndex].phast.losses.coolingLosses[index].gasCoolingLoss, type: 'Gas', name: this.phast.modifications[this.exploreModIndex].phast.losses.coolingLosses[index].name });
+        let tmpWarnings: { flowWarning: string, tempWarning: string } = this.getGasWarnings(this.phast.modifications[this.exploreModIndex].phast.losses.coolingLosses[index].gasCoolingLoss);
+        this.modificationWarnings.push(tmpWarnings)
       }
       let check: boolean = (this.baselineLosses[index].loss.flowRate != this.modifiedLosses[index].loss.flowRate);
       if (!this.showCooling && check) {
@@ -93,7 +94,16 @@ export class ExploreCoolingFormComponent implements OnInit {
       this.showTemp.push(check);
       index++
     })
-    console.log(this.modifiedLosses);
+  }
+
+  getLiquidWarnings(liquidCoolingLoss: LiquidCoolingLoss): { flowWarning: string, tempWarning: string } {
+    let tmpLiquidCoolingWarnings: LiquidCoolingWarnings = this.coolingLossesService.checkLiquidWarnings(liquidCoolingLoss);
+    return { flowWarning: tmpLiquidCoolingWarnings.liquidFlowWarning, tempWarning: tmpLiquidCoolingWarnings.temperatureWarning };
+  }
+
+  getGasWarnings(gasCoolingLoss: GasCoolingLoss): { flowWarning: string, tempWarning: string } {
+    let tmpGasCoolingWarnings: GasCoolingWarnings = this.coolingLossesService.checkGasWarnings(gasCoolingLoss);
+    return { flowWarning: tmpGasCoolingWarnings.gasFlowWarning, tempWarning: tmpGasCoolingWarnings.temperatureWarning };
   }
 
   toggleCooling() {
@@ -103,6 +113,7 @@ export class ExploreCoolingFormComponent implements OnInit {
         if (loss.type == this.modifiedLosses[index].type) {
           this.modifiedLosses[index].loss.flowRate = loss.loss.flowRate;
           this.modifiedLosses[index].loss.initialTemperature = loss.loss.initialTemperature;
+          this.checkModificationWarning(this.modifiedLosses[index], index)
         }
       })
     }
@@ -113,25 +124,26 @@ export class ExploreCoolingFormComponent implements OnInit {
       if (obj.type == this.modifiedLosses[index].type) {
         this.modifiedLosses[index].loss.flowRate = obj.loss.flowRate;
       }
+      this.checkModificationWarning(this.modifiedLosses[index], index)
     }
   }
 
-  checkFlowRate(num: number, flowRate: number, index: number) {
-    if (flowRate < 0) {
-      if (num == 1) {
-        this.flowError1[index] = 'Flow rate must be equal or greater than 0';
-      } else if (num == 2) {
-        this.flowError2[index] = 'Flow rate must be equal or greater than 0';
-      }
-    } else {
-      if (num == 1) {
-        this.flowError1[index] = null;
-      } else if (num == 2) {
-        this.flowError2[index] = null;
-      }
-    }
-    this.calculate();
-  }
+  // checkFlowRate(num: number, flowRate: number, index: number) {
+  //   if (flowRate < 0) {
+  //     if (num == 1) {
+  //       this.flowError1[index] = 'Flow rate must be equal or greater than 0';
+  //     } else if (num == 2) {
+  //       this.flowError2[index] = 'Flow rate must be equal or greater than 0';
+  //     }
+  //   } else {
+  //     if (num == 1) {
+  //       this.flowError1[index] = null;
+  //     } else if (num == 2) {
+  //       this.flowError2[index] = null;
+  //     }
+  //   }
+  //   this.calculate();
+  // }
 
   toggleTemp(index: number, obj: { loss: LiquidCoolingLoss | GasCoolingLoss, type: string }) {
     if (this.showTemp[index] == false) {
@@ -142,46 +154,70 @@ export class ExploreCoolingFormComponent implements OnInit {
         } else if (obj.type == 'Liquid') {
           this.modifiedLosses[index].loss.outletTemperature = obj.loss.outletTemperature;
         }
+        this.checkModificationWarning(this.modifiedLosses[index], index)
       }
     }
   }
 
-  checkTemp(num: number, obj: { loss: LiquidCoolingLoss | GasCoolingLoss, type: string }, index: number) {
-    if (obj.type == this.modifiedLosses[index].type) {
-      if (obj.type == 'Liquid') {
-        if (obj.loss.initialTemperature > this.modifiedLosses[index].loss.outletTemperature) {
-          if (num == 1) {
-            this.tempError1[index] = 'Inlet temperature greater the outlet temperature.';
-          } else if (num == 2) {
-            this.tempError2[index] = 'Inlet temperature greater the outlet temperature.';
-          }
-        } else {
-          this.noTempError(num, index);
-        }
-      } else if (obj.type == 'Gas') {
-        if (obj.loss.initialTemperature > this.modifiedLosses[index].loss.finalTemperature) {
-          if (num == 1) {
-            this.tempError1[index] = 'Inlet temperature greater the outlet temperature.';
-          } else if (num == 2) {
-            this.tempError2[index] = 'Inlet temperature greater the outlet temperature.';
-          }
-        } else {
-          this.noTempError(num, index);
-        }
-      }
+  // checkTemp(num: number, obj: { loss: LiquidCoolingLoss | GasCoolingLoss, type: string }, index: number) {
+  //   if (obj.type == this.modifiedLosses[index].type) {
+  //     if (obj.type == 'Liquid') {
+  //       if (obj.loss.initialTemperature > this.modifiedLosses[index].loss.outletTemperature) {
+  //         if (num == 1) {
+  //           this.tempError1[index] = 'Inlet temperature greater the outlet temperature.';
+  //         } else if (num == 2) {
+  //           this.tempError2[index] = 'Inlet temperature greater the outlet temperature.';
+  //         }
+  //       } else {
+  //         this.noTempError(num, index);
+  //       }
+  //     } else if (obj.type == 'Gas') {
+  //       if (obj.loss.initialTemperature > this.modifiedLosses[index].loss.finalTemperature) {
+  //         if (num == 1) {
+  //           this.tempError1[index] = 'Inlet temperature greater the outlet temperature.';
+  //         } else if (num == 2) {
+  //           this.tempError2[index] = 'Inlet temperature greater the outlet temperature.';
+  //         }
+  //       } else {
+  //         this.noTempError(num, index);
+  //       }
+  //     }
+  //   }
+  //   else {
+  //     this.noTempError(num, index);
+  //   }
+  //   this.calculate();
+  // }
+
+  // noTempError(num: number, index: number) {
+  //   if (num == 1) {
+  //     this.tempError1[index] = null;
+  //   } else if (num == 2) {
+  //     this.tempError2[index] = null;
+  //   }
+  // }
+
+  checkBaselineWarning(obj: { loss: LiquidCoolingLoss | GasCoolingLoss, type: string }, index: number) {
+    let tmpWarnings: { flowWarning: string, tempWarning: string };
+    if (obj.type == 'Gas') {
+      tmpWarnings = this.getGasWarnings(obj.loss);
+    } else if (obj.type == 'Liquid') {
+      tmpWarnings = this.getLiquidWarnings(obj.loss);
     }
-    else {
-      this.noTempError(num, index);
-    }
+    this.baselineWarnings[index] = tmpWarnings;
     this.calculate();
   }
 
-  noTempError(num: number, index: number) {
-    if (num == 1) {
-      this.tempError1[index] = null;
-    } else if (num == 2) {
-      this.tempError2[index] = null;
+  checkModificationWarning(obj: { loss: LiquidCoolingLoss | GasCoolingLoss, type: string }, index: number) {
+    let tmpWarnings: { flowWarning: string, tempWarning: string };
+    if (obj.type == 'Gas') {
+      tmpWarnings = this.getGasWarnings(obj.loss);
+    } else if (obj.type == 'Liquid') {
+      tmpWarnings = this.getLiquidWarnings(obj.loss);
     }
+    this.modificationWarnings[index] = tmpWarnings;
+    console.log(this.modificationWarnings);
+    this.calculate();
   }
 
   focusField(str: string) {
@@ -193,7 +229,7 @@ export class ExploreCoolingFormComponent implements OnInit {
   }
 
   focusOut() {
-
+    this.changeField.emit('default');
   }
 
   calculate() {
