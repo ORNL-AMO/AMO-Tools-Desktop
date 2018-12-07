6451ae333d5fe4c7156772a1f98e7a0d074d2da0
Dmitry Howard <dhoward@intertech.com>
2018-12-07 10:15:49 -0600

    Made some updates to psat examples, still has errors

--- src/app/core/mockPsat.ts
+++ src/app/core/mockPsat.ts
@@ -126,10 +126,10 @@ export const MockPsat: Assessment = {
                         motor_field_current: 130.3,
                         motor_field_voltage: 460,
                         cost_kw_hour: 0.066,
-                        implementationCosts: null,
-                        specifiedDriveEfficiency: null,
                         fluidType: "Water",
-                        fluidTemperature: 68
+                        fluidTemperature: 68,
+                        specifiedDriveEfficiency: null,
+                        implementationCosts: null
                     },
                     outputs: {
                         pump_efficiency: 64.4,
@@ -140,6 +140,8 @@ export const MockPsat: Assessment = {
                         motor_power_factor: 44.67,
                         motor_current: 54.92,
                         motor_power: 19.54,
+                        load_factor: 0.15,
+                        drive_efficiency: 95.52,
                         annual_energy: 171.21,
                         annual_cost: 11299.56,
                         annual_savings_potential: 0,
@@ -181,10 +183,10 @@ export const MockPsat: Assessment = {
                         motor_field_current: 130.3,
                         motor_field_voltage: 460,
                         cost_kw_hour: 0.066,
-                        implementationCosts: null,
-                        specifiedDriveEfficiency: null,
                         fluidType: "Water",
-                        fluidTemperature: 68
+                        fluidTemperature: 68,
+                        specifiedDriveEfficiency: null,
+                        implementationCosts: null
                     },
                     outputs: {
                         pump_efficiency: 81.81,
@@ -195,6 +197,8 @@ export const MockPsat: Assessment = {
                         motor_power_factor: 47.47,
                         motor_current: 55.82,
                         motor_power: 21.11,
+                        load_factor: 0.16,
+                        drive_efficiency: 95.55,
                         annual_energy: 184.96,
                         annual_cost: 12207.29,
                         annual_savings_potential: 0,
@@ -208,62 +212,6 @@ export const MockPsat: Assessment = {
                     fieldDataNotes: ''
                 }
             },
-            {
-                psat: {
-                    name: "New",
-                    inputs: {
-                        pump_style: 6,
-                        pump_specified: 87.52,
-                        pump_rated_speed: 1785,
-                        drive: 1,
-                        kinematic_viscosity: 1.107,
-                        specific_gravity: 1.002,
-                        stages: 1,
-                        fixed_speed: 0,
-                        line_frequency: 60,
-                        motor_rated_power: 150.01,
-                        motor_rated_speed: 1785,
-                        efficiency_class: 2,
-                        efficiency: 95,
-                        motor_rated_voltage: 460,
-                        load_estimation_method: 1,
-                        motor_rated_fla: 168.62,
-                        margin: 0,
-                        operating_hours: 8760,
-                        flow_rate: 2500,
-                        head: 137,
-                        motor_field_power: 88.2,
-                        motor_field_current: 130.3,
-                        motor_field_voltage: 460,
-                        cost_kw_hour: 0.066,
-                        fluidType: "Water",
-                        fluidTemperature: 68,
-                        specifiedDriveEfficiency: null,
-                        implementationCosts: null
-                    },
-                    outputs: {
-                        pump_efficiency: 87.52,
-                        motor_rated_power: 150.01,
-                        motor_shaft_power: 103.2,
-                        pump_shaft_power: 98.99,
-                        motor_efficiency: 96.35,
-                        motor_power_factor: 83.42,
-                        motor_current: 120.22,
-                        motor_power: 79.91,
-                        annual_energy: 700,
-                        annual_cost: 46199.95,
-                        annual_savings_potential: 0,
-                        optimization_rating: 0
-                    }
-                },
-                notes: {
-                    systemBasicsNotes: '',
-                    pumpFluidNotes: '',
-                    motorNotes: '',
-                    fieldDataNotes: ''
-                },
-                exploreOpportunities: true
-            }
         ]
     },
     selected: false,
