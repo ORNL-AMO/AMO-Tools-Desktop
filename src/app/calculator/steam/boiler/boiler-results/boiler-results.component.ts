47dde9ad0ab1b78539842a8dba2c64e76c35d964
mark-root <mroot@intertech.com>
2019-02-15 13:44:05 -0600

    updated boiler calc table

--- src/app/calculator/steam/boiler/boiler-results/boiler-results.component.ts
+++ src/app/calculator/steam/boiler/boiler-results/boiler-results.component.ts
@@ -16,49 +16,18 @@ export class BoilerResultsComponent implements OnInit {
   results: BoilerOutput;
   @Input()
   inputData: BoilerInput;
-  energyMeasurement: string;
 
   @ViewChild('copyTable0') copyTable0: ElementRef;
   table0String: any;
-  @ViewChild('copyTable1') copyTable1: ElementRef;
-  table1String: any;
-  @ViewChild('copyTable2') copyTable2: ElementRef;
-  table2String: any;
-  @ViewChild('copyTable3') copyTable3: ElementRef;
-  table3String: any;
 
-
-  constructor(private steamService: SteamService) { }
+  constructor() { }
 
   ngOnInit() {
-    if (this.settings.steamEnergyMeasurement == 'kWh') {
-      this.energyMeasurement = 'kW';
-    } else {
-      this.energyMeasurement = this.settings.steamEnergyMeasurement + '/hr';
-    }
+
   }
 
-  getDisplayUnit(unit: string) {
-    if (unit) {
-      return this.steamService.getDisplayUnit(unit);
-    } else {
-      return unit;
-    }
-  }
 
   updateTable0String() {
     this.table0String = this.copyTable0.nativeElement.innerText;
-  }
-
-  updateTable1String() {
-    this.table1String = this.copyTable1.nativeElement.innerText;
-  }
-
-  updateTable2String() {
-    this.table2String = this.copyTable2.nativeElement.innerText;
-  }
-  
-  updateTable3String() {
-    this.table3String = this.copyTable3.nativeElement.innerText;
   }
 }
