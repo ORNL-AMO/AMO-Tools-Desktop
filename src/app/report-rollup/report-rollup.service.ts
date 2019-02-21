d203e455395a27c1781d8d863084fc43f28e1eb3
mark-root <mroot@intertech.com>
2019-02-21 15:27:43 -0600

    fixed rollup summary steam

--- src/app/report-rollup/report-rollup.service.ts
+++ src/app/report-rollup/report-rollup.service.ts
@@ -125,7 +125,6 @@ export class ReportRollupService {
     this.ssmtArray = new Array<ReportItem>();
     let selected = directory.assessments.filter((val) => { return val.selected; });
     selected.forEach(assessment => {
-      console.log(assessment);
       this.pushAssessment(assessment);
     });
     directory.subDirectory.forEach(subDir => {
@@ -397,16 +396,15 @@ export class ReportRollupService {
   initSsmtCompare(resultsArr: Array<AllSsmtResultsData>) {
     let tmpResults: Array<SsmtCompare> = new Array<SsmtCompare>();
     resultsArr.forEach(result => {
-      console.log(result);
       let minCost = _.minBy(result.modificationResults, (result) => { return result.totalOperatingCost; });
       let modIndex;
-      if (minCost) {
+      if (minCost != undefined) {
         modIndex = _.findIndex(result.modificationResults, { totalOperatingCost: minCost.totalOperatingCost });
       }
       let ssmtAssessments = this.ssmtAssessments.value;
       let assessmentIndex = _.findIndex(ssmtAssessments, (val) => { return val.assessment.id === result.assessmentId; });
       let item = ssmtAssessments[assessmentIndex];
-      if (result.isBaseline || !modIndex) {
+      if (result.isBaseline || modIndex == undefined) {
         tmpResults.push({ baseline: item.assessment.ssmt, modification: item.assessment.ssmt, assessmentId: result.assessmentId, selectedIndex: -1, name: item.assessment.name, assessment: item.assessment, settings: item.settings });
       } else {
         tmpResults.push({ baseline: item.assessment.ssmt, modification: item.assessment.ssmt.modifications[modIndex].ssmt, assessmentId: result.assessmentId, selectedIndex: modIndex, name: item.assessment.name, assessment: item.assessment, settings: item.settings });
@@ -446,10 +444,9 @@ export class ReportRollupService {
                 mod.ssmt.resultsCalculated = true;
               }
               let tmpResults: SSMTOutput = mod.ssmt.outputData;
-              if (tmpResults.boilerOutput) {
-                console.log(mod.ssmt.name);
-                modResultsArr.push(tmpResults);
-              }
+              //if (tmpResults.boilerOutput) {
+              modResultsArr.push(tmpResults);
+              //}
             });
             tmpResultsArr.push({ baselineResults: baselineResults, modificationResults: modResultsArr, assessmentId: val.assessment.id });
           } else {
