e04e54b7bc760dfe8177574623527e93aa196ce5
Allie Ledbetter <aeledbetter1999@gmail.com>
2019-02-27 15:31:49 -0500

    Electricity help text works, steam and fuel still broken

--- src/app/phast/metered-energy/metered-energy-help/metered-energy-help.component.ts
+++ src/app/phast/metered-energy/metered-energy-help/metered-energy-help.component.ts
@@ -11,19 +11,54 @@ export class MeteredEnergyHelpComponent implements OnInit {
   @Input()
   inPreAssessment: boolean;
   @Input()
-  showSteam: boolean;
+  calcType: string;
+  @Input()
+  currentEnergySourceType: string;
+  @Input()
+  currentAssessmentType: string;
   @Input()
   showElectricity: boolean;
   @Input()
+  showSteam: boolean;
+  @Input()
   showFuel: boolean;
-  
+  @Input()
+  showDescription: boolean;
 
   constructor() { }
 
   ngOnInit() {
   }
+  ngOnChanges() {
+    if (this.calcType !== 'pump') {
+      this.showDescription = true;
+    } else {
+      this.showDescription = false;
+    }
 
-  setField(str: string) {
-    this.currentField = str;
+    if (this.currentEnergySourceType === 'Electricity') {
+      this.showElectricity = true;
+      this.showSteam = false;
+      this.showFuel = false;
+    } else if (this.currentEnergySourceType === 'Fuel') {
+      this.showElectricity = false;
+      this.showSteam = false;
+      this.showFuel = true;
+    } else if (this.currentEnergySourceType === 'Steam') {
+      this.showElectricity = false;
+      this.showSteam = true;
+      this.showFuel = false;
+    } else if (this.currentEnergySourceType === 'Hybrid') {
+      if (this.currentField !== 'kwRating') {
+        this.showElectricity = false;
+        this.showSteam = false;
+        this.showFuel = true;
+      } else {
+
+        this.showElectricity = true;
+        this.showSteam = false;
+        this.showFuel = false;
+      }
+    }
   }
-}
+  }
