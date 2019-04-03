454071c676947917bdf1d3ba5d485f49ea9d7b1b
mark-root <mroot@intertech.com>
2019-04-03 13:56:02 -0500

    intial adding edit to treasure chest

--- src/app/treasure-hunt/treasure-chest/lighting-replacement-card/lighting-replacement-card.component.ts
+++ src/app/treasure-hunt/treasure-chest/lighting-replacement-card/lighting-replacement-card.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { LightingReplacementTreasureHunt } from '../../../shared/models/treasure-hunt';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { LightingReplacementTreasureHunt, OpportunitySheet } from '../../../shared/models/treasure-hunt';
 import { Settings } from '../../../shared/models/settings';
 
 @Component({
@@ -12,14 +12,35 @@ export class LightingReplacementCardComponent implements OnInit {
   replacement: LightingReplacementTreasureHunt;
   @Input()
   settings: Settings;
-
+  @Input()
+  index: number;
+  @Output('emitEditOpportunitySheet')
+  emitEditOpportunitySheet = new EventEmitter<OpportunitySheet>();
+  @Output('emitEditLighting')
+  emitEditLighting = new EventEmitter<LightingReplacementTreasureHunt>();
+  
   dropdownOpen: boolean = false;
+  title: string;
+
   constructor() { }
 
   ngOnInit() {
+    if(!this.replacement.opportunitySheet){
+      this.title = 'Lighting Replacement #' + (this.index + 1);
+    }else{
+      this.title = this.replacement.opportunitySheet.name;
+    }
   }
 
   showDropdown() {
     this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  editOpportunitySheet(){
+    this.emitEditOpportunitySheet.emit(this.replacement.opportunitySheet);
+  }
+
+  editLighting(){
+    this.emitEditLighting.emit(this.replacement);
   }
 }
--- src/app/treasure-hunt/treasure-chest/treasure-chest.component.ts
+++ src/app/treasure-hunt/treasure-chest/treasure-chest.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { TreasureHunt } from '../../shared/models/treasure-hunt';
+import { Component, OnInit, Input, ViewChild, EventEmitter, Output } from '@angular/core';
+import { TreasureHunt, LightingReplacementTreasureHunt, OpportunitySheet } from '../../shared/models/treasure-hunt';
 import { Settings } from 'http2';
+import { LightingReplacementService } from '../../calculator/lighting/lighting-replacement/lighting-replacement.service';
+import { ModalDirective } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-treasure-chest',
@@ -12,10 +14,49 @@ export class TreasureChestComponent implements OnInit {
   treasureHunt: TreasureHunt;
   @Input()
   settings: Settings;
-  
-  constructor() { }
+  @Output('emitUpdateTreasureHunt')
+  emitUpdateTreasureHunt = new EventEmitter<TreasureHunt>();
+
+  @ViewChild('saveCalcModal') public saveCalcModal: ModalDirective;
+  @ViewChild('opportunitySheetModal') public opportunitySheetModal: ModalDirective;
+
+  selectedCalc: string = 'none';
+  selectedEditIndex: number;
+  selectedEditLightingReplacement: LightingReplacementTreasureHunt;
+  selectedEditOpportunitySheet: OpportunitySheet;
+  constructor(private lightingReplacementService: LightingReplacementService) { }
 
   ngOnInit() {
   }
 
+  selectCalc(str: string) {
+    this.selectedCalc = str;
+  }
+
+  editLighting(lightingReplacement: LightingReplacementTreasureHunt, index: number) {
+    this.selectedEditIndex = index;
+    this.selectedEditLightingReplacement = lightingReplacement;
+    this.lightingReplacementService.baselineData = lightingReplacement.baseline;
+    this.lightingReplacementService.modificationData = lightingReplacement.modifications;
+    this.selectedEditOpportunitySheet = lightingReplacement.opportunitySheet;
+    this.selectCalc('lighting-replacement');
+  }
+
+  saveEditLighting(updatedData: LightingReplacementTreasureHunt) {
+    this.selectedEditLightingReplacement.baseline = updatedData.baseline;
+    this.selectedEditLightingReplacement.modifications = updatedData.modifications;
+    this.saveCalcModal.show();
+  }
+
+  saveLighting(){
+    this.treasureHunt.lightingReplacements[this.selectedEditIndex] = this.selectedEditLightingReplacement;
+    this.emitUpdateTreasureHunt.emit(this.treasureHunt);
+    this.selectedEditLightingReplacement = undefined;
+    this.selectedEditOpportunitySheet = undefined;
+    this.saveCalcModal.hide();
+  }
+
+  showOpportunitySheetModal(){
+    this.opportunitySheetModal.show();
+  }
 }
