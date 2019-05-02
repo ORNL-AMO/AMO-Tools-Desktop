8abc201d6741da609a3f961a902042a338deb5f5
mark-root <mroot@intertech.com>
2019-05-02 14:42:17 -0500

    add bar and donut chart for payback periods

--- src/app/treasure-hunt/treasure-hunt-report/opportunity-payback/opportunity-payback-bar-chart/opportunity-payback-bar-chart.component.ts
+++ src/app/treasure-hunt/treasure-hunt-report/opportunity-payback/opportunity-payback-bar-chart/opportunity-payback-bar-chart.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild, HostListener, SimpleChanges } from '@angular/core';
+import * as c3 from 'c3';
 import { OpportunitiesPaybackDetails } from '../../../../shared/models/treasure-hunt';
-
 @Component({
   selector: 'app-opportunity-payback-bar-chart',
   templateUrl: './opportunity-payback-bar-chart.component.html',
@@ -9,9 +9,60 @@ import { OpportunitiesPaybackDetails } from '../../../../shared/models/treasure-
 export class OpportunityPaybackBarChartComponent implements OnInit {
   @Input()
   opportunitiesPaybackDetails: OpportunitiesPaybackDetails;
+
+  chart: any;
+  @ViewChild('barChartElement') barChartElement: ElementRef;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  ngAfterViewInit() {
+    this.initChart();
+  }
+
+  ngOnDestroy() { }
+
+  initChart() {
+    this.chart = c3.generate({
+      bindto: this.barChartElement.nativeElement,
+      data: {
+        type: 'bar',
+        columns: [
+          ['Less than 1 Year ', this.opportunitiesPaybackDetails.lessThanOneYear.totalSavings],
+          ['1 to 2 Years ', this.opportunitiesPaybackDetails.oneToTwoYears.totalSavings],
+          ['2 to 3 Years ', this.opportunitiesPaybackDetails.twoToThreeYears.totalSavings],
+          ['More than 3 Years ', this.opportunitiesPaybackDetails.moreThanThreeYears.totalSavings],
+        ]
+      },
+      axis: {
+        // x: {
+        //   label: {
+        //     show: false
+        //   },
+        //   tick: {
+        //     type: 'category',
+        //     categories:  ['Payback Period'] 
+        //   }
+        // },
+        y: {
+          label: {
+            text: "Cost Savings ($)",
+            position: 'outer-middle'
+          },
+          tick: {
+            format: function (d) { return '$ ' + (d).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); }
+          }
+        }
+      },
+      grid: {
+        y: {
+          show: true
+        }
+      },
+      tooltip: {
+        show: false
+      }
+    });
   }
 
 }
--- src/app/treasure-hunt/treasure-hunt-report/opportunity-payback/opportunity-payback-donut/opportunity-payback-donut.component.ts
+++ src/app/treasure-hunt/treasure-hunt-report/opportunity-payback/opportunity-payback-donut/opportunity-payback-donut.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
+import * as c3 from 'c3';
 import { OpportunitiesPaybackDetails } from '../../../../shared/models/treasure-hunt';
 
 @Component({
@@ -9,9 +10,44 @@ import { OpportunitiesPaybackDetails } from '../../../../shared/models/treasure-
 export class OpportunityPaybackDonutComponent implements OnInit {
   @Input()
   opportunitiesPaybackDetails: OpportunitiesPaybackDetails;
+
+  chart: any;
+  @ViewChild('donutChartElement') donutChartElement: ElementRef;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  ngAfterViewInit() {
+    this.initChart();
+  }
+
+  ngOnDestroy() { }
+
+  initChart() {
+    this.chart = c3.generate({
+      bindto: this.donutChartElement.nativeElement,
+      data: {
+        type: 'donut',
+        columns: [
+          ['Less than 1 Year ', this.opportunitiesPaybackDetails.lessThanOneYear.totalSavings],
+          ['1 to 2 Years ', this.opportunitiesPaybackDetails.oneToTwoYears.totalSavings],
+          ['2 to 3 Years ', this.opportunitiesPaybackDetails.twoToThreeYears.totalSavings],
+          ['More than 3 Years ', this.opportunitiesPaybackDetails.moreThanThreeYears.totalSavings],
+        ]
+      },
+      // legend: {
+      //   show: false
+      // },
+      // color: {
+      //   pattern: ['#52489C', '#3498DB', '#6DAFA9', '#60B044', '#FF0000'], // the three color levels for the percentage values.
+      //   threshold: {
+      //     values: [25, 50]
+      //   }
+      // },
+      // tooltip: {
+      //   show: false
+      // },
+    });
   }
 
 }
