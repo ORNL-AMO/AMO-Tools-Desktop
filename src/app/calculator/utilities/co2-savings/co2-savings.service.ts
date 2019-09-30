8ab5ba0afd6f705b40ae7f8321c8299ddc4a9136
mark-root <mroot@intertech.com>
2019-09-30 12:48:59 -0500

    conversions for co2 savings

--- src/app/calculator/utilities/co2-savings/co2-savings.service.ts
+++ src/app/calculator/utilities/co2-savings/co2-savings.service.ts
@@ -1,35 +1,47 @@
 import { Injectable } from '@angular/core';
+import { Settings } from '../../../shared/models/settings';
+import { ConvertUnitsService } from '../../../shared/convert-units/convert-units.service';
 
 @Injectable()
 export class Co2SavingsService {
   baselineData: Array<Co2SavingsData>;
   modificationData: Array<Co2SavingsData>;
-  constructor() { }
+  constructor(private convertUnitsService: ConvertUnitsService) { }
 
-  generateExample(isBaseline: boolean): Co2SavingsData {
-    if (isBaseline) {
-      return {
-        energyType: 'fuel',
-        totalEmissionOutputRate: 53.06,
-        electricityUse: 1995,
-        fuelType: 'Natural Gas',
-        energySource: 'Natural Gas',
-        totalEmissionOutput: 0
-      };
+  generateExample(isBaseline: boolean, settings: Settings): Co2SavingsData {
+    let emissionOutputRate: number = 53.06;
+    let electricityUse: number = 1995;
+    if (!isBaseline) {
+      electricityUse = 1500;
+    }
+    if (settings.unitsOfMeasure != 'Imperial') {
+      let conversionHelper: number = this.convertUnitsService.value(1).from('MMBtu').to('GJ');
+      emissionOutputRate = emissionOutputRate / conversionHelper;
+      emissionOutputRate = Number(emissionOutputRate.toFixed(3));
+      electricityUse = this.convertUnitsService.value(electricityUse).from('MMBtu').to('GJ');
+      electricityUse = Number(electricityUse.toFixed(3));
     }
     return {
       energyType: 'fuel',
-      totalEmissionOutputRate: 53.06,
-      electricityUse: 1500,
+      totalEmissionOutputRate: emissionOutputRate,
+      electricityUse: electricityUse,
       fuelType: 'Natural Gas',
       energySource: 'Natural Gas',
       totalEmissionOutput: 0
     };
   }
 
-  calculate(data: Co2SavingsData): Co2SavingsData {
-    if (data.totalEmissionOutputRate && data.electricityUse) {
-      data.totalEmissionOutput = (data.totalEmissionOutputRate) * (data.electricityUse / 1000);
+  calculate(data: Co2SavingsData, settings: Settings): Co2SavingsData {
+    //use copy for conversion data
+    let dataCpy: Co2SavingsData = JSON.parse(JSON.stringify(data));
+    if (settings.unitsOfMeasure != 'Imperial' && data.energyType == 'Fuel') {
+      let conversionHelper: number = this.convertUnitsService.value(1).from('GJ').to('MMBtu');
+      dataCpy.totalEmissionOutputRate = dataCpy.totalEmissionOutputRate / conversionHelper;
+      dataCpy.electricityUse = this.convertUnitsService.value(dataCpy.electricityUse).from('GJ').to('MMBtu');
+    }
+    if (dataCpy.totalEmissionOutputRate && dataCpy.electricityUse) {
+      //set results on original obj
+      data.totalEmissionOutput = (dataCpy.totalEmissionOutputRate) * (dataCpy.electricityUse / 1000);
     } else {
       data.totalEmissionOutput = 0;
     }
