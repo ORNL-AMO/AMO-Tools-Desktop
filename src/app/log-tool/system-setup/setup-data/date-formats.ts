b513d6587153d1c8fde94113ff6f5271db0db21d
Cranfill <cranfillkd@ornl.gov>
2020-01-24 08:54:27 -0500

    Added varity of date and times

--- src/app/log-tool/system-setup/setup-data/date-formats.ts
+++ src/app/log-tool/system-setup/setup-data/date-formats.ts
@@ -1,50 +1,50 @@
 export const _dateFormats: Array<{ value: Array<string>, display: string }> = [
     {
         display: 'MM-DD-YYYY 12:00 XM',
-        value: ['MM/DD/YYYY hh:mm a', 'MM/DD/YYYY hh:mm A']
+        value: ['MM-DD-YYYY hh','MM-DD-YYYY hh:mm','MM-DD-YYYY hh:mm:ss','MM-DD-YYYY hh a','MM-DD-YYYY hh:mm a','MM-DD-YYYY hh:mm:ss a','MM-DD-YYYY hh A','MM-DD-YYYY hh:mm A','MM-DD-YYYY hh:mm:ss A']
     },
     {
         display: 'MM-DD-YY 12:00 XM',
-        value: ['MM/DD/YY hh:mm a', 'MM/DD/YY hh:mm A']
+        value: ['MM-DD-YY hh','MM-DD-YY hh:mm','MM-DD-YY hh:mm:ss','MM-DD-YY hh a','MM-DD-YY hh:mm a','MM-DD-YY hh:mm:ss a','MM-DD-YY hh A','MM-DD-YY hh:mm A','MM-DD-YY hh:mm:ss A']
     },
     {
         display: 'DD-MM-YYYY 12:00 XM',
-        value: ['DD/MM/YYYY hh:mm a', 'DD/MM/YYYY hh:mm A']
+        value: ['DD-MM-YYYY hh','DD-MM-YYYY hh:mm','DD-MM-YYYY hh:mm:ss','DD-MM-YYYY hh a','DD-MM-YYYY hh:mm a','DD-MM-YYYY hh:mm:ss a','DD-MM-YYYY hh A','DD-MM-YYYY hh:mm A','DD-MM-YYYY hh:mm:ss A']
     },
     {
         display: 'DD-MM-YY 12:00 XM',
-        value: ['DD/MM/YY hh:mm a', 'DD/MM/YY hh:mm A']
+        value: ['DD-MM-YY hh','DD-MM-YY hh:mm','DD-MM-YY hh:mm:ss','DD-MM-YY hh a','DD-MM-YY hh:mm a','DD-MM-YY hh:mm:ss a','DD-MM-YY hh A','DD-MM-YY hh:mm A','DD-MM-YY hh:mm:ss A']
     },
     {
         display: 'MM-DD-YYYY 24:00',
-        value: ['MM/DD/YYYY HH:mm', 'M/DD/YYYY H:mm', 'M/DD/YYYY H:mm:s']
+        value: ['MM-DD-YYYY HH','MM-DD-YYYY HH:mm','MM-DD-YYYY HH:mm:ss','MM-DD-YYYY kk','MM-DD-YYYY kk:mm','MM-DD-YYYY kk:mm:ss']
     },
     {
         display: 'MM-DD-YY 24:00',
-        value: ['MM/DD/YY HH:mm']
+        value: ['MM-DD-YY HH','MM-DD-YY HH:mm','MM-DD-YY HH:mm:ss','MM-DD-YY kk','MM-DD-YY kk:mm','MM-DD-YY kk:mm:ss']
     },
     {
         display: 'DD-MM-YYYY 24:00',
-        value: ['DD/MM/YYYY HH:mm']
+        value: ['DD-MM-YYYY HH','DD-MM-YYYY HH:mm','DD-MM-YYYY HH:mm:ss','DD-MM-YYYY kk','DD-MM-YYYY kk:mm','DD-MM-YYYY kk:mm:ss']
     },
     {
         display: 'DD-MM-YY 24:00',
-        value: ['DD/MM/YY HH:mm']
+        value: ['DD-MM-YY HH','DD-MM-YY HH:mm','DD-MM-YY HH:mm:ss','DD-MM-YY kk','DD-MM-YY kk:mm','DD-MM-YY kk:mm:ss']
     },
     {
         display: 'YYYY-MM-DD 12:00 XM',
-        value: ['YYYY/MM/DD hh:mm a', 'YYYY/MM/DD hh:mm A']
+        value: ['YYYY/MM/DD hh','YYYY/MM/DD hh:mm','YYYY/MM/DD hh:mm:ss','YYYY/MM/DD hh a','YYYY/MM/DD hh:mm a','YYYY/MM/DD hh:mm:ss a','YYYY/MM/DD hh A','YYYY/MM/DD hh:mm A','YYYY/MM/DD hh:mm:ss A']
     },
     {
         display: 'YYYY-DD-MM 12:00 XM',
-        value: ['YYYY/DD/MM hh:mm a', 'YYYY/DD/MM hh:mm A']
+        value: ['YYYY-DD-MM hh','YYYY-DD-MM hh:mm','YYYY-DD-MM hh:mm:ss','YYYY-DD-MM hh a','YYYY-DD-MM hh:mm a','YYYY-DD-MM hh:mm:ss a','YYYY-DD-MM hh A','YYYY-DD-MM hh:mm A','YYYY-DD-MM hh:mm:ss A']
     },
     {
         display: 'YYYY-DD-MM 24:00',
-        value: ['YYYY/DD/MM HH:mm']
+        value: ['YYYY-DD-MM HH','YYYY-DD-MM HH:mm','YYYY-DD-MM HH:mm:ss','YYYY-DD-MM kk','YYYY-DD-MM kk:mm','YYYY-DD-MM kk:mm:ss']
     },
     {
         display: 'YYYY-MM-DD 24:00',
-        value: ['YYYY/MM/DD HH:mm']
+        value: ['YYYY-MM-DD HH','YYYY-MM-DD HH:mm','YYYY-MM-DD HH:mm:ss','YYYY-MM-DD kk','YYYY-MM-DD kk:mm','YYYY-MM-DD kk:mm:ss']
     },
 ]
