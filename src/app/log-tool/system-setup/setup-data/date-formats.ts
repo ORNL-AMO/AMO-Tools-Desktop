bd9e01eb6859c9d266c9d06d7ed33d54bb75bcd4
Cranfill <cranfillkd@ornl.gov>
2020-01-29 15:20:05 -0500

    Added single hour and / compatibility

--- src/app/log-tool/system-setup/setup-data/date-formats.ts
+++ src/app/log-tool/system-setup/setup-data/date-formats.ts
@@ -1,50 +1,86 @@
 export const _dateFormats: Array<{ value: Array<string>, display: string }> = [
     {
         display: 'MM-DD-YYYY 12:00 XM',
-        value: ['MM-DD-YYYY hh','MM-DD-YYYY hh:mm','MM-DD-YYYY hh:mm:ss','MM-DD-YYYY hh a','MM-DD-YYYY hh:mm a','MM-DD-YYYY hh:mm:ss a','MM-DD-YYYY hh A','MM-DD-YYYY hh:mm A','MM-DD-YYYY hh:mm:ss A']
+        value: ['MM/DD/YYYY','MM/DD/YYYY h','MM/DD/YYYY h:mm','MM/DD/YYYY h:mm:ss','MM/DD/YYYY h a','MM/DD/YYYY h:mm a','MM/DD/YYYY h:mm:ss a','MM/DD/YYYY h A','MM/DD/YYYY h:mm A','MM/DD/YYYY h:mm:ss A',
+        'MM/DD/YYYY','MM/DD/YYYY hh','MM/DD/YYYY hh:mm','MM/DD/YYYY hh:mm:ss','MM/DD/YYYY hh a','MM/DD/YYYY hh:mm a','MM/DD/YYYY hh:mm:ss a','MM/DD/YYYY hh A','MM/DD/YYYY hh:mm A','MM/DD/YYYY hh:mm:ss A',
+        'MM-DD-YYYY','MM-DD-YYYY h','MM-DD-YYYY h:mm','MM-DD-YYYY h:mm:ss','MM-DD-YYYY h a','MM-DD-YYYY h:mm a','MM-DD-YYYY h:mm:ss a','MM-DD-YYYY h A','MM-DD-YYYY h:mm A','MM-DD-YYYY h:mm:ss A',
+        'MM-DD-YYYY','MM-DD-YYYY hh','MM-DD-YYYY hh:mm','MM-DD-YYYY hh:mm:ss','MM-DD-YYYY hh a','MM-DD-YYYY hh:mm a','MM-DD-YYYY hh:mm:ss a','MM-DD-YYYY hh A','MM-DD-YYYY hh:mm A','MM-DD-YYYY hh:mm:ss A']
     },
     {
         display: 'MM-DD-YY 12:00 XM',
-        value: ['MM-DD-YY hh','MM-DD-YY hh:mm','MM-DD-YY hh:mm:ss','MM-DD-YY hh a','MM-DD-YY hh:mm a','MM-DD-YY hh:mm:ss a','MM-DD-YY hh A','MM-DD-YY hh:mm A','MM-DD-YY hh:mm:ss A']
+        value: ['MM/DD/YY','MM/DD/YY h','MM/DD/YY h:mm','MM/DD/YY h:mm:ss','MM/DD/YY h a','MM/DD/YY h:mm a','MM/DD/YY h:mm:ss a','MM/DD/YY h A','MM/DD/YY h:mm A','MM/DD/YY h:mm:ss A',
+        'MM/DD/YY','MM/DD/YY hh','MM/DD/YY hh:mm','MM/DD/YY hh:mm:ss','MM/DD/YY hh a','MM/DD/YY hh:mm a','MM/DD/YY hh:mm:ss a','MM/DD/YY hh A','MM/DD/YY hh:mm A','MM/DD/YY hh:mm:ss A',
+        'MM-DD-YY','MM-DD-YY h','MM-DD-YY h:mm','MM-DD-YY h:mm:ss','MM-DD-YY h a','MM-DD-YY h:mm a','MM-DD-YY h:mm:ss a','MM-DD-YY h A','MM-DD-YY h:mm A','MM-DD-YY h:mm:ss A',
+        'MM-DD-YY','MM-DD-YY hh','MM-DD-YY hh:mm','MM-DD-YY hh:mm:ss','MM-DD-YY hh a','MM-DD-YY hh:mm a','MM-DD-YY hh:mm:ss a','MM-DD-YY hh A','MM-DD-YY hh:mm A','MM-DD-YY hh:mm:ss A']
     },
     {
         display: 'DD-MM-YYYY 12:00 XM',
-        value: ['DD-MM-YYYY hh','DD-MM-YYYY hh:mm','DD-MM-YYYY hh:mm:ss','DD-MM-YYYY hh a','DD-MM-YYYY hh:mm a','DD-MM-YYYY hh:mm:ss a','DD-MM-YYYY hh A','DD-MM-YYYY hh:mm A','DD-MM-YYYY hh:mm:ss A']
+        value: ['DD/MM/YYYY','DD/MM/YYYY h','DD/MM/YYYY h:mm','DD/MM/YYYY h:mm:ss','DD/MM/YYYY h a','DD/MM/YYYY h:mm a','DD/MM/YYYY h:mm:ss a','DD/MM/YYYY h A','DD/MM/YYYY h:mm A','DD/MM/YYYY h:mm:ss A',
+        'DD/MM/YYYY','DD/MM/YYYY hh','DD/MM/YYYY hh:mm','DD/MM/YYYY hh:mm:ss','DD/MM/YYYY hh a','DD/MM/YYYY hh:mm a','DD/MM/YYYY hh:mm:ss a','DD/MM/YYYY hh A','DD/MM/YYYY hh:mm A','DD/MM/YYYY hh:mm:ss A',
+        'DD-MM-YYYY','DD-MM-YYYY h','DD-MM-YYYY h:mm','DD-MM-YYYY h:mm:ss','DD-MM-YYYY h a','DD-MM-YYYY h:mm a','DD-MM-YYYY h:mm:ss a','DD-MM-YYYY h A','DD-MM-YYYY h:mm A','DD-MM-YYYY h:mm:ss A',
+        'DD-MM-YYYY','DD-MM-YYYY hh','DD-MM-YYYY hh:mm','DD-MM-YYYY hh:mm:ss','DD-MM-YYYY hh a','DD-MM-YYYY hh:mm a','DD-MM-YYYY hh:mm:ss a','DD-MM-YYYY hh A','DD-MM-YYYY hh:mm A','DD-MM-YYYY hh:mm:ss A']
     },
     {
         display: 'DD-MM-YY 12:00 XM',
-        value: ['DD-MM-YY hh','DD-MM-YY hh:mm','DD-MM-YY hh:mm:ss','DD-MM-YY hh a','DD-MM-YY hh:mm a','DD-MM-YY hh:mm:ss a','DD-MM-YY hh A','DD-MM-YY hh:mm A','DD-MM-YY hh:mm:ss A']
+        value: ['DD/MM/YY','DD/MM/YY h','DD/MM/YY h:mm','DD/MM/YY h:mm:ss','DD/MM/YY h a','DD/MM/YY h:mm a','DD/MM/YY h:mm:ss a','DD/MM/YY h A','DD/MM/YY h:mm A','DD/MM/YY h:mm:ss A',
+        'DD/MM/YY','DD/MM/YY hh','DD/MM/YY hh:mm','DD/MM/YY hh:mm:ss','DD/MM/YY hh a','DD/MM/YY hh:mm a','DD/MM/YY hh:mm:ss a','DD/MM/YY hh A','DD/MM/YY hh:mm A','DD/MM/YY hh:mm:ss A',
+        'DD-MM-YY','DD-MM-YY h','DD-MM-YY h:mm','DD-MM-YY h:mm:ss','DD-MM-YY h a','DD-MM-YY h:mm a','DD-MM-YY h:mm:ss a','DD-MM-YY h A','DD-MM-YY h:mm A','DD-MM-YY h:mm:ss A',
+        'DD-MM-YY','DD-MM-YY hh','DD-MM-YY hh:mm','DD-MM-YY hh:mm:ss','DD-MM-YY hh a','DD-MM-YY hh:mm a','DD-MM-YY hh:mm:ss a','DD-MM-YY hh A','DD-MM-YY hh:mm A','DD-MM-YY hh:mm:ss A']
     },
     {
         display: 'MM-DD-YYYY 24:00',
-        value: ['MM-DD-YYYY HH','MM-DD-YYYY HH:mm','MM-DD-YYYY HH:mm:ss','MM-DD-YYYY kk','MM-DD-YYYY kk:mm','MM-DD-YYYY kk:mm:ss']
+        value: ['MM/DD/YYYY','MM/DD/YYYY H','MM/DD/YYYY H:mm','MM/DD/YYYY H:mm:ss','MM/DD/YYYY kk','MM/DD/YYYY kk:mm','MM/DD/YYYY kk:mm:ss',
+        'MM/DD/YYYY','MM/DD/YYYY HH','MM/DD/YYYY HH:mm','MM/DD/YYYY HH:mm:ss','MM/DD/YYYY kk','MM/DD/YYYY kk:mm','MM/DD/YYYY kk:mm:ss',
+        'MM-DD-YYYY','MM-DD-YYYY H','MM-DD-YYYY H:mm','MM-DD-YYYY H:mm:ss','MM-DD-YYYY kk','MM-DD-YYYY kk:mm','MM-DD-YYYY kk:mm:ss',
+        'MM-DD-YYYY','MM-DD-YYYY HH','MM-DD-YYYY HH:mm','MM-DD-YYYY HH:mm:ss','MM-DD-YYYY kk','MM-DD-YYYY kk:mm','MM-DD-YYYY kk:mm:ss']
     },
     {
         display: 'MM-DD-YY 24:00',
-        value: ['MM-DD-YY HH','MM-DD-YY HH:mm','MM-DD-YY HH:mm:ss','MM-DD-YY kk','MM-DD-YY kk:mm','MM-DD-YY kk:mm:ss']
+        value: ['MM/DD/YY','MM/DD/YY H','MM/DD/YY H:mm','MM/DD/YY H:mm:ss','MM/DD/YY kk','MM/DD/YY kk:mm','MM/DD/YY kk:mm:ss',
+        'MM/DD/YY','MM/DD/YY HH','MM/DD/YY HH:mm','MM/DD/YY HH:mm:ss','MM/DD/YY kk','MM/DD/YY kk:mm','MM/DD/YY kk:mm:ss',
+        'MM-DD-YY','MM-DD-YY H','MM-DD-YY H:mm','MM-DD-YY H:mm:ss','MM-DD-YY kk','MM-DD-YY kk:mm','MM-DD-YY kk:mm:ss',
+        'MM-DD-YY','MM-DD-YY HH','MM-DD-YY HH:mm','MM-DD-YY HH:mm:ss','MM-DD-YY kk','MM-DD-YY kk:mm','MM-DD-YY kk:mm:ss']
     },
     {
         display: 'DD-MM-YYYY 24:00',
-        value: ['DD-MM-YYYY HH','DD-MM-YYYY HH:mm','DD-MM-YYYY HH:mm:ss','DD-MM-YYYY kk','DD-MM-YYYY kk:mm','DD-MM-YYYY kk:mm:ss']
+        value: ['DD/MM/YYYY','DD/MM/YYYY H','DD/MM/YYYY H:mm','DD/MM/YYYY H:mm:ss','DD/MM/YYYY kk','DD/MM/YYYY kk:mm','DD/MM/YYYY kk:mm:ss',
+        'DD/MM/YYYY','DD/MM/YYYY HH','DD/MM/YYYY HH:mm','DD/MM/YYYY HH:mm:ss','DD/MM/YYYY kk','DD/MM/YYYY kk:mm','DD/MM/YYYY kk:mm:ss',
+        'DD-MM-YYYY','DD-MM-YYYY H','DD-MM-YYYY H:mm','DD-MM-YYYY H:mm:ss','DD-MM-YYYY kk','DD-MM-YYYY kk:mm','DD-MM-YYYY kk:mm:ss',
+        'DD-MM-YYYY','DD-MM-YYYY HH','DD-MM-YYYY HH:mm','DD-MM-YYYY HH:mm:ss','DD-MM-YYYY kk','DD-MM-YYYY kk:mm','DD-MM-YYYY kk:mm:ss']
     },
     {
         display: 'DD-MM-YY 24:00',
-        value: ['DD-MM-YY HH','DD-MM-YY HH:mm','DD-MM-YY HH:mm:ss','DD-MM-YY kk','DD-MM-YY kk:mm','DD-MM-YY kk:mm:ss']
+        value: ['DD/MM/YY','DD/MM/YY H','DD/MM/YY H:mm','DD/MM/YY H:mm:ss','DD/MM/YY kk','DD/MM/YY kk:mm','DD/MM/YY kk:mm:ss',
+        'DD/MM/YY','DD/MM/YY HH','DD/MM/YY HH:mm','DD/MM/YY HH:mm:ss','DD/MM/YY kk','DD/MM/YY kk:mm','DD/MM/YY kk:mm:ss',
+        'DD-MM-YY','DD-MM-YY H','DD-MM-YY H:mm','DD-MM-YY H:mm:ss','DD-MM-YY kk','DD-MM-YY kk:mm','DD-MM-YY kk:mm:ss',
+        'DD-MM-YY','DD-MM-YY HH','DD-MM-YY HH:mm','DD-MM-YY HH:mm:ss','DD-MM-YY kk','DD-MM-YY kk:mm','DD-MM-YY kk:mm:ss']
     },
     {
         display: 'YYYY-MM-DD 12:00 XM',
-        value: ['YYYY/MM/DD hh','YYYY/MM/DD hh:mm','YYYY/MM/DD hh:mm:ss','YYYY/MM/DD hh a','YYYY/MM/DD hh:mm a','YYYY/MM/DD hh:mm:ss a','YYYY/MM/DD hh A','YYYY/MM/DD hh:mm A','YYYY/MM/DD hh:mm:ss A']
+        value: ['YYYY/MM/DD','YYYY/MM/DD h','YYYY/MM/DD h:mm','YYYY/MM/DD h:mm:ss','YYYY/MM/DD h a','YYYY/MM/DD h:mm a','YYYY/MM/DD h:mm:ss a','YYYY/MM/DD h A','YYYY/MM/DD h:mm A','YYYY/MM/DD h:mm:ss A',
+        'YYYY/MM/DD','YYYY/MM/DD hh','YYYY/MM/DD hh:mm','YYYY/MM/DD hh:mm:ss','YYYY/MM/DD hh a','YYYY/MM/DD hh:mm a','YYYY/MM/DD hh:mm:ss a','YYYY/MM/DD hh A','YYYY/MM/DD hh:mm A','YYYY/MM/DD hh:mm:ss A',
+        'YYYY-MM-DD','YYYY-MM-DD h','YYYY-MM-DD h:mm','YYYY-MM-DD h:mm:ss','YYYY-MM-DD h a','YYYY-MM-DD h:mm a','YYYY-MM-DD h:mm:ss a','YYYY-MM-DD h A','YYYY-MM-DD h:mm A','YYYY-MM-DD h:mm:ss A',
+        'YYYY-MM-DD','YYYY-MM-DD hh','YYYY-MM-DD hh:mm','YYYY-MM-DD hh:mm:ss','YYYY-MM-DD hh a','YYYY-MM-DD hh:mm a','YYYY-MM-DD hh:mm:ss a','YYYY-MM-DD hh A','YYYY-MM-DD hh:mm A','YYYY-MM-DD hh:mm:ss A']
     },
     {
         display: 'YYYY-DD-MM 12:00 XM',
-        value: ['YYYY-DD-MM hh','YYYY-DD-MM hh:mm','YYYY-DD-MM hh:mm:ss','YYYY-DD-MM hh a','YYYY-DD-MM hh:mm a','YYYY-DD-MM hh:mm:ss a','YYYY-DD-MM hh A','YYYY-DD-MM hh:mm A','YYYY-DD-MM hh:mm:ss A']
+        value: ['YYYY/DD/MM','YYYY/DD/MM h','YYYY/DD/MM h:mm','YYYY/DD/MM h:mm:ss','YYYY/DD/MM h a','YYYY/DD/MM h:mm a','YYYY/DD/MM h:mm:ss a','YYYY/DD/MM h A','YYYY/DD/MM h:mm A','YYYY/DD/MM h:mm:ss A',
+        'YYYY/DD/MM','YYYY/DD/MM hh','YYYY/DD/MM hh:mm','YYYY/DD/MM hh:mm:ss','YYYY/DD/MM hh a','YYYY/DD/MM hh:mm a','YYYY/DD/MM hh:mm:ss a','YYYY/DD/MM hh A','YYYY/DD/MM hh:mm A','YYYY/DD/MM hh:mm:ss A',
+        'YYYY-DD-MM','YYYY-DD-MM h','YYYY-DD-MM h:mm','YYYY-DD-MM h:mm:ss','YYYY-DD-MM h a','YYYY-DD-MM h:mm a','YYYY-DD-MM h:mm:ss a','YYYY-DD-MM h A','YYYY-DD-MM h:mm A','YYYY-DD-MM h:mm:ss A',
+        'YYYY-DD-MM','YYYY-DD-MM hh','YYYY-DD-MM hh:mm','YYYY-DD-MM hh:mm:ss','YYYY-DD-MM hh a','YYYY-DD-MM hh:mm a','YYYY-DD-MM hh:mm:ss a','YYYY-DD-MM hh A','YYYY-DD-MM hh:mm A','YYYY-DD-MM hh:mm:ss A']
     },
     {
         display: 'YYYY-DD-MM 24:00',
-        value: ['YYYY-DD-MM HH','YYYY-DD-MM HH:mm','YYYY-DD-MM HH:mm:ss','YYYY-DD-MM kk','YYYY-DD-MM kk:mm','YYYY-DD-MM kk:mm:ss']
+        value: ['YYYY/DD/MM','YYYY/DD/MM H','YYYY/DD/MM H:mm','YYYY/DD/MM H:mm:ss','YYYY/DD/MM kk','YYYY/DD/MM kk:mm','YYYY/DD/MM kk:mm:ss',
+        'YYYY/DD/MM','YYYY/DD/MM HH','YYYY/DD/MM HH:mm','YYYY/DD/MM HH:mm:ss','YYYY/DD/MM kk','YYYY/DD/MM kk:mm','YYYY/DD/MM kk:mm:ss',
+        'YYYY-DD-MM','YYYY-DD-MM H','YYYY-DD-MM H:mm','YYYY-DD-MM H:mm:ss','YYYY-DD-MM kk','YYYY-DD-MM kk:mm','YYYY-DD-MM kk:mm:ss',
+        'YYYY-DD-MM','YYYY-DD-MM HH','YYYY-DD-MM HH:mm','YYYY-DD-MM HH:mm:ss','YYYY-DD-MM kk','YYYY-DD-MM kk:mm','YYYY-DD-MM kk:mm:ss']
     },
     {
         display: 'YYYY-MM-DD 24:00',
-        value: ['YYYY-MM-DD HH','YYYY-MM-DD HH:mm','YYYY-MM-DD HH:mm:ss','YYYY-MM-DD kk','YYYY-MM-DD kk:mm','YYYY-MM-DD kk:mm:ss']
+        value: ['YYYY/MM/DD','YYYY/MM/DD H','YYYY/MM/DD H:mm','YYYY/MM/DD H:mm:ss','YYYY/MM/DD kk','YYYY/MM/DD kk:mm','YYYY/MM/DD kk:mm:ss',
+        'YYYY/MM/DD','YYYY/MM/DD HH','YYYY/MM/DD HH:mm','YYYY/MM/DD HH:mm:ss','YYYY/MM/DD kk','YYYY/MM/DD kk:mm','YYYY/MM/DD kk:mm:ss',
+        'YYYY-MM-DD','YYYY-MM-DD H','YYYY-MM-DD H:mm','YYYY-MM-DD H:mm:ss','YYYY-MM-DD kk','YYYY-MM-DD kk:mm','YYYY-MM-DD kk:mm:ss',
+        'YYYY-MM-DD','YYYY-MM-DD HH','YYYY-MM-DD HH:mm','YYYY-MM-DD HH:mm:ss','YYYY-MM-DD kk','YYYY-MM-DD kk:mm','YYYY-MM-DD kk:mm:ss']
     },
 ]
