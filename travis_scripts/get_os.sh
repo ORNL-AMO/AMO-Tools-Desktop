ba5d9d58df781889fe6b2e2ead101b1282103b97
causeyc <47064193+causeyc@users.noreply.github.com>
2020-02-21 09:08:08 -0500

    Update get_os.sh

--- travis_scripts/get_os.sh
+++ travis_scripts/get_os.sh
@@ -2,11 +2,11 @@
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]
 then
-    export OS_NAME="linux"
+    export OS_FLAG="l"
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]
 then
-    export OS_NAME="mac"
+    export OS_FLAG="m"
 elif [[ "$TRAVIS_OS_NAME" == "windows" ]]
 then
-    export OS_NAME="windows"
+    export OS_FLAG="w"
 fi
