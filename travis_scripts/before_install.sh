1d9a6d10e59b95e5a75c34514959b03b431b5140
causeyc <47064193+causeyc@users.noreply.github.com>
2020-03-06 16:01:55 -0500

    Update before_install.sh

--- travis_scripts/before_install.sh
+++ travis_scripts/before_install.sh
@@ -5,7 +5,7 @@ set -ev
 if [[ $TRAVIS_OS_NAME == 'windows' ]]; then
   echo "Skipping 'npm i -g npm@6.4.0' command on Windows"
 else
-  travis_retry npm i -g npm@6.4.0;
+  npm i -g npm@6.4.0;
 fi
 
 exit 0;
