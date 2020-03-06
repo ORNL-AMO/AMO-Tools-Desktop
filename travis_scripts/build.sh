c33c294f304d912cc6e1a60c48816b7743a7540f
causeyc <47064193+causeyc@users.noreply.github.com>
2020-03-06 16:01:00 -0500

    Update build.sh

--- travis_scripts/build.sh
+++ travis_scripts/build.sh
@@ -3,7 +3,7 @@
 set -ev
 
 if [[ $TRAVIS_BRANCH == 'master' ]]; then
-  travis_wait 30 npm run ng-high-memory;
+  npm run ng-high-memory;
   ./node_modules/.bin/build -"$OS_FLAG" --x64 --publish="never";
   ls;
   ls ../output/;
