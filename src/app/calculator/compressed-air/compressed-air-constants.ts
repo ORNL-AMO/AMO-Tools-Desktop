abd440f36f85d96d5cd00b42b01869b6a8dd9015
Colin Causey <causeyc@findlay.edu>
2020-04-03 14:56:00 -0400

    Added compressed-air-constants.ts

---
+++ src/app/calculator/compressed-air/compressed-air-constants.ts
@@ -0,0 +1,82 @@
+export const pipeSizeConstants: Array<{ value: number, display: string }> = [
+    {
+        value: 0,
+        display: '1/2'
+    },
+    {
+        value: 1,
+        display: '3/4'
+    },
+    {
+        value: 2,
+        display: '1'
+    },
+    {
+        value: 3,
+        display: '1 1/4'
+    },
+    {
+        value: 4,
+        display: '1 1/2'
+    },
+    {
+        value: 5,
+        display: '2'
+    },
+    {
+        value: 6,
+        display: '2 1/2'
+    },
+    {
+        value: 7,
+        display: '3'
+    },
+    {
+        value: 8,
+        display: '3 1/2'
+    },
+    {
+        value: 9,
+        display: '4'
+    },
+    {
+        value: 10,
+        display: '5'
+    },
+    {
+        value: 11,
+        display: '6'
+    },
+    {
+        value: 12,
+        display: '8'
+    },
+    {
+        value: 13,
+        display: '10'
+    },
+    {
+        value: 14,
+        display: '12'
+    },
+    {
+        value: 15,
+        display: '14'
+    },
+    {
+        value: 16,
+        display: '16'
+    },
+    {
+        value: 17,
+        display: '18'
+    },
+    {
+        value: 18,
+        display: '20'
+    },
+    {
+        value: 19,
+        display: '24'
+    }
+]
