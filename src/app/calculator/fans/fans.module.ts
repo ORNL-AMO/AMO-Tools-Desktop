40dc17cc5dc80befc8d6d11444cc7ef523054244
nbintertech <nblondheim@intertech.com>
2020-06-11 11:13:47 -0500

    Move psychometric calculator routing to general

--- src/app/calculator/fans/fans.module.ts
+++ src/app/calculator/fans/fans.module.ts
@@ -5,14 +5,12 @@ import { FanAnalysisModule } from './fan-analysis/fan-analysis.module';
 import { SystemAndEquipmentCurveModule } from '../system-and-equipment-curve/system-and-equipment-curve.module';
 import { FansListComponent } from './fans-list/fans-list.component';
 import { RouterModule } from '@angular/router';
-import { FanPsychometricModule } from './fan-psychometric/fan-psychometric.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FanEfficiencyModule,
     FanAnalysisModule,
-    FanPsychometricModule,
     SystemAndEquipmentCurveModule,
     RouterModule
   ],
