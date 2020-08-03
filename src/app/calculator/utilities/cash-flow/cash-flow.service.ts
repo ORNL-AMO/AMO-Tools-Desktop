7167126c2b1b16f2890e65ec1856e5074f9c9c9c
nbintertech <nblondheim@intertech.com>
2020-08-03 08:43:13 -0500

    Add Plotly chart to cash flow calc

--- src/app/calculator/utilities/cash-flow/cash-flow.service.ts
+++ src/app/calculator/utilities/cash-flow/cash-flow.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { CashFlowForm } from './cash-flow';
+import { TraceData } from '../../../shared/models/plotting';
 
 @Injectable()
 export class CashFlowService {
@@ -11,4 +12,59 @@ export class CashFlowService {
     this.calculate = new BehaviorSubject<boolean>(true);
   }
 
+  getTrace(name: string, data: Array<number>, years: Array<number>): TraceData {
+    let trace = {
+      x: years,
+      y: data,
+      type: 'bar',
+      name: name,
+      hovertemplate:  `$%{y:.2r}`,
+      showlegend: true,
+    };
+
+    return trace;
+  }
+
+  getEmptyChart() {
+    return {
+      name: 'Cash Flow',
+      data: [],
+      layout: {
+        hovermode: 'closest',
+        barmode: 'relative',
+        legend: {
+          orientation: 'h',
+        },
+        xaxis: {
+          autorange: true,
+          type: 'auto',
+        },
+        yaxis: {
+          autorange: true,
+          type: 'auto',
+          title: {
+            text: 'Value (%)'
+          },
+        },
+        margin: {
+          t: 25,
+          b: 75,
+          l: 75,
+          r: 25
+        }
+      },
+      config: {
+        modeBarButtonsToRemove: ['autoScale2d', 'lasso2d', 'pan2d', 'select2d', 'toggleSpikelines', 'hoverClosestCartesian', 'hoverCompareCartesian'],
+        displaylogo: false,
+        displayModeBar: true,
+        responsive: true
+      },
+    };
+  }
 }
+
+
+export interface Expense {
+  name: string,
+  data: Array<number>
+};
