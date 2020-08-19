3699e3c9adf99cf7da3f6e21e7f5794cb97fdd48
mark-root <mroot@intertech.com>
2020-08-19 13:04:18 -0500

    add load charecteristics help text

--- src/app/motor-inventory/help-panel/load-characteristics-help/load-characteristics-help.component.ts
+++ src/app/motor-inventory/help-panel/load-characteristics-help/load-characteristics-help.component.ts
@@ -10,15 +10,23 @@ export class LoadCharacteristicsHelpComponent implements OnInit {
 
   focusedField: string;
   focusedFieldSub: Subscription;
+  percentValue: number;
   constructor(private motorInventoryService: MotorInventoryService) { }
 
   ngOnInit(): void {
     this.focusedFieldSub = this.motorInventoryService.focusedField.subscribe(val => {
       this.focusedField = val;
+      if (this.focusedField == 'efficiency75' || this.focusedField == 'powerFactor75') {
+        this.percentValue = 75;
+      } else if (this.focusedField == 'efficiency50' || this.focusedField == 'powerFactor50') {
+        this.percentValue = 50;
+      } else if (this.focusedField == 'efficiency25' || this.focusedField == 'powerFactor25') {
+        this.percentValue = 25;
+      }
     });
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.focusedFieldSub.unsubscribe();
   }
 
