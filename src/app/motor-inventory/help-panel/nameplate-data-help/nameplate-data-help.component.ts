42f4daa64d12095262f5e8e33da537d9f14c3164
mark-root <mroot@intertech.com>
2020-08-19 14:48:04 -0500

    add nameplate data help text

--- src/app/motor-inventory/help-panel/nameplate-data-help/nameplate-data-help.component.ts
+++ src/app/motor-inventory/help-panel/nameplate-data-help/nameplate-data-help.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MotorInventoryService } from '../../motor-inventory.service';
 import { Subscription } from 'rxjs';
+import { Settings } from '../../../shared/models/settings';
 @Component({
   selector: 'app-nameplate-data-help',
   templateUrl: './nameplate-data-help.component.html',
@@ -10,9 +11,11 @@ export class NameplateDataHelpComponent implements OnInit {
 
   focusedField: string;
   focusedFieldSub: Subscription;
+  settings: Settings;
   constructor(private motorInventoryService: MotorInventoryService) { }
 
   ngOnInit(): void {
+    this.settings = this.motorInventoryService.settings.getValue();
     this.focusedFieldSub = this.motorInventoryService.focusedField.subscribe(val => {
       this.focusedField = val;
     });
