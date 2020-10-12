bd1c4afd1a63dc915ce91ad20e641a0880cc322e
nbintertech <nblondheim@intertech.com>
2020-10-12 10:10:43 -0500

    Add table for effort payback chart

--- src/app/treasure-hunt/treasure-hunt-report/report-graphs/effort-savings-chart/effort-savings-chart.component.ts
+++ src/app/treasure-hunt/treasure-hunt-report/report-graphs/effort-savings-chart/effort-savings-chart.component.ts
@@ -1,9 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { TreasureHuntResults } from '../../../../shared/models/treasure-hunt';
 
 import * as Plotly from 'plotly.js';
 import { SimpleChart, TraceData } from '../../../../shared/models/plotting';
 import { graphColors } from '../../../../phast/phast-report/report-graphs/graphColors';
+
+export interface ChartOpportunity {
+  curveNumber: number, 
+  pointNumber: number, 
+  isHidden: boolean,
+  name: string,
+  effort: number,
+  savings: number,
+  payback: number,
+  color: string
+};
 
 @Component({
   selector: 'app-effort-savings-chart',
@@ -22,18 +33,20 @@ export class EffortSavingsChartComponent implements OnInit {
   hoverBtnGridLines: boolean = false;
   displayGridLinesTooltip: boolean = false;
 
-  chartCurves: Array<{curveNumber: number, pointNumber: number, isHidden: boolean}> = [];
-  hoverLabels: Array<{curveNumber: number, pointNumber: number, isHidden: boolean}> = [];
+  chartOpportunities: Array<ChartOpportunity> = [];
+  hoverLabels: Array<{curveNumber: number, pointNumber: number}> = [];
   showingHoverLabels: boolean = false;
   effortChartId: string = 'effortChartDiv';
   effortChart: SimpleChart;
+
+  @ViewChild('dataSummaryTable', { static: false }) dataSummaryTable: ElementRef;
+  dataSummaryTableString: any;
   constructor() { }
 
   ngOnInit(): void {
     this.initRenderChart();
   }
 
-  
   initRenderChart() {
     Plotly.purge(this.effortChartId);
 
@@ -90,27 +103,24 @@ export class EffortSavingsChartComponent implements OnInit {
          this.updateVisibleLabels(legendClick);
         });
         chart.on('plotly_relayout', () => {
-          // this.refreshHoverLabels();
           this.showingHoverLabels = false;
         });
       });
   }
 
-  // refreshHoverLabels() {
-  //   if (this.showingHoverLabels) {
-  //     // reset labels on zoom in/out
-  //     Plotly.Fx.hover(this.effortChartId, []);
-  //     Plotly.Fx.hover(this.effortChartId, this.hoverLabels);
-  //   }
-  // }
-
   updateVisibleLabels(legendClick) {
-    this.chartCurves.map(label => {
+    this.chartOpportunities.map(label => {
       if (label.curveNumber == legendClick.curveNumber) {
         label.isHidden = !label.isHidden;
       }
     });
-    this.hoverLabels = this.chartCurves.filter(label => !label.isHidden);
+    this.hoverLabels = this.chartOpportunities.filter(label => !label.isHidden).map(label => {
+      return {
+        curveNumber: label.curveNumber,
+        pointNumber: label.pointNumber
+      }
+    });
+
     if (this.showingHoverLabels) {
       // Match Plotly's 300ms doubleclick timeout
       setTimeout(() => {
@@ -128,6 +138,7 @@ export class EffortSavingsChartComponent implements OnInit {
   buildTrace() {
     this.treasureHuntResults.opportunitySummaries.forEach((summary, index) => {
       if (summary.opportunityCost.implementationEffort) {
+        let currentColor = graphColors[index % graphColors.length];
         let trace: TraceData = {
           x: [summary.payback],
           y: [summary.opportunityCost.implementationEffort],
@@ -140,20 +151,36 @@ export class EffortSavingsChartComponent implements OnInit {
           },
           mode: 'markers',
           marker: {
-            color: [graphColors[index]],
+            color: [currentColor],
             size: [summary.costSavings],
             sizeref: 6,
-            // Plotly default size calc
+            // Plotly default sizeref calc
             // sizeref: 2.0 * Math.max(...sizePayback) / (maxMarkerSize**2),
             sizemin: 10,
             sizemode: 'area',
           },
         }
         this.effortChart.data.push(trace);
-        this.chartCurves.push({curveNumber: index, pointNumber: 0, isHidden: false});
+        this.chartOpportunities.push(
+          {
+            curveNumber: index, 
+            pointNumber: 0, 
+            isHidden: false,
+            name: summary.opportunityName,
+            effort: summary.opportunityCost.implementationEffort,
+            savings: summary.costSavings,
+            payback: summary.payback,
+            color: currentColor
+          }
+        );
       }
     });
-    this.hoverLabels = JSON.parse(JSON.stringify(this.chartCurves));
+    this.hoverLabels = JSON.parse(JSON.stringify(this.chartOpportunities.map(label => {
+      return {
+        curveNumber: label.curveNumber,
+        pointNumber: label.pointNumber
+      }
+    })));
   }
 
    hideTooltip(btnType: string) {
@@ -214,6 +241,10 @@ export class EffortSavingsChartComponent implements OnInit {
     this.effortChart.layout.xaxis.showgrid = !showingGridX;
     this.effortChart.layout.yaxis.showgrid = !showingGridY;
     this.updateChart();
+  }
+
+  updateTableString() {
+    this.dataSummaryTableString = this.dataSummaryTable.nativeElement.innerText;
   }
 
 }
