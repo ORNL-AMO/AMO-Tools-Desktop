0cec8448c972492e23b9f6c188d62a21a0045e2b
mark-root <mroot@intertech.com>
2020-10-13 13:10:27 -0500

    initialize aerator performance form

--- src/app/waste-water/aerator-performance-form/aerator-performance-form.component.ts
+++ src/app/waste-water/aerator-performance-form/aerator-performance-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { WasteWater } from '../../shared/models/waste-water';
+import { WasteWaterService } from '../waste-water.service';
+import { AeratorPerformanceFormService } from './aerator-performance-form.service';
 
 @Component({
   selector: 'app-aerator-performance-form',
@@ -6,10 +10,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aerator-performance-form.component.css']
 })
 export class AeratorPerformanceFormComponent implements OnInit {
+  @Input()
+  isBaseline: boolean;
+  @Input()
+  inSetup: boolean;
+  @Input()
+  selected: boolean;
 
-  constructor() { }
+  //aerator types
+  areatorTypes: Array<{value: number, display: string}> = [
+    {
+      value: 1,
+      display: 'Mechanical Aerator'
+    },
+    {
+      value: 2,
+      display: 'Positive Displacement Blower'
+    },
+    {
+      value: 3,
+      display: 'Centrifugal Blowers'
+    }
+  ]
+
+  form: FormGroup;
+  constructor(private wasteWaterService: WasteWaterService, private aeratorPerformanceFormService: AeratorPerformanceFormService) { }
 
   ngOnInit(): void {
+    let wasteWater: WasteWater = this.wasteWaterService.wasteWater.getValue();
+    this.form = this.aeratorPerformanceFormService.getFormFromObj(wasteWater.baselineData.aeratorPerformanceData);
+    //TODO: Baseline/modification
+    // if(this.isBaseline){
+
+    // }else{
+    //   //TODO: Modification Logic
+    // }
   }
+
+  save() {
+
+
+    //TODO: Baseline/modification
+    // if(this.isBaseline){
+
+    // }else{
+    //   //TODO: Modification Logic
+    // }
+  }
+
+  focusField(str: string){
+
+  }
+  isOperatingDODifferent() { }
+  isAlphaDifferent() { }
+  isBetaDifferent() { }
+  isSOTRDifferent() { }
+  isAerationDifferent() { }
+  isElevationDifferent() { }
+  isOperatingTimeDifferent() { }
+  isTypeAeratorsDifferent() { }
+  isSpeedDifferent() { }
+  isEnergyCostUnitDifferent() { }
+
+
+
+
+
+
 
 }
--- src/app/waste-water/aerator-performance-form/aerator-performance-form.service.ts
+++ src/app/waste-water/aerator-performance-form/aerator-performance-form.service.ts
@@ -1,7 +1,40 @@
 import { Injectable } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AeratorPerformanceData } from '../../shared/models/waste-water';
 
 @Injectable()
 export class AeratorPerformanceFormService {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  getFormFromObj(obj: AeratorPerformanceData): FormGroup{
+    let form: FormGroup = this.formBuilder.group({
+      OperatingDO: [obj.OperatingDO, [Validators.required, Validators.min(0)]],
+      Alpha: [obj.Alpha, [Validators.required, Validators.min(0)]],
+      Beta: [obj.Beta, [Validators.required, Validators.min(0)]],
+      SOTR: [obj.SOTR, [Validators.required, Validators.min(0)]],
+      Aeration: [obj.Aeration, [Validators.required, Validators.min(0)]],
+      Elevation: [obj.Elevation, [Validators.required, Validators.min(0)]],
+      OperatingTime: [obj.OperatingTime, [Validators.required, Validators.min(0)]],
+      TypeAerators: [obj.TypeAerators, [Validators.required, Validators.min(0)]],
+      Speed: [obj.Speed, [Validators.required, Validators.min(0), Validators.max(100)]],
+      EnergyCostUnit: [obj.EnergyCostUnit, [Validators.required, Validators.min(0)]]
+    });
+    return form;
+  }
+
+  getObjFromForm(form: FormGroup): AeratorPerformanceData{
+    return {
+      OperatingDO: form.controls.OperatingDO.value,
+      Alpha: form.controls.Alpha.value,
+      Beta: form.controls.Beta.value,
+      SOTR: form.controls.SOTR.value,
+      Aeration: form.controls.Aeration.value,
+      Elevation: form.controls.Elevation.value,
+      OperatingTime: form.controls.OperatingTime.value,
+      TypeAerators: form.controls.TypeAerators.value,
+      Speed: form.controls.Speed.value,
+      EnergyCostUnit: form.controls.EnergyCostUnit.value
+    }
+  }
 }
