1f3ec891e4bc67e94bb52681f52fb8af82e1c23a
mark-root <mroot@intertech.com>
2020-10-22 12:14:48 -0500

    aerator performance help text

--- src/app/waste-water/results-panel/help-panel/aerator-performance-help/aerator-performance-help.component.ts
+++ src/app/waste-water/results-panel/help-panel/aerator-performance-help/aerator-performance-help.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { WasteWaterService } from '../../../waste-water.service';
-
+import { StandardSOTRValues } from './standardSOTRValues';
 @Component({
   selector: 'app-aerator-performance-help',
   templateUrl: './aerator-performance-help.component.html',
@@ -11,9 +11,12 @@ export class AeratorPerformanceHelpComponent implements OnInit {
 
   focusedField: string;
   focusedFieldSub: Subscription;
+
+  standardSOTRValues: Array<{ label: string, value: number }>;
   constructor(private wasteWaterService: WasteWaterService) { }
 
   ngOnInit(): void {
+    this.standardSOTRValues = StandardSOTRValues;
     this.focusedFieldSub = this.wasteWaterService.focusedField.subscribe(val => {
       this.focusedField = val;
     });
---
+++ src/app/waste-water/results-panel/help-panel/aerator-performance-help/standardSOTRValues.ts
@@ -0,0 +1,62 @@
+export const StandardSOTRValues: Array<{ label: string, value: number }> = [
+    {
+        label: 'Ultra-fine bubble diffusers',
+        value: 3.7
+    },
+    {
+        label: 'Fine bubble diffusers',
+        value: 3.1
+    },
+    {
+        label: 'Medium bubble diffusers',
+        value: 2.4
+    },
+    {
+        label: 'Coarse bubble diffusers',
+        value: 1.9
+    },
+    {
+        label: 'Surface slow speed',
+        value: 3.1
+    },
+    {
+        label: 'Surface slow speed w/ draft tube',
+        value: 2.8
+    },
+    {
+        label: 'Surface high speed',
+        value: 2.7
+    },
+    {
+        label: 'Surface downdraft turbine',
+        value: 2.6
+    },
+    {
+        label: 'Submerged turbine w/ sparger',
+        value: 2.6
+    },
+    {
+        label: 'Submerged impeller',
+        value: 2.6
+    },
+    {
+        label: 'Surface brush and blade',
+        value: 2.7
+    },
+    {
+        label: 'Surface Orbal disc',
+        value: 2.4
+    },
+    {
+        label: 'Static tube system',
+        value: 2.3
+    },
+    {
+        label: 'Jet',
+        value: 2.9
+    },
+    {
+        label: 'Aspirator',
+        value: 2.1
+    },
+]
