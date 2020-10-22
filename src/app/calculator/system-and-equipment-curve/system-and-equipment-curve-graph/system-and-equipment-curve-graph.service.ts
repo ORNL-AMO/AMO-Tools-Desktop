b949461f51454fef704bb993b293ec5ca3babaac
nbintertech <nblondheim@intertech.com>
2020-10-22 14:41:24 -0500

    Add efficiency and power for user selected points

--- src/app/calculator/system-and-equipment-curve/system-and-equipment-curve-graph/system-and-equipment-curve-graph.service.ts
+++ src/app/calculator/system-and-equipment-curve/system-and-equipment-curve-graph/system-and-equipment-curve-graph.service.ts
@@ -104,6 +104,17 @@ export class SystemAndEquipmentCurveGraphService {
     return intersection;
   }
 
+  getSelectedDataPointEfficiency(userDataPoint: SystemCurveDataPoint, equipmentType: string, settings: Settings, isModification: boolean): SystemCurveDataPoint {
+    let baselinePowerPairs = this.systemAndEquipmentCurveService.baselinePowerDataPairs;
+    let systemCurveDataPoint: SystemCurveDataPoint;
+    if (equipmentType == 'fan') {
+      systemCurveDataPoint = this.systemAndEquipmentCurveService.calculateFanEfficiency(baselinePowerPairs, userDataPoint, settings, isModification);
+    } else {
+      systemCurveDataPoint = this.systemAndEquipmentCurveService.calculatePumpEfficiency(baselinePowerPairs, userDataPoint, settings, isModification);
+    }
+    return systemCurveDataPoint;
+  }
+
   getTraceDataFromPoint(selectedPoint: DataPoint): TraceData {
     let trace: TraceData = {
       x: [selectedPoint.x],
@@ -327,5 +338,6 @@ export interface HoverGroupData {
 export interface SystemCurveDataPoint extends DataPoint {
   power?: number,
   efficiency?: number,
-  fluidPower?: number
+  fluidPower?: number,
+  isUserPoint?: boolean
 }
