0569fe66558039ffdffaf3c02ed9eaabf6b65d88
mark-root <mroot@intertech.com>
2020-10-22 12:39:30 -0500

    add initial help text for activated sludge

--- src/app/waste-water/results-panel/help-panel/activated-sludge-help/activated-sludge-help.component.ts
+++ src/app/waste-water/results-panel/help-panel/activated-sludge-help/activated-sludge-help.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { WasteWaterService } from '../../../waste-water.service';
 
 @Component({
   selector: 'app-activated-sludge-help',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivatedSludgeHelpComponent implements OnInit {
 
-  constructor() { }
+  focusedField: string;
+  focusedFieldSub: Subscription;
+  constructor(private wasteWaterService: WasteWaterService) { }
 
   ngOnInit(): void {
+    this.focusedFieldSub = this.wasteWaterService.focusedField.subscribe(val => {
+      this.focusedField = val;
+    });
   }
 
+  ngOnDestroy(){
+    this.focusedFieldSub.unsubscribe();
+  }
 }
