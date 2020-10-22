ea8798eda3db0d11410bd222e75a2743cc1d51d5
mark-root <mroot@intertech.com>
2020-10-22 10:39:27 -0500

    add help components

---
+++ src/app/waste-water/results-panel/help-panel/activated-sludge-help/activated-sludge-help.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activated-sludge-help',
+  templateUrl: './activated-sludge-help.component.html',
+  styleUrls: ['./activated-sludge-help.component.css']
+})
+export class ActivatedSludgeHelpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/waste-water/results-panel/help-panel/aerator-performance-help/aerator-performance-help.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aerator-performance-help',
+  templateUrl: './aerator-performance-help.component.html',
+  styleUrls: ['./aerator-performance-help.component.css']
+})
+export class AeratorPerformanceHelpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
