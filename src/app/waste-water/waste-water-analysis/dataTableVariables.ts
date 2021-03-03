ee523af2954e54f89179f479c069b9b4cedafff6
nbintertech <nblondheim@intertech.com>
2021-03-03 08:48:22 -0600

    Move up MLSS in datavariables

--- src/app/waste-water/waste-water-analysis/dataTableVariables.ts
+++ src/app/waste-water/waste-water-analysis/dataTableVariables.ts
@@ -7,7 +7,14 @@ export interface DataTableVariable {
     selected: boolean
 }
 
-export const DataTableVariables: Array<DataTableVariable> = [{
+export const DataTableVariables: Array<DataTableVariable> = [
+{
+    name: 'MLSS',
+    label: 'MLSS Concentration',
+    metricUnit: 'mg/L',
+    imperialUnit: 'mg/L',
+    selected: true
+},{
     // S - 'soluble' e - 'effluent' 
     name: 'Se',
     label: 'Effluent CBOD5 Concentration (S&#8337;)',
@@ -35,12 +42,6 @@ export const DataTableVariables: Array<DataTableVariable> = [{
 }, {
     name: 'MLVSS',
     label: 'MLVSS Concentration',
-    metricUnit: 'mg/L',
-    imperialUnit: 'mg/L',
-    selected: true
-}, {
-    name: 'MLSS',
-    label: 'MLSS Concentration',
     metricUnit: 'mg/L',
     imperialUnit: 'mg/L',
     selected: true
