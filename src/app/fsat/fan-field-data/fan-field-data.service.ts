4971245616a7906673245a930c7d5976ef0d00aa
mark-root <mroot@intertech.com>
2021-03-16 15:44:03 -0500

    remove specific gravity from fan calculation, hardcode to 1

--- src/app/fsat/fan-field-data/fan-field-data.service.ts
+++ src/app/fsat/fan-field-data/fan-field-data.service.ts
@@ -15,7 +15,7 @@ export class FanFieldDataService {
     }
     let form: FormGroup = this.formBuilder.group({
       operatingHours: [obj.operatingHours, [Validators.required, Validators.min(0), Validators.max(8760)]],
-      flowRate: [obj.flowRate, [Validators.required, GreaterThanValidator.greaterThan(0), Validators.min(0) ]],
+      flowRate: [obj.flowRate, [Validators.required, GreaterThanValidator.greaterThan(0)]],
       inletPressure: [obj.inletPressure, [Validators.required, Validators.max(0)]],
       outletPressure: [obj.outletPressure, [Validators.required, Validators.min(obj.inletPressure)]],
       loadEstimatedMethod: [obj.loadEstimatedMethod, Validators.required],
