e416cc803c0f228fcb6be9b7425b73b7a0960f04
rmroot <mroot@intertech.com>
2021-07-07 15:20:49 -0500

    add profile graphs

--- src/app/compressed-air-assessment/system-profile/system-profile-graphs/system-profile-graphs.component.ts
+++ src/app/compressed-air-assessment/system-profile/system-profile-graphs/system-profile-graphs.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ProfileSummary } from '../../../shared/models/compressed-air-assessment';
+import { CompressedAirAssessmentService } from '../../compressed-air-assessment.service';
+import { SystemProfileService } from '../system-profile.service';
+import * as Plotly from 'plotly.js';
 @Component({
   selector: 'app-system-profile-graphs',
   templateUrl: './system-profile-graphs.component.html',
@@ -7,9 +11,221 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SystemProfileGraphsComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild("airflowGraph", { static: false }) airflowGraph: ElementRef;
+  @ViewChild("powerGraph", { static: false }) powerGraph: ElementRef;
+  @ViewChild("capacityGraph", { static: false }) capacityGraph: ElementRef;
+
+  compressedAirAssessmentSub: Subscription;
+  profileSummary: Array<ProfileSummary>;
+  constructor(private systemProfileService: SystemProfileService, private compressedAirAssessmentService: CompressedAirAssessmentService) { }
 
   ngOnInit(): void {
+    this.compressedAirAssessmentSub = this.compressedAirAssessmentService.compressedAirAssessment.subscribe(val => {
+      this.profileSummary = this.systemProfileService.calculateProfileSummary(val);
+      this.drawCharts();
+    });
   }
 
+  ngOnDestroy() {
+    this.compressedAirAssessmentSub.unsubscribe();
+  }
+
+
+  ngAfterViewInit() {
+    this.drawCharts();
+  }
+
+
+  drawCharts() {
+    this.drawAirflowChart();
+    this.drawPowerChart();
+    this.drawCapacityChart();
+  }
+
+  drawAirflowChart() {
+    if (this.profileSummary && this.airflowGraph) {
+      // let chartData: Array<ProfileChartData> = this.getChartData();
+      let traceData = new Array();
+      this.profileSummary.forEach(compressorProfile => {
+        let trace = {
+          x: compressorProfile.profileSummaryData.map(data => { return data.timeInterval }),
+          y: compressorProfile.profileSummaryData.map(data => { return data.airflow }),
+          type: 'bar',
+          name: compressorProfile.compressorName
+
+        }
+        traceData.push(trace);
+      })
+      var layout = {
+        barmode: 'stack',
+        title: {
+          text: 'System Air Flow',
+          font: {
+            size: 18
+          },
+        },
+        xaxis: {
+          // ticksuffix: '%',
+          // autotick: false,
+          title: {
+            text: 'Hour',
+            font: {
+              size: 16
+            },
+          },
+          automargin: true
+        },
+        yaxis: {
+          // range: [0, 105],
+          // ticksuffix: '%',
+          title: {
+            text: 'Airflow (acfm)',
+            font: {
+              size: 16
+            },
+          },
+          hoverformat: ",.2f",
+          // automargin: true
+        },
+        margin: {
+          t: 20,
+          r: 20
+        },
+        legend: {
+          orientation: "h",
+          y: 1.5
+        }
+      };
+      var config = {
+        responsive: true,
+        displaylogo: false
+      };
+      Plotly.newPlot(this.airflowGraph.nativeElement, traceData, layout, config);
+    }
+
+
+  }
+
+  drawPowerChart() {
+    if (this.profileSummary && this.powerGraph) {
+      // let chartData: Array<ProfileChartData> = this.getChartData();
+      let traceData = new Array();
+      this.profileSummary.forEach(compressorProfile => {
+        let trace = {
+          x: compressorProfile.profileSummaryData.map(data => { return data.timeInterval }),
+          y: compressorProfile.profileSummaryData.map(data => { return data.power }),
+          type: 'bar',
+          name: compressorProfile.compressorName
+
+        }
+        traceData.push(trace);
+      })
+      var layout = {
+        barmode: 'stack',
+        title: {
+          text: 'System Power',
+          font: {
+            size: 18
+          },
+        },
+        xaxis: {
+          // ticksuffix: '%',
+          autotick: false,
+          title: {
+            text: 'Hour',
+            font: {
+              size: 16
+            },
+          },
+          automargin: true
+        },
+        yaxis: {
+          // range: [0, 105],
+          // ticksuffix: '%',
+          title: {
+            text: 'Power (kW)',
+            font: {
+              size: 16
+            },
+          },
+          hoverformat: ",.2f",
+          // automargin: true
+        },
+        margin: {
+          t: 20,
+          r: 20
+        },
+        legend: {
+          orientation: "h",
+          y: 1.5
+        }
+      };
+      var config = {
+        responsive: true,
+        displaylogo: false
+      };
+      Plotly.newPlot(this.powerGraph.nativeElement, traceData, layout, config);
+    }
+  }
+  drawCapacityChart() {
+    if (this.profileSummary && this.capacityGraph) {
+      // let chartData: Array<ProfileChartData> = this.getChartData();
+      let traceData = new Array();
+      this.profileSummary.forEach(compressorProfile => {
+        let trace = {
+          x: compressorProfile.profileSummaryData.map(data => { return data.timeInterval }),
+          y: compressorProfile.profileSummaryData.map(data => { return data.percentCapacity }),
+          type: 'bar',
+          name: compressorProfile.compressorName
+
+        }
+        traceData.push(trace);
+      })
+      var layout = {
+        barmode: 'stack',
+        title: {
+          text: 'System Capacity',
+          font: {
+            size: 18
+          },
+        },
+        xaxis: {
+          // ticksuffix: '%',
+          autotick: false,
+          title: {
+            text: 'Hour',
+            font: {
+              size: 16
+            },
+          },
+          automargin: true
+        },
+        yaxis: {
+          // range: [0, 105],
+          ticksuffix: '%',
+          title: {
+            text: 'Capacity (%)',
+            font: {
+              size: 16
+            },
+          },
+          hoverformat: ",.2f",
+          // automargin: true
+        },
+        margin: {
+          t: 20,
+          r: 20
+        },
+        legend: {
+          orientation: "h",
+          y: 1.5
+        }
+      };
+      var config = {
+        responsive: true,
+        displaylogo: false
+      };
+      Plotly.newPlot(this.capacityGraph.nativeElement, traceData, layout, config);
+    }
+  }
 }
--- src/app/compressed-air-assessment/system-profile/system-profile.service.ts
+++ src/app/compressed-air-assessment/system-profile/system-profile.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { SystemProfileSetup } from '../../shared/models/compressed-air-assessment';
+import { CompressedAirAssessment, CompressorInventoryItem, ProfileSummary, ProfileSummaryData, SystemProfileSetup } from '../../shared/models/compressed-air-assessment';
+import { CompressedAirCalculationService, CompressorCalcResult } from '../compressed-air-calculation.service';
+import * as _ from 'lodash';
 
 @Injectable()
 export class SystemProfileService {
 
 
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private compressedAirCalculationService: CompressedAirCalculationService) { }
 
   getProfileSetupFormFromObj(systemProfileSetup: SystemProfileSetup): FormGroup {
     let form: FormGroup = this.formBuilder.group({
@@ -19,7 +21,7 @@ export class SystemProfileService {
     return form;
   }
 
-  getProfileSetupFromForm(form: FormGroup): SystemProfileSetup{
+  getProfileSetupFromForm(form: FormGroup): SystemProfileSetup {
     return {
       dayType: form.controls.dayType.value,
       numberOfHours: form.controls.numberOfHours.value,
@@ -28,4 +30,53 @@ export class SystemProfileService {
     }
   }
 
+
+  calculateProfileSummary(compressedAirAssessment: CompressedAirAssessment): Array<ProfileSummary> {
+    compressedAirAssessment.systemProfile.profileSummary.forEach(summary => {
+      let compressor: CompressorInventoryItem = compressedAirAssessment.compressorInventoryItems.find(item => { return item.itemId == summary.compressorId });
+      summary.profileSummaryData.forEach(summaryData => {
+        let computeFrom: 1 | 2 | 3;
+        let computeFromVal: number;
+        if (compressedAirAssessment.systemProfile.systemProfileSetup.profileDataType == 'power') {
+          computeFrom = 2;
+          computeFromVal = summaryData.power;
+        } else if (compressedAirAssessment.systemProfile.systemProfileSetup.profileDataType == 'percentCapacity') {
+          computeFrom = 1;
+          computeFromVal = summaryData.percentCapacity;
+        } else if (compressedAirAssessment.systemProfile.systemProfileSetup.profileDataType == 'airflow') {
+          computeFrom = 3;
+          computeFromVal = summaryData.airflow;
+        }
+        let calcResult: CompressorCalcResult = this.compressedAirCalculationService.compressorsCalc(compressor, computeFrom, computeFromVal);
+        summaryData.airflow = calcResult.capacityCalculated;
+        summaryData.power = calcResult.powerCalculated;
+        summaryData.percentCapacity = calcResult.percentageCapacity;
+        summaryData.percentPower = calcResult.percentagePower;
+      });
+    });
+    return compressedAirAssessment.systemProfile.profileSummary;
+  }
+
+  calculateProfileSummaryTotals(profileSummary: Array<ProfileSummary>): Array<{ airflow: number, power: number, percentCapacity: number, percentPower: number }> {
+    let totals: Array<{ airflow: number, power: number, percentCapacity: number, percentPower: number }> = new Array();
+    let intervals: Array<number> = profileSummary[0].profileSummaryData.map(data => { return data.timeInterval });
+    let allData: Array<ProfileSummaryData> = _.flatMap(profileSummary, (summary) => { return summary.profileSummaryData });
+
+    intervals.forEach(interval => {
+      let filteredData: Array<ProfileSummaryData> = allData.filter(data => { return data.timeInterval == interval });
+      let totalAirFlow: number = _.sumBy(filteredData, 'airflow');
+      let totalPower: number = _.sumBy(filteredData, 'power');
+      let totalPercentCapacity: number = _.sumBy(filteredData, 'percentCapacity');
+      let percentCapacity: number = totalPercentCapacity / filteredData.length;
+      let totalPercentPower: number = _.sumBy(filteredData, 'percentPower');
+      let percentPower: number = totalPercentPower / filteredData.length;
+      totals.push({
+        airflow: totalAirFlow,
+        power: totalPower,
+        percentCapacity: percentCapacity,
+        percentPower: percentPower
+      });
+    });
+    return totals;
+  }
 }
