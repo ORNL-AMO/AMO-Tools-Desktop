9a6b9731cdff4b3a41bbc5c699de948f84102871
rmroot <mroot@intertech.com>
2021-07-14 10:22:24 -0500

    add new assessment and apply data from data explorer

--- src/app/log-tool/day-type-analysis/day-type-menu/select-assessment-modal/select-assessment-modal.component.ts
+++ src/app/log-tool/day-type-analysis/day-type-menu/select-assessment-modal/select-assessment-modal.component.ts
@@ -8,7 +8,13 @@ import { Assessment } from '../../../../shared/models/assessment';
 import { CompressedAirAssessment } from '../../../../shared/models/compressed-air-assessment';
 import { LogToolDbService } from '../../../log-tool-db.service';
 import { LogToolDbData, LogToolField } from '../../../log-tool-models';
-import { LogToolService } from '../../../log-tool.service';
+import * as _ from 'lodash';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Directory } from '../../../../shared/models/directory';
+import { DirectoryDbService } from '../../../../indexedDb/directory-db.service';
+import { AssessmentService } from '../../../../dashboard/assessment.service';
+import { Settings } from '../../../../shared/models/settings';
+import { SettingsDbService } from '../../../../indexedDb/settings-db.service';
 
 @Component({
   selector: 'app-select-assessment-modal',
@@ -23,14 +29,20 @@ export class SelectAssessmentModalComponent implements OnInit {
   @ViewChild('selectAssessmentModal', { static: false }) public selectAssessmentModal: ModalDirective;
 
   compressedAirAssessments: Array<Assessment>;
-
+  directories: Array<Directory>
+  addNewAssessment: boolean = false;
+  newAssessmentForm: FormGroup;
   constructor(private assessmentDbService: AssessmentDbService, private logToolDbService: LogToolDbService,
-    private indexedDbService: IndexedDbService, private router: Router, 
-    private compressedAirAssessmentService: CompressedAirAssessmentService, private logToolService: LogToolService) { }
+    private indexedDbService: IndexedDbService, private router: Router,
+    private compressedAirAssessmentService: CompressedAirAssessmentService, private directoryDbService: DirectoryDbService,
+    private formBuilder: FormBuilder, private assessmentService: AssessmentService, private settingsDbService: SettingsDbService) { }
 
   ngOnInit(): void {
+    this.directories = this.directoryDbService.getAll();
     let allAssessments: Array<Assessment> = this.assessmentDbService.getAll();
     this.compressedAirAssessments = allAssessments.filter(assessment => { return assessment.type == "CompressedAir" });
+    this.compressedAirAssessments = _.orderBy(this.compressedAirAssessments, 'modifiedDate');
+    this.initForm();
   }
 
 
@@ -51,6 +63,14 @@ export class SelectAssessmentModalComponent implements OnInit {
     this.close.emit(true);
   }
 
+  initForm() {
+    this.newAssessmentForm = this.formBuilder.group({
+      'assessmentName': ['New Assessment', Validators.required],
+      'assessmentType': ['CompressedAir', Validators.required],
+      'directoryId': [1, Validators.required]
+    });
+    this.newAssessmentForm.controls.assessmentType.disable();
+  }
 
   selectAssessment(assessment: Assessment) {
     assessment.compressedAirAssessment = this.setDayTypesFromLogTool(assessment.compressedAirAssessment, this.logToolDbService.logToolDbData[0])
@@ -63,19 +83,18 @@ export class SelectAssessmentModalComponent implements OnInit {
     })
   }
 
-  setDayTypesFromLogTool(compressedAirAssessment: CompressedAirAssessment, logToolDbData: LogToolDbData): CompressedAirAssessment{
+  setDayTypesFromLogTool(compressedAirAssessment: CompressedAirAssessment, logToolDbData: LogToolDbData): CompressedAirAssessment {
     let logToolFields: Array<LogToolField> = new Array();
 
     logToolDbData.setupData.fields.forEach(field => {
-      if(!field.isDateField && field.useField){
+      if (!field.isDateField && field.useField) {
         logToolFields.push(field);
       }
     });
-    console.log(logToolFields);
     compressedAirAssessment.logToolData = {
-      logToolFields: logToolFields, 
+      logToolFields: logToolFields,
       dayTypeSummaries: logToolDbData.dayTypeData.dayTypeSummaries
-    } 
+    }
     compressedAirAssessment.logToolData.dayTypeSummaries.forEach(summary => {
       delete summary.data
     });
@@ -91,5 +110,42 @@ export class SelectAssessmentModalComponent implements OnInit {
       }
     });
     return compressedAirAssessment;
+  }
+
+  createAssessment() {
+    if (this.newAssessmentForm.valid) {
+      let tmpAssessment = this.assessmentService.getNewAssessment('CompressedAir');
+      tmpAssessment.name = this.newAssessmentForm.controls.assessmentName.value;
+      tmpAssessment.directoryId = this.newAssessmentForm.controls.directoryId.value;
+      let settings: Settings = this.settingsDbService.getByDirectoryId(tmpAssessment.directoryId);
+      tmpAssessment.compressedAirAssessment = this.assessmentService.getNewCompressedAirAssessment(settings);
+      // this.addAssessment(tmpAssessment, '/compressed-air/');
+      this.indexedDbService.addAssessment(tmpAssessment).then(id => {
+        tmpAssessment.id = id;
+        this.selectAssessment(tmpAssessment);
+      });
+    }
+  }
+
+  showAddNewAssessment() {
+    this.addNewAssessment = true;
+  }
+
+  hideAddNewAssessment() {
+    this.addNewAssessment = false;
+  }
+
+  getParentDirStr(id: number) {
+    let parentDir = _.find(this.directories, (dir) => { return dir.id === id; });
+    if (parentDir) {
+      let str = parentDir.name + '/';
+      while (parentDir.parentDirectoryId) {
+        parentDir = _.find(this.directories, (dir) => { return dir.id === parentDir.parentDirectoryId; });
+        str = parentDir.name + '/' + str;
+      }
+      return str;
+    } else {
+      return '';
+    }
   }
 }
