788579f014ec05f47f57e8da453fd460d8471efa
rmroot <mroot@intertech.com>
2021-07-15 12:28:01 -0500

    performance points form bug fix

--- src/app/compressed-air-assessment/inventory/performance-points/max-full-flow/max-full-flow.component.ts
+++ src/app/compressed-air-assessment/inventory/performance-points/max-full-flow/max-full-flow.component.ts
@@ -16,7 +16,6 @@ export class MaxFullFlowComponent implements OnInit {
 
   selectedCompressorSub: Subscription;
   form: FormGroup;
-  isFormChange: boolean = false;
   maxFullFlowLabel: string;
 
   showPressureCalc: boolean;
@@ -31,12 +30,8 @@ export class MaxFullFlowComponent implements OnInit {
       if (val) {
         this.selectedCompressor = val;
         this.checkShowCalc();
-        if (this.isFormChange == false) {
-          this.setMaxFullFlowLabel(val.compressorControls.controlType);
-          this.form = this.inventoryService.getPerformancePointFormFromObj(val.performancePoints.maxFullFlow);
-        } else {
-          this.isFormChange = false;
-        }
+        this.setMaxFullFlowLabel(val.compressorControls.controlType);
+        this.form = this.inventoryService.getPerformancePointFormFromObj(val.performancePoints.maxFullFlow);
       }
     });
   }
@@ -54,7 +49,6 @@ export class MaxFullFlowComponent implements OnInit {
     let compressedAirAssessment: CompressedAirAssessment = this.compressedAirAssessmentService.compressedAirAssessment.getValue();
     let compressorIndex: number = compressedAirAssessment.compressorInventoryItems.findIndex(item => { return item.itemId == selectedCompressor.itemId });
     compressedAirAssessment.compressorInventoryItems[compressorIndex] = selectedCompressor;
-    this.isFormChange = true;
     this.compressedAirAssessmentService.updateCompressedAir(compressedAirAssessment);
     this.inventoryService.selectedCompressor.next(selectedCompressor);
   }
