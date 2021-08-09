e456dc663d9f9831d4d77f9e53d5bbc6acbc89d1
rhernandez-intertech <rhernandez@intertech.com>
2021-08-09 09:51:16 -0500

    help text added

--- src/app/calculator/utilities/altitude-correction/altitude-correction-help/altitude-correction-help.component.ts
+++ src/app/calculator/utilities/altitude-correction/altitude-correction-help/altitude-correction-help.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AltitudeCorrectionService } from '../altitude-correction.service';
 
 @Component({
   selector: 'app-altitude-correction-help',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AltitudeCorrectionHelpComponent implements OnInit {
 
-  constructor() { }
+  currentField: string;
+  currentFieldSub: Subscription;
 
-  ngOnInit(): void {
+  constructor(private altitudeCorrectionService: AltitudeCorrectionService) { }
+
+  ngOnInit() {
+    this.currentFieldSub = this.altitudeCorrectionService.currentField.subscribe(value => {
+      this.currentField = value;
+    });
+  }
+
+  ngOnDestroy(){
+    this.currentFieldSub.unsubscribe();
   }
 
 }
