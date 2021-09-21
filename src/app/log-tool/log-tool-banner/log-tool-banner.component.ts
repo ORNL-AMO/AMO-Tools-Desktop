918fdfc41ca8abcd036f6d835fb2cdb5aa8d4fd6
rmroot <mroot@intertech.com>
2021-09-21 14:32:53 -0500

    updated styling modal and button

--- src/app/log-tool/log-tool-banner/log-tool-banner.component.ts
+++ src/app/log-tool/log-tool-banner/log-tool-banner.component.ts
@@ -29,14 +29,16 @@ export class LogToolBannerComponent implements OnInit {
     });
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.dataSubmittedSub.unsubscribe();
     this.dataCleanedSub.unsubscribe();
     this.noDayTypeAnalysisSub.unsubscribe();
   }
 
-  openExportData(){
-    this.logToolService.openExportData.next(true);
+  openExportData() {
+    if (this.dataCleaned) {
+      this.logToolService.openExportData.next(true);
+    }
   }
 
 }
