057609c66ca0dbee607262085fbcdde02f33cf2d
rhernandez-intertech <rhernandez@intertech.com>
2021-10-01 17:01:28 -0500

    Pump Operations and service created

---
+++ src/app/psat/pump-operations/pump-operations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pump-operations',
+  templateUrl: './pump-operations.component.html',
+  styleUrls: ['./pump-operations.component.css']
+})
+export class PumpOperationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
