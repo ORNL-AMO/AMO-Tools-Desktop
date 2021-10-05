09e5911f5911bb280419ca093b35e8110c07326b
rhernandez-intertech <rhernandez@intertech.com>
2021-10-05 15:57:07 -0500

    pump operations help text added

--- src/app/psat/pump-operations/pump-operations.component.ts
+++ src/app/psat/pump-operations/pump-operations.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, Input, SimpleChanges, ElementRef, HostListener } from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap';
+import { PSAT } from '../../shared/models/psat';
+import { Settings } from '../../shared/models/settings';
+import { CompareService } from '../compare.service';
+import { HelpPanelService } from '../help-panel/help-panel.service';
+import { FormGroup, Validators, ValidatorFn } from '@angular/forms';
+import { Assessment } from '../../shared/models/assessment';
+import { PsatWarningService, FieldDataWarnings } from '../psat-warning.service';
+import { PsatService } from '../psat.service';
+import { OperatingHours } from '../../shared/models/operations';
+import { PumpOperationsService } from './pump-operations.service';
 
 @Component({
   selector: 'app-pump-operations',
@@ -6,10 +17,252 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pump-operations.component.css']
 })
 export class PumpOperationsComponent implements OnInit {
+  @Input()
+  psat: PSAT;
+  @Output('saved')
+  saved = new EventEmitter<boolean>();
+  @Input()
+  selected: boolean;
+  @Input()
+  settings: Settings;
+  @Input()
+  baseline: boolean;
+  @Input()
+  inSetup: boolean;
+  @Input()
+  assessment: Assessment;
+  @Input()
+  modificationIndex: number;
 
-  constructor() { }
+  @ViewChild('headToolModal', { static: false }) public headToolModal: ModalDirective;
+  @ViewChild('formElement', { static: false }) formElement: ElementRef;
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.setOpHoursModalWidth();
+  }
 
-  ngOnInit(): void {
+  formWidth: number;
+  showOperatingHoursModal: boolean = false;
+
+  headToolResults: any = {
+    differentialElevationHead: 0.0,
+    differentialPressureHead: 0.0,
+    differentialVelocityHead: 0.0,
+    estimatedSuctionFrictionHead: 0.0,
+    estimatedDischargeFrictionHead: 0.0,
+    pumpHead: 0.0
+  };
+
+  loadEstimateMethods: Array<{ display: string, value: number }> = [
+    {
+      display: 'Power',
+      value: 0
+    },
+    {
+      display: 'Current',
+      value: 1
+    }
+  ];
+
+  psatForm: FormGroup;
+  fieldDataWarnings: FieldDataWarnings;
+  idString: string;
+
+  constructor(private psatService: PsatService, private psatWarningService: PsatWarningService, private compareService: CompareService, private helpPanelService: HelpPanelService, private pumpOperationsService: PumpOperationsService) { }
+
+  ngOnInit() {
+    if (!this.baseline) {
+      this.idString = 'psat_modification_' + this.modificationIndex;
+    }
+    else {
+      this.idString = 'psat_baseline';
+    }
+    this.init();
+    if (!this.selected) {
+      this.disableForm();
+    }
+
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.selected && !changes.selected.isFirstChange()) {
+      if (!this.selected) {
+        this.disableForm();
+      } else {
+        this.enableForm();
+      }
+    }
+    if (changes.modificationIndex && !changes.modificationIndex.isFirstChange()) {
+      this.init();
+    }
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.setOpHoursModalWidth();
+    }, 100)
+  }
+
+  init() {
+    if (!this.psat.inputs.cost_kw_hour) {
+      this.psat.inputs.cost_kw_hour = this.settings.electricityCost;
+    }
+    this.psatForm = this.pumpOperationsService.getFormFromObj(this.psat.inputs, this.baseline, this.psat.inputs.whatIfScenario);
+    this.helpPanelService.currentField.next('operatingHours');
+    this.checkWarnings();
+  }
+
+  disableForm() {
+    this.psatForm.controls.loadEstimatedMethod.disable();
+  }
+
+  enableForm() {
+    this.psatForm.controls.loadEstimatedMethod.enable();
+  }
+
+  focusField(inputName: string) {
+    if (!this.baseline && inputName === 'measuredVoltage') {
+      inputName = 'modMeasuredVoltage';
+    }
+    this.helpPanelService.currentField.next(inputName);
+  }
+
+  save() {
+    this.psat.inputs = this.pumpOperationsService.getPsatInputsFromForm(this.psatForm, this.psat.inputs);
+    this.checkWarnings();
+    this.saved.emit(true);
+  }
+
+  checkWarnings() {
+    this.fieldDataWarnings = this.psatWarningService.checkFieldData(this.psat, this.settings, this.baseline);
+  }
+
+  changeLoadMethod() {
+    let motorAmpsValidators: Array<ValidatorFn> = new Array();
+    let motorKWValidators: Array<ValidatorFn> = new Array();
+
+    if (this.psatForm.controls.loadEstimatedMethod.value == 0) {
+      motorKWValidators = [Validators.required];
+    } else {
+      motorAmpsValidators = [Validators.required];
+    }
+    this.psatForm.controls.motorAmps.setValidators(motorAmpsValidators);
+    this.psatForm.controls.motorAmps.reset(this.psatForm.controls.motorAmps.value);
+    if (this.psatForm.controls.motorAmps.value) {
+      this.psatForm.controls.motorAmps.markAsDirty();
+    }
+
+    this.psatForm.controls.motorKW.setValidators(motorKWValidators);
+    this.psatForm.controls.motorKW.reset(this.psatForm.controls.motorKW.value);
+    if (this.psatForm.controls.motorKW.value) {
+      this.psatForm.controls.motorKW.markAsDirty();
+    }
+    this.save();
+  }
+
+  showHeadToolModal() {
+    if (this.selected) {
+      this.psatService.modalOpen.next(true);
+      this.headToolModal.show();
+    }
+  }
+
+  hideHeadToolModal() {
+    this.psatService.modalOpen.next(true);
+    if (this.psatForm.controls.head.value != this.psat.inputs.head) {
+      this.psatForm.patchValue({
+        head: this.psat.inputs.head
+      })
+    }
+    this.headToolModal.hide();
+  }
+
+  closeOperatingHoursModal() {
+    this.showOperatingHoursModal = false;
+    this.psatService.modalOpen.next(false);
+  }
+
+  openOperatingHoursModal() {
+    this.showOperatingHoursModal = true;
+    this.psatService.modalOpen.next(true);
+  }
+
+  updateOperatingHours(oppHours: OperatingHours) {
+    this.psat.operatingHours = oppHours;
+    this.psatForm.controls.operatingHours.patchValue(oppHours.hoursPerYear);
+    this.save();
+    this.closeOperatingHoursModal();
+  }
+
+  setOpHoursModalWidth() {
+    if (this.formElement.nativeElement.clientWidth) {
+      this.formWidth = this.formElement.nativeElement.clientWidth;
+    }
+  }
+
+  canCompare() {
+    if (this.compareService.baselinePSAT && this.compareService.modifiedPSAT && !this.inSetup) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isOperatingHoursDifferent() {
+    if (this.canCompare()) {
+      return this.compareService.isOperatingHoursDifferent();
+    } else {
+      return false;
+    }
+  }
+  isCostKwhrDifferent() {
+    if (this.canCompare()) {
+      return this.compareService.isCostKwhrDifferent();
+    } else {
+      return false;
+    }
+  }
+  isFlowRateDifferent() {
+    if (this.canCompare()) {
+      return this.compareService.isFlowRateDifferent();
+    } else {
+      return false;
+    }
+  }
+  isHeadDifferent() {
+    if (this.canCompare()) {
+      return this.compareService.isHeadDifferent();
+    } else {
+      return false;
+    }
+  }
+  isLoadEstimationMethodDifferent() {
+    if (this.canCompare()) {
+      return this.compareService.isLoadEstimationMethodDifferent();
+    } else {
+      return false;
+    }
+  }
+  isMotorFieldPowerDifferent() {
+    if (this.canCompare()) {
+      return this.compareService.isMotorFieldPowerDifferent();
+    } else {
+      return false;
+    }
+  }
+  isMotorFieldCurrentDifferent() {
+    if (this.canCompare()) {
+      return this.compareService.isMotorFieldCurrentDifferent();
+    } else {
+      return false;
+    }
+  }
+  isMotorFieldVoltageDifferent() {
+    if (this.canCompare()) {
+      return this.compareService.isMotorFieldVoltageDifferent();
+    } else {
+      return false;
+    }
   }
 
 }
--- src/app/psat/pump-operations/pump-operations.service.ts
+++ src/app/psat/pump-operations/pump-operations.service.ts
@@ -1,9 +1,62 @@
 import { Injectable } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators, ValidatorFn } from '@angular/forms';
+import { PsatInputs } from '../../shared/models/psat';
 @Injectable({
   providedIn: 'root'
 })
 export class PumpOperationsService {
+    constructor(private formBuilder: FormBuilder) { }
 
-  constructor() { }
+  getFormFromObj(psatInputs: PsatInputs, isBaseline: boolean, isWhatIfScenario?: boolean): FormGroup {
+    let loadEstimationMethodValidators: Array<ValidatorFn> = [];
+    let motorKwValidators: Array<ValidatorFn> = [];
+    let motorAmpsValidators: Array<ValidatorFn> = [];
+    let measuredVoltageValidators: Array<ValidatorFn> = [];
+    if (isBaseline || !psatInputs.whatIfScenario) {
+      loadEstimationMethodValidators = [Validators.required];
+      measuredVoltageValidators = [Validators.required];
+      if (psatInputs.load_estimation_method == 0) {
+        motorKwValidators = [Validators.required]
+      } else if (psatInputs.load_estimation_method == 1) {
+        motorAmpsValidators = [Validators.required]
+      }
+    }
+    //TODO: remove eventually. this is here for support in removing operating_fraction from suite v0.3.2
+    if (!psatInputs.operating_hours && psatInputs.operating_fraction) {
+      psatInputs.operating_hours = psatInputs.operating_fraction * 8760;
+    }
+
+    let form: FormGroup = this.formBuilder.group({
+      operatingHours: [psatInputs.operating_hours, [Validators.required, Validators.min(0), Validators.max(8760)]],
+      costKwHr: [psatInputs.cost_kw_hour, [Validators.required, Validators.min(0), Validators.max(1)]],
+      flowRate: [psatInputs.flow_rate, [Validators.required, Validators.min(0)]],
+      head: [psatInputs.head, [Validators.required, Validators.min(0.1)]],
+      loadEstimatedMethod: [psatInputs.load_estimation_method, loadEstimationMethodValidators],
+      motorKW: [psatInputs.motor_field_power, motorKwValidators],
+      motorAmps: [psatInputs.motor_field_current, motorAmpsValidators],
+      measuredVoltage: [psatInputs.motor_field_voltage, measuredVoltageValidators],
+      implementationCosts: [psatInputs.implementationCosts]
+    });
+    for (let key in form.controls) {
+      if (form.controls[key].value) {
+        form.controls[key].markAsDirty();
+      }
+    }
+    return form;
+  }
+
+
+  getPsatInputsFromForm(form: FormGroup, psatInputs: PsatInputs): PsatInputs {
+    psatInputs.operating_hours = form.controls.operatingHours.value;
+    psatInputs.cost_kw_hour = form.controls.costKwHr.value;
+    psatInputs.flow_rate = form.controls.flowRate.value;
+    psatInputs.head = form.controls.head.value;
+    psatInputs.load_estimation_method = form.controls.loadEstimatedMethod.value;
+    psatInputs.motor_field_power = form.controls.motorKW.value;
+    psatInputs.motor_field_current = form.controls.motorAmps.value;
+    psatInputs.motor_field_voltage = form.controls.measuredVoltage.value;
+    psatInputs.implementationCosts = form.controls.implementationCosts.value;
+    return psatInputs;
+
+  }
 }
