36739c158a5a788c243e94bff05ec54c4d539a71
rhernandez-intertech <rhernandez@intertech.com>
2021-10-08 08:58:06 -0500

    need help with smooth drag

--- src/app/shared/calculator-drag-bar/calculator-drag-bar.component.ts
+++ src/app/shared/calculator-drag-bar/calculator-drag-bar.component.ts
@@ -12,11 +12,14 @@ export class CalculatorDragBarComponent implements OnInit {
 
   @HostListener('drag', ['$event'])
   onDrag(event: DragEvent) {
-    // if (event.pageX > 50 && event.pageX < 1000) {
-    //   this.calculatorDragBarService.sidebarX.next(event.pageX);
-    // }
-    this.setSidebarX(event.pageX);
+    if (event.pageX > 50 && event.pageX < 1000) {
+      this.calculatorDragBarService.sidebarX.next(event.pageX);
+    }
+    //this.setSidebarX(event.pageX);
+    this.dragAction = event.pageX;
   }
+
+  dragAction: number;
 
   constructor(private calculatorDragBarService: CalculatorDragBarService) { }
 
@@ -32,17 +35,17 @@ export class CalculatorDragBarComponent implements OnInit {
     }
   }
 
-  // ngAfterViewInit() {
-  //   let sidebarXValue: number = this.calculatorDragBarService.sidebarX.getValue();
-  //   if (sidebarXValue == undefined && this.dragBar != undefined) {
-  //     this.calculatorDragBarService.sidebarX.next(this.dragBar.nativeElement.offsetLeft);
-  //   }
-  //   // let dashboardSidebarX: number = this.calculatorDragBarService.sidebarX.getValue();
-  //   // let offsetPageX: number = pageX - dashboardSidebarX;
-  //   // //adjust these values to make more sense with a calculator form
-  //   // if (offsetPageX > 500 && offsetPageX < 1000) {
-  //   //   this.chillerStagingService.sidebarX.next(offsetPageX);
-  //   // }
-  // }
+  ngAfterViewInit() {
+    // let sidebarXValue: number = this.calculatorDragBarService.sidebarX.getValue();
+    // if (sidebarXValue == undefined && this.dragBar != undefined) {
+    //   this.calculatorDragBarService.sidebarX.next(this.dragBar.nativeElement.offsetLeft);
+    // }
+    let dashboardSidebarX: number = this.calculatorDragBarService.sidebarX.getValue();
+    let offsetPageX: number = this.dragAction - dashboardSidebarX;
+    //adjust these values to make more sense with a calculator form
+    if (offsetPageX > 500 && offsetPageX < 1000) {
+      this.calculatorDragBarService.sidebarX.next(this.dragBar.nativeElement);
+    }
+  }
 
 }
