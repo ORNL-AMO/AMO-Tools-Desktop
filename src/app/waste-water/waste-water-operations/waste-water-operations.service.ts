d39945af6ad22350315381fcda846e8f44fb9251
rhernandez-intertech <rhernandez@intertech.com>
2021-11-10 10:38:16 -0600

    time increment value being sent, fixed crashing problem

--- src/app/waste-water/waste-water-operations/waste-water-operations.service.ts
+++ src/app/waste-water/waste-water-operations/waste-water-operations.service.ts
@@ -10,6 +10,7 @@ export class WasteWaterOperationsService {
   getFormFromObj(obj: WasteWaterOperations): FormGroup{
     let form: FormGroup = this.formBuilder.group({
       MaxDays: [obj.MaxDays, [Validators.required, Validators.min(0)]],
+      TimeIncrement: [obj.TimeIncrement, [Validators.required, Validators.min(0)]],
       operatingMonths: [obj.operatingMonths, [Validators.min(1), Validators.max(12)]],
       EnergyCostUnit: [obj.EnergyCostUnit, [Validators.required, Validators.min(0)]]
     });
@@ -19,6 +20,7 @@ export class WasteWaterOperationsService {
   getObjFromForm(form: FormGroup): WasteWaterOperations{
     return {
       MaxDays: form.controls.MaxDays.value,
+      TimeIncrement: form.controls.TimeIncrement.value,
       operatingMonths: form.controls.operatingMonths.value,
       EnergyCostUnit: form.controls.EnergyCostUnit.value
     }
