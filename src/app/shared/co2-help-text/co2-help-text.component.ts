8c2d6cbd818df504bc635efed3fcee0a7e2936e3
ysintertech <ysakovets@intertech.com>
2022-02-28 12:46:16 -0600

    added co2 help text shared component

---
+++ src/app/shared/co2-help-text/co2-help-text.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Settings } from '../models/settings';
+
+@Component({
+  selector: 'app-co2-help-text',
+  templateUrl: './co2-help-text.component.html',
+  styleUrls: ['./co2-help-text.component.css']
+})
+export class Co2HelpTextComponent implements OnInit {
+
+  @Input() currentField: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
