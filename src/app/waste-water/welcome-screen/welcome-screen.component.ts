d0cdd2559b3940901b113178e90a52c2befd36cf
rmroot <mroot@intertech.com>
2022-03-07 14:36:20 -0600

    init was water welcome screen

---
+++ src/app/waste-water/welcome-screen/welcome-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-screen',
+  templateUrl: './welcome-screen.component.html',
+  styleUrls: ['./welcome-screen.component.css']
+})
+export class WelcomeScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
