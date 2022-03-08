9f5ce95d6abea84d773e22412d8db54f915eae90
rmroot <mroot@intertech.com>
2022-03-07 19:36:17 -0600

    first draft of waste water manual

--- src/app/waste-water/welcome-screen/welcome-screen.component.ts
+++ src/app/waste-water/welcome-screen/welcome-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-welcome-screen',
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome-screen.component.css']
 })
 export class WelcomeScreenComponent implements OnInit {
+  @Output('emitClose')
+  emitClose: EventEmitter<boolean> = new EventEmitter<boolean>(false);
 
+  showWelcomeScreen: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.showWelcomeScreen = true;
+    }, 500)
+  }
+
+
+  closePopUp() {
+    this.showWelcomeScreen = false;
+    setTimeout(() => {
+      this.emitClose.emit(true);
+    }, 1500)
+  }
 }
