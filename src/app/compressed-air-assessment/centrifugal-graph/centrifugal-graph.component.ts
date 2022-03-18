2a3f15b22241a0e11389a121987f0b63472bef18
rhernandez-intertech <rhernandez@intertech.com>
2022-03-18 10:36:49 -0500

    centrifugal graph component added

---
+++ src/app/compressed-air-assessment/centrifugal-graph/centrifugal-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-centrifugal-graph',
+  templateUrl: './centrifugal-graph.component.html',
+  styleUrls: ['./centrifugal-graph.component.css']
+})
+export class CentrifugalGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
