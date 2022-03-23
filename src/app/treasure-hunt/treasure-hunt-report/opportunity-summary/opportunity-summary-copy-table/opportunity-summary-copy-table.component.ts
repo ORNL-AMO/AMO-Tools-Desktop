76ab7694f912e08509e392affd55c9b4ad80be8e
rhernandez-intertech <rhernandez@intertech.com>
2022-03-23 16:17:37 -0500

    TH opprotunity summary export to Excel woorkbook added

--- src/app/treasure-hunt/treasure-hunt-report/opportunity-summary/opportunity-summary-copy-table/opportunity-summary-copy-table.component.ts
+++ src/app/treasure-hunt/treasure-hunt-report/opportunity-summary/opportunity-summary-copy-table/opportunity-summary-copy-table.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input, ChangeDetectorRef } from '@angular/core';
 import { OpportunityCost, OpportunitySummary } from '../../../../shared/models/treasure-hunt';
 import { Settings } from '../../../../shared/models/settings';
+import * as XLSX from 'xlsx';
+import { ModalDirective } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-opportunity-summary-copy-table',
@@ -13,14 +15,22 @@ export class OpportunitySummaryCopyTableComponent implements OnInit {
   @Input()
   settings: Settings;
 
-  @ViewChild('copyTable', { static: false }) copyTable: ElementRef;
-  tableString: any;
-
+  @ViewChild('copyTable', { static: false }) copyTable: ElementRef; 
+  @ViewChild('copyTable2', { static: false }) copyTable2: ElementRef;  
+  @ViewChild('exportModal', { static: false }) public exportModal: ModalDirective; 
+  
+  
+  fileName: string;
   individualOpportunitySummaries: Array<OpportunitySummary>;
-  constructor() { }
+  constructor( private cd: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.individualOpportunitySummaries = new Array();
+  }
+
+  ngAfterViewInit(){
+    this.setIndividualSummaries();
+    this.cd.detectChanges();
   }
 
   setIndividualSummaries() {
@@ -73,8 +83,48 @@ export class OpportunitySummaryCopyTableComponent implements OnInit {
     }
   }
 
+  getEffortToImplement(oppCost: OpportunityCost): number {
+    if (oppCost) {
+      return oppCost.implementationEffort;
+    } else {
+      return 0;
+    }
+  }
+
   updateTableString() {
+    this.setIndividualSummaries();   
+    this.exportToExcel();
+  } 
+
+  exportToExcel() {
+    let tableId1: string = this.copyTable.nativeElement.id;
+    let tableId2: string = this.copyTable2.nativeElement.id;
+    const date = new Date();
+    const dateStr = (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear();
+    if(!this.fileName){
+      this.fileName = dateStr +'_Treasure-Hunt-Tracking.xlsx';
+    }
+    let targetTableElm: HTMLElement = document.getElementById(tableId1);
+    let targetTable2Elm: HTMLElement = document.getElementById(tableId2);
+    let sheet1: XLSX.WorkSheet = XLSX.utils.table_to_sheet(targetTableElm, <XLSX.Table2SheetOpts>{ sheet: "Opportunity Summary" });
+    let sheet2: XLSX.WorkSheet = XLSX.utils.table_to_sheet(targetTable2Elm, <XLSX.Table2SheetOpts>{ sheet: "Project Tracking" });
+    let wb: XLSX.WorkBook = XLSX.utils.book_new();
+    wb.SheetNames = ['Opportunity Summary', 'Project Tracking'];
+    wb.Sheets = {['Opportunity Summary']: sheet1, ['Project Tracking']: sheet2 };
+    
+    XLSX.writeFile(wb, this.fileName + '.xlsx');
+    this.hideExportModal();
+  }
+
+  showExportModal() {
+    const date = new Date();
+    const dateStr = (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear();   
+    this.fileName = dateStr +'_Treasure-Hunt-Tracking.xlsx';
     this.setIndividualSummaries();
-    this.tableString = this.copyTable.nativeElement.innerText;
+    this.exportModal.show();
+  }
+
+  hideExportModal() {
+    this.exportModal.hide();
   }
 }
