dec6b1ab2be644e276222c238f4c031e0aaed29e
rhernandez-intertech <rhernandez@intertech.com>
2022-03-25 10:32:29 -0500

    cleaning up code

--- src/app/compressed-air-assessment/centrifugal-graph/centrifugal-graph.component.ts
+++ src/app/compressed-air-assessment/centrifugal-graph/centrifugal-graph.component.ts
@@ -29,7 +29,7 @@ export class CentrifugalGraphComponent implements OnInit {
   @ViewChild('centrifugalGraph', { static: false }) centrifugalGraph: ElementRef;
 
   selectedCompressorType: number;
-  dataSub: Subscription;
+  compressorInventorySub: Subscription;
   selectedCompressor: CompressorInventoryItem;
   centrifugalSpecifics: CentrifugalSpecifics;
   centrifugalCompressors: Array<CompressorInventoryItem>;
@@ -85,7 +85,7 @@ export class CentrifugalGraphComponent implements OnInit {
       });
       this.drawChart();
     } else {
-      this.dataSub = this.inventoryService.selectedCompressor.subscribe(val => {
+      this.compressorInventorySub = this.inventoryService.selectedCompressor.subscribe(val => {
         this.selectedCompressor = val;
         this.compressedAirAssessment = this.compressedAirAssessmentService.compressedAirAssessment.getValue();
         this.centrifugalSpecifics = this.selectedCompressor.centrifugalSpecifics;
@@ -102,7 +102,7 @@ export class CentrifugalGraphComponent implements OnInit {
 
   ngOnDestroy() {
     if (!this.inReport) {
-      this.dataSub.unsubscribe();
+      this.compressorInventorySub.unsubscribe();
     }
   }
 
@@ -121,100 +121,51 @@ export class CentrifugalGraphComponent implements OnInit {
     if (this.printView) {
       this.showAllCompressors = true;
     }
-    if (this.centrifugalGraph && this.compressedAirAssessment) {
+    if (this.centrifugalGraph && this.compressedAirAssessment && this.showGraph) {
       let traceData: Array<TraceData> = new Array();
-      if (this.showGraph) {
-        if (this.showAllCompressors) {
-          let currentColorIndex: number = -1;
-          this.compressedAirAssessment.compressorInventoryItems.forEach(compressor => {
-            let currentTraceColor: string;
-            if (compressor.nameplateData.compressorType == 6 && compressor.compressorControls.controlType != undefined) {
-              if (currentColorIndex == 9) {
-                currentColorIndex = -1;
-              }
-              currentColorIndex++;
-              currentTraceColor = this.plotlyTraceColors[currentColorIndex];
-              let trace1: TraceData = {
-                x: [compressor.centrifugalSpecifics.surgeAirflow, compressor.centrifugalSpecifics.maxFullLoadCapacity],
-                y: [compressor.centrifugalSpecifics.minFullLoadPressure, compressor.centrifugalSpecifics.maxFullLoadPressure],
-                type: 'scatter',
-                name: compressor.name + ' Surge Limit',
-                hovertemplate: "(Airflow: %{x:.2f} acfm, Pressure: %{y:.2f} psig) <extra></extra>",
-                mode: 'lines+markers',
-                marker: {
-                  size: 12,
-                  symbol: 'star-diamond',
-                },
-                fillcolor: currentTraceColor
-              };
-              traceData.push(trace1);
 
-              currentColorIndex++;
-              currentTraceColor = this.plotlyTraceColors[currentColorIndex];
-
-              let xData: Array<number> = [compressor.centrifugalSpecifics.maxFullLoadCapacity, compressor.nameplateData.fullLoadRatedCapacity, compressor.centrifugalSpecifics.minFullLoadCapacity];
-              let yData: Array<number> = [compressor.centrifugalSpecifics.maxFullLoadPressure, compressor.nameplateData.fullLoadOperatingPressure, compressor.centrifugalSpecifics.minFullLoadPressure];
-              let trace2: TraceData = {
-                x: xData.map(data => { return data }),
-                y: yData.map(data => { return data }),
-                type: 'scatter',
-                name: compressor.name + ' Rated Operating Curve',
-                hovertemplate: "(Airflow: %{x:.2f} acfm, Pressure: %{y:.2f} psig) <extra></extra>",
-                mode: 'lines+markers',
-                line: {
-                  shape: 'spline',
-                  dash: 'dot',
-                  color: currentTraceColor
-                },
-                marker: {
-                  size: 12,
-                  symbol: 'star',
-                },
-                fillcolor: currentTraceColor
-              };
-              traceData.push(trace2);
+      if (this.showAllCompressors) {
+        let currentColorIndex: number = -1;
+        this.compressedAirAssessment.compressorInventoryItems.forEach(compressor => {
+          let currentTraceColor: string;
+          if (compressor.nameplateData.compressorType == 6 && compressor.compressorControls.controlType != undefined) {
+            if (currentColorIndex == 9) {
+              currentColorIndex = -1;
             }
-          });
+            currentColorIndex++;
+            currentTraceColor = this.plotlyTraceColors[currentColorIndex];
 
-        } else {
-          let trace1: TraceData = {
-            x: [this.centrifugalSpecifics.surgeAirflow, this.centrifugalSpecifics.maxFullLoadCapacity],
-            y: [this.centrifugalSpecifics.minFullLoadPressure, this.centrifugalSpecifics.maxFullLoadPressure],
-            type: 'scatter',
-            name: this.selectedCompressor.name + ' Surge Limit',
-            hovertemplate: "(Airflow: %{x:.2f} acfm, Pressure: %{y:.2f} psig) <extra></extra>",
-            mode: 'lines+markers',
-            marker: {
-              size: 12,
-              symbol: 'star-diamond',
-            },
-            fillcolor: '#1f77b4'
-          };
-          traceData.push(trace1);
+            let xData1: Array<number> = [compressor.centrifugalSpecifics.surgeAirflow, compressor.centrifugalSpecifics.maxFullLoadCapacity];
+            let yData1: Array<number> = [compressor.centrifugalSpecifics.minFullLoadPressure, compressor.centrifugalSpecifics.maxFullLoadPressure];
+            let name: string = compressor.name + ' Surge Limit';
+            let trace1: TraceData = this.getTrace(xData1, yData1, name, currentTraceColor, 'soild', 'star-diamond');
+            traceData.push(trace1);
 
-          let xData: Array<number> = [this.centrifugalSpecifics.maxFullLoadCapacity, this.selectedCompressor.nameplateData.fullLoadRatedCapacity, this.centrifugalSpecifics.minFullLoadCapacity];
-          let yData: Array<number> = [this.centrifugalSpecifics.maxFullLoadPressure, this.selectedCompressor.nameplateData.fullLoadOperatingPressure, this.centrifugalSpecifics.minFullLoadPressure];
-          let trace2: TraceData = {
-            x: xData.map(data => { return data }),
-            y: yData.map(data => { return data }),
-            type: 'scatter',
-            name: this.selectedCompressor.name + ' Rated Operating Curve',
-            hovertemplate: "(Airflow: %{x:.2f} acfm, Pressure: %{y:.2f} psig) <extra></extra>",
-            mode: 'lines+markers',
-            line: {
-              shape: 'spline',
-              dash: 'dot',
-              color: '#ff7f0e'
-            },
-            marker: {
-              size: 12,
-              symbol: 'star',
-            },
-            fillcolor: '#ff7f0e'
-          };
-          traceData.push(trace2);
-        }
+            currentColorIndex++;
+            currentTraceColor = this.plotlyTraceColors[currentColorIndex];
+
+            let xData2: Array<number> = [compressor.centrifugalSpecifics.maxFullLoadCapacity, compressor.nameplateData.fullLoadRatedCapacity, compressor.centrifugalSpecifics.minFullLoadCapacity];
+            let yData2: Array<number> = [compressor.centrifugalSpecifics.maxFullLoadPressure, compressor.nameplateData.fullLoadOperatingPressure, compressor.centrifugalSpecifics.minFullLoadPressure];
+            let name2: string = compressor.name + ' Rated Operating Curve';
+            let trace2: TraceData = this.getTrace(xData2, yData2, name2, currentTraceColor, 'dot', 'star');
+            traceData.push(trace2);
+          }
+        });
+
+      } else {
+        let xData1: Array<number> = [this.centrifugalSpecifics.surgeAirflow, this.centrifugalSpecifics.maxFullLoadCapacity];
+        let yData1: Array<number> = [this.centrifugalSpecifics.minFullLoadPressure, this.centrifugalSpecifics.maxFullLoadPressure];
+        let name1: string = this.selectedCompressor.name + ' Surge Limit';
+        let trace1: TraceData = this.getTrace(xData1, yData1, name1, '#1f77b4', 'soild', 'star-diamond');
+        traceData.push(trace1);
+
+        let xData2: Array<number> = [this.centrifugalSpecifics.maxFullLoadCapacity, this.selectedCompressor.nameplateData.fullLoadRatedCapacity, this.centrifugalSpecifics.minFullLoadCapacity];
+        let yData2: Array<number> = [this.centrifugalSpecifics.maxFullLoadPressure, this.selectedCompressor.nameplateData.fullLoadOperatingPressure, this.centrifugalSpecifics.minFullLoadPressure];
+        let name2: string = this.selectedCompressor.name + ' Rated Operating Curve';
+        let trace2: TraceData = this.getTrace(xData2, yData2, name2, '#ff7f0e', 'dot', 'star');
+        traceData.push(trace2);
       }
+
 
 
       var layout = {
@@ -253,9 +204,32 @@ export class CentrifugalGraphComponent implements OnInit {
         displaylogo: false
       };
       this.plotlyService.newPlot(this.centrifugalGraph.nativeElement, traceData, layout, config);
+    } 
 
-    }
 
+  }
+
+  getTrace(xData: Array<number>, yData: Array<number>, name: string, color: string, dash: string, symbol: string): TraceData {
+    let trace: TraceData = {
+      x: xData.map(data => { return data }),
+      y: yData.map(data => { return data }),
+      type: 'scatter',
+      name: name,
+      hovertemplate: "(Airflow: %{x:.2f} acfm, Pressure: %{y:.2f} psig) <extra></extra>",
+      mode: 'lines+markers',
+      line: {
+        shape: 'spline',
+        dash: dash,
+        color: color
+      },
+      marker: {
+        size: 12,
+        symbol: symbol,
+      },
+      fillcolor: color
+    };
+
+    return trace;
 
   }
 
