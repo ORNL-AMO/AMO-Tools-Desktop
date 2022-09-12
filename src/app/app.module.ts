f6375c8876d5493f7a77f11a3ae13db9feeb6fa7
nblondheim <nblondheim@intertech.com>
2022-09-12 07:31:24 -0500

    load wasm script in app module  with app initializer methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { CoreModule } from './core/core.module';
@@ -22,6 +22,53 @@ import { environment } from '../environments/environment';
     }),
   ],
   entryComponents: [AppComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+   {
+    provide: APP_INITIALIZER,
+    useFactory: () => initializeWasmScript,
+    multi: true
+   },
+  ]
 })
-export class AppModule { }
+export class AppModule {}
+
+
+export function loadScriptFromPath(id: string, url: string): Promise<void> {
+  let script = <HTMLScriptElement>document.getElementById(id);
+  if (script) {
+    return Promise.resolve();
+  }
+
+  return new Promise<void>((resolve, reject) => {
+    script = document.createElement("script");
+    document.body.appendChild(script);
+
+    script.onload = () => resolve();
+    script.onerror = (ev: ErrorEvent) => reject(ev.error);
+    script.id = id;
+    script.async = true;
+    script.src = url;
+  });
+}
+
+
+export function initializeWasmScript(): Promise<any> {
+  return new Promise((resolve, reject) => {
+    // Prepare global objects
+    window['dbInstance'] = undefined;
+		window['Module'] = {
+      onRuntimeInitialized: function () {
+        window['dbInstance'] = new window['Module'].SQLite(":memory:", true);
+        console.log('AMO Tools Suite module initialized');
+        resolve(module);
+			}
+		}
+
+    // load client.js, ensure module created before app init
+    loadScriptFromPath('wasmClient', `client.js`)
+      .then(() => {
+        const module = <any> {locateFile: (file: string) => {}};
+      })
+  });
+}
