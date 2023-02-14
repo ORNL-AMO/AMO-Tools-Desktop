1d0acd9dab595c234a1a4aefd7420209b426fe19
nblondheim <nblondheim@intertech.com>
2023-02-14 13:22:42 -0600

    Add web value for measur_platform param

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewEncapsulation, ViewContainerRef } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { environment } from '../environments/environment';
 // declare ga as a function to access the JS code in TS
 declare let gtag: Function;
 
@@ -13,11 +14,16 @@ export class AppComponent {
   private viewContainerRef: ViewContainerRef;
   constructor(viewContainerRef: ViewContainerRef, private router: Router) {
     this.viewContainerRef = viewContainerRef;
+    
     this.router.events.subscribe(event => {
-        if (event instanceof NavigationEnd) {
-          gtag('config', 'G-EEHE8GEBH4', {'page_path': event.urlAfterRedirects});
+      if (event instanceof NavigationEnd) {
+          let path: string = environment.production? event.urlAfterRedirects : 'testing-web'
+          gtag('config', 'G-EEHE8GEBH4', {
+            'page_path': path,
+            'measur_platform': 'web',
+          });
         }
       });
-    }
+  }
 }
 
