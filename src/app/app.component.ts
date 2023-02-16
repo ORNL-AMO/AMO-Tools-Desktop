6e82bbbd555b9e81806d84d4d6cfba86dad7b892
nblondheim <nblondheim@intertech.com>
2023-02-16 10:57:53 -0600

    Add MEASUR app open event, only send config once, don't send gamp when in web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,13 +14,18 @@ export class AppComponent {
   private viewContainerRef: ViewContainerRef;
   constructor(viewContainerRef: ViewContainerRef, private router: Router) {
     this.viewContainerRef = viewContainerRef;
+    // analytics handled through gatg() automatically manages sessions, visits, clicks, etc
+    gtag('config', 'G-EEHE8GEBH4');
+    gtag('event', 'measur_app_open', {
+      'measur_platform': 'measur-web'
+    });
     
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-          let path: string = environment.production? event.urlAfterRedirects : 'testing-web'
-          gtag('config', 'G-EEHE8GEBH4', {
+          let path: string = environment.production? event.urlAfterRedirects : 'testing-web';
+          gtag('event', 'page_view', {
             'page_path': path,
-            'measur_platform': 'web',
+            'measur_platform': 'measur-web',
           });
         }
       });
