b2b970eaaa66cef6edaef8011d8c240a67f8db48
nblondheim <nblondheim@intertech.com>
2024-04-04 08:14:33 -0500

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, ViewEncapsulation, ViewContainerRef } from '@angular/core';
+import { Component, ViewEncapsulation, ViewContainerRef, ApplicationRef } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { environment } from '../environments/environment';
 import { AnalyticsService } from './shared/analytics/analytics.service';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { concat, filter, first, interval } from 'rxjs';
 // declare ga as a function to access the JS code in TS
 declare let gtag: Function;
 
@@ -12,11 +14,12 @@ declare let gtag: Function;
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  private viewContainerRef: ViewContainerRef;
-  constructor(viewContainerRef: ViewContainerRef,
+  constructor(
     private analyticsService: AnalyticsService,
+    private appRef: ApplicationRef,
+    private updates: SwUpdate,
     private router: Router) {
-    this.viewContainerRef = viewContainerRef;
+
     if (environment.production) {
       // analytics handled through gatg() automatically manages sessions, visits, clicks, etc
       gtag('config', 'G-EEHE8GEBH4');
@@ -29,5 +32,69 @@ export class AppComponent {
         }
       });
     }
+
+    updates.versionUpdates.subscribe(evt => {
+      // TODO pwa only
+      switch (evt.type) {
+        case 'VERSION_DETECTED':
+          console.log(`Downloading new app version: ${evt.version.hash}`);
+          break;
+        case 'VERSION_READY':
+          console.log(`Current app version: ${evt.currentVersion.hash}`);
+          console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+          // this.promptUserUpdate();
+          break;
+        case 'VERSION_INSTALLATION_FAILED':
+          console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+          break;
+      }
+    });
+
+    const appIsStable = appRef.isStable.pipe(first((isStable) => isStable === true));
+    const everyThirtySec = interval(1 * 20 * 1000);
+    // const everySixHours = interval(6 * 60 * 60 * 1000);
+    const everySixHoursOnceAppIsStable = concat(appIsStable, everyThirtySec);
+    
+    everySixHoursOnceAppIsStable.subscribe(async () => {
+      try {
+        const updateFound = await updates.checkForUpdate();
+        debugger;
+        console.log(updateFound ? 'A new version is available.' : 'Already on the latest version.');
+      } catch (err) {
+        console.error('Failed to check for updates:', err);
+      }
+    });
+
+    updates.unrecoverable.subscribe((event) => {
+      console.log(
+        'An error occurred that we cannot recover from:\n' +
+          event.reason +
+          '\n\nPlease reload the page.',
+      );
+    });
   }
+
+  ngOnInit() {
+    console.log(window.location.origin);
+    console.log(window.location.href);
+  }
+
+    promptUserUpdate(): void {
+      // Ask user to update
+      this.updates.activateUpdate()
+          .then((success) => {
+            console.log('successful update?', success);
+            // todo need to have current location saved somewhere to reload page
+            // todo below will fail because it will be '/landing-screen'
+
+            window.location.assign('');
+            // window.location.href = '';
+            // window.location.reload();
+            // todo this reload does not work in local dev, but may on the dev server? in prod we can reload from landing page
+          })
+          .catch(error => {
+            console.error('Failed to apply SW updates:', error);
+          });
+  }
+
 }
