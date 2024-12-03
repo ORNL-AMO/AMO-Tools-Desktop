47217f84c18986689ca20c801cf647fbe68db421
nblondheim <nblondheim@intertech.com>
2024-12-03 11:54:28 -0600

    Add new reports to mock zenhub old reports

--- scripts/download-stats.js
+++ scripts/download-stats.js
@@ -7,7 +7,8 @@ const fs = require('fs');
 
 const API_URL_MEASUR = 'https://api.github.com/repos/ORNL-AMO/AMO-Tools-Desktop';
 const API_URL_VERIFI = 'https://api.github.com/repos/ORNL-AMO/VERIFI';
-const MAX_PAGES = 10;
+
+const MAX_PAGES = 20;
 const OUTPUT_DIR = path.join(__dirname, 'download-stats');
 
 // Field name strings
@@ -20,6 +21,68 @@ const Windows = 'Windows';
 const MacZip = 'Mac .ZIP';
 const MacDMG = 'Mac .DMG';
 
+
+const Repos = [
+    {
+        name: 'AMO-Tools-Desktop',
+        labels: [
+            "CO2 Emissions",
+            "Compressed Air",
+            "Data Explorer",
+            "Databases",
+            "Fans",
+            "Motor Inventory",
+            "Motors",
+            "Process Cooling",
+            "Process Heating",
+            "Pumps",
+            "Steam",
+            "Treasure Hunt",
+            "Waste Water",
+            "Water",
+            "Web/WebAssembly",
+        ]
+    },
+    {
+        name: 'AMO-Tools-Suite',
+        labels: [
+            "Compressed Air",
+            "Databases",
+            "Fans",
+            "Motors",
+            "Process Cooling",
+            "Process Heating",
+            "Pumps",
+            "Steam",
+            "Treasure Hunt",
+            "Waste Water",
+            "Water",
+            "Web/WebAssembly",   
+        ]
+    },
+    {
+        name: 'VERIFI',
+        labels: [
+            "CO2 Emissions",
+            "Reports",
+            "Analysis",
+            "Dashboard",
+            "Data Input",
+            "Graphs",
+            "Import/Export",
+            "Regression",
+            "UI/UX",
+            "Units",
+        ]
+    },
+    // {
+    //     name: 'Choose-Your-Own-Adventure',
+    //     labels: [
+    
+    //     ]
+    // }
+]
+
 main();
 
 async function main() {
@@ -30,22 +93,28 @@ async function main() {
     }
 
     const headers = getHeaders();
-    const releases = await getReleases(headers, API_URL_MEASUR, 'raw_MEASUR_data.json');
-    // const releases = require('./download-stats/raw-data.json');
-    const csv = parseReleases(releases);
-    outputCsv(csv, 'MEASUR_downloads.csv');
+    // const releasesMeasur = await getReleases(headers, API_URL_MEASUR, 'raw_MEASUR_data.json');
+    // const releaseMeasurCSV = parseReleases(releasesMeasur);
+    // outputCsv(releaseMeasurCSV, 'MEASUR_downloads.csv');
 
-    const releasesVERIFI = await getReleases(headers, API_URL_VERIFI, 'raw_VERIFI_data.json');
-    // const releases = require('./download-stats/raw-data.json');
-    const csvVERIFI = parseReleases(releasesVERIFI);
-    outputCsv(csvVERIFI, 'VERIFI_downloads.csv');
+    // const releasesVERIFI = await getReleases(headers, API_URL_VERIFI, 'raw_VERIFI_data.json');
+    // const releaseVerifiCSV = parseReleases(releasesVERIFI);
+    // outputCsv(releaseVerifiCSV, 'VERIFI_downloads.csv');
+
+    // const issues = await getIssues(headers, API_URL_MEASUR, 'raw_MEASUR_issues.json');
+    // const {csvTotals, csvIssues} = parseIssues(issues, 'MEASUR');
+    const {totalsCsv, issuesCsv} = await buildIssueStatsCSVs(headers);
+
+    console.log('finished buildIssueStatsCSVs')
+
+    outputCsv(totalsCsv, 'AMO_total_issues.csv');
+    outputCsv(issuesCsv, 'AMO_issues.csv');
 }
 
 function getHeaders() {
     let headers = {};
-
     if (process.env.git_token) {
-        headers = { 'Authorization': process.env.git_token };
+        headers = { 'Authorization': `Bearer ${process.env.git_token}` };
     }
     else {
         console.log(
@@ -57,6 +126,182 @@ function getHeaders() {
     }
     return headers;
 }
+
+async function getRepoIssuesByLabel(reqHeaders, repoName, label, repoOutputFilePath) {
+    let issues = [];
+    for (let pageNum = 1; pageNum <= MAX_PAGES; pageNum++) {
+        // console.log(`Requesting page ${pageNum} from the GitHub API...`);
+        const URL = `https://api.github.com/repos/ORNL-AMO/${repoName}/issues?state=all&labels=${encodeURIComponent(label)}`;
+        console.log('URL', URL)
+        let response = await getUrl(URL, {
+            params: {
+                page: pageNum,
+                per_page: 100
+            },
+            headers: reqHeaders,
+        });
+        let headers = response.headers;
+        let data = response.data;
+        if (data.length === 0) {
+            console.log('Reached last page.');
+            break;
+        }
+        issues.push(...response.data);
+        printLimits(headers);
+    }
+
+    await fs.writeFileSync(repoOutputFilePath, JSON.stringify(issues), {flag: 'a'});
+    return issues;
+}
+
+async function buildIssueStatsCSVs(headers) {
+    let csvTotalDataRows = [];
+    let csvIssueDataRows = []
+    
+    for await (let repo of Repos) {
+        const repoOutFile = path.join(OUTPUT_DIR, `raw_${repo.name}_issues.json`);
+        if (fs.existsSync(repoOutFile)) {
+            fs.unlinkSync(repoOutFile);
+        }
+
+        const repoTotalRow = {
+            'Repo': repo.name,
+            'Label': 'Total',
+            'Total Issues': 0, 
+            'Total Open': 0,
+            'Total Closed': 0
+        }
+
+        for await (let label of repo.labels) {
+            const issues = await getRepoIssuesByLabel(headers, repo.name, label, repoOutFile);
+            let labelTotalRowData = {
+                'Repo': repo.name,
+                'Label': label,
+                'Total Issues': issues.length,
+                'Total Open': 0,
+                'Total Closed': 0
+            }
+
+            repoTotalRow['Total Issues'] += issues.length;
+            for (let issue of issues) {
+                let issueRowData = {
+                    'Repo': repo.name,
+                    'Label': label,
+                    'Issue Number': issue.number,
+                    'Title': issue.title,
+                    'Status': issue.state.toUpperCase(),
+                };
+                if (issue.state === 'open') {
+                    repoTotalRow['Total Open']++;
+                    labelTotalRowData['Total Open']++;
+                } else if (issue.state === 'closed') {
+                    repoTotalRow['Total Closed']++;
+                    labelTotalRowData['Total Closed']++;
+                }
+                csvIssueDataRows.push(issueRowData);
+            }
+            csvTotalDataRows.push(labelTotalRowData);
+        }
+
+        csvTotalDataRows.push(repoTotalRow);
+    }
+    
+    const columns = ['Repo', 'Label', 'Total Issues', 'Total Open', 'Total Closed'];
+    const issueColumns = ['Repo', 'Label', 'Issue Number', 'Title', 'Status'];
+    const totalsCsv = Papa.unparse(csvTotalDataRows, { columns: columns, quotes: true });
+    const issuesCsv = Papa.unparse(csvIssueDataRows, { columns: issueColumns, quotes: true });
+    return {totalsCsv, issuesCsv};
+}
+
+
+// function parseIssues(issues, repoName) {
+//     const csvTotalDataRows = [];
+//     const csvIssueDataRows = []
+
+//     const repoTotalRow = {
+//         'Repo': repoName,
+//         'Label': 'Total',
+//         'Total Issues': 0, 
+//         'Total Open': 0,
+//         'Total Closed': 0
+//     }
+
+//     for (let measurLabel of MEASURLabels) {
+//         let labelTotalRowData = {
+//             'Repo': repoName,
+//             'Label': measurLabel,
+//             'Total Issues': 0, 
+//             'Total Open': 0,
+//             'Total Closed': 0
+//         }
+
+//         for (let issue of issues) {
+//             let currentlabelIssue = issue.labels.find(label => label.name === measurLabel);
+//             if (currentlabelIssue !== undefined)  {
+//                 repoTotalRow['Total Issues']++;
+//                 labelTotalRowData['Total Issues']++;
+//                 if (issue.state === 'open') {
+//                     repoTotalRow['Total Open']++;
+//                     labelTotalRowData['Total Open']++;
+//                 } else if (issue.state === 'closed') {
+//                     repoTotalRow['Total Closed']++;
+//                     labelTotalRowData['Total Closed']++;
+//                 }
+//                 let issueRowData = {
+//                     'Repo': repoName,
+//                     'Label': measurLabel,
+//                     'Issue Number': issue.number,
+//                     'Title': issue.title,
+//                     'Status': issue.state.toUpperCase(),
+//                 };
+//                 csvIssueDataRows.push(issueRowData);
+//             }
+//         }
+
+//         console.log('label row', labelTotalRowData)
+//         csvTotalDataRows.push(labelTotalRowData);
+//     }
+    
+//     csvTotalDataRows.unshift(repoTotalRow);
+//     const columns = ['Repo', 'Label', 'Total Issues', 'Total Open', 'Total Closed'];
+//     const csvTotals = Papa.unparse(csvTotalDataRows, { columns: columns, quotes: true });
+
+//     const issueColumns = ['Repo', 'Label', 'Issue Number', 'Title', 'Status'];
+//     const csvIssues = Papa.unparse(csvIssueDataRows, { columns: issueColumns, quotes: true });
+
+//     return {csvTotals, csvIssues};
+// }
+
+
+// async function getIssues(reqHeaders, URL, jsonURL) {
+//     let issues = [];
+//     for (let pageNum = 1; pageNum <= MAX_PAGES; pageNum++) {
+//         console.log(`Requesting page ${pageNum} from the GitHub API...`);
+//         let response = await getUrl(URL + `/issues?state=all`, {
+//             params: {
+//                 page: pageNum,
+//                 per_page: 100
+//             },
+//             headers: reqHeaders,
+//         });
+//         let headers = response.headers;
+//         let data = response.data;
+//         if (data.length === 0) {
+//             console.log('Reached last page.');
+//             break;
+//         }
+//         issues.push(...response.data);
+//         printLimits(headers);
+//     }
+
+//     console.log(`Found ${issues.length} issues in total.`);
+//     let outPath = path.join(OUTPUT_DIR, jsonURL);
+//     await fs.writeFileSync(outPath, JSON.stringify(issues));
+//     console.log(`Wrote raw output to ${outPath}`);
+//     return issues;
+// }
+
+
 
 async function getReleases(reqHeaders, URL, jsonURL) {
     let releases = [];
@@ -87,6 +332,7 @@ async function getReleases(reqHeaders, URL, jsonURL) {
     console.log(`Wrote raw output to ${outPath}`);
     return releases;
 }
+
 
 function parseReleases(releases) {
     const jsonData = [];
@@ -159,3 +405,4 @@ async function getUrl(url, params) {
         process.exit(1);
     }
 }
+
