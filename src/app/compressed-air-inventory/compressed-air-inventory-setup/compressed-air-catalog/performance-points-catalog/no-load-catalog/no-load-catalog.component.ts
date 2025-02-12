9fbac9dacccab31d8b5a754aa38b68a89343bb66
Rachel Stone <rhernandez@intertech.com>
2025-02-12 13:10:13 -0600

    no load PP added to catalog

--- src/app/compressed-air-inventory/compressed-air-inventory-setup/compressed-air-catalog/performance-points-catalog/no-load-catalog/no-load-catalog.component.ts
+++ src/app/compressed-air-inventory/compressed-air-inventory-setup/compressed-air-catalog/performance-points-catalog/no-load-catalog/no-load-catalog.component.ts
@@ -1,10 +1,163 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Settings } from '../../../../../shared/models/settings';
+import { UntypedFormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { CompressedAirItem, PerformancePoint } from '../../../../compressed-air-inventory';
+import { CompressedAirInventoryService } from '../../../../compressed-air-inventory.service';
+import { CompressedAirCatalogService } from '../../compressed-air-catalog.service';
+import { PerformancePointsCatalogService } from '../performance-points-catalog.service';
+import { NoLoadCatalogService } from './no-load-catalog.service';
 
 @Component({
-  selector: 'app-no-load-catalog',
+  selector: '[app-no-load-catalog]',
   templateUrl: './no-load-catalog.component.html',
   styleUrl: './no-load-catalog.component.css'
 })
-export class NoLoadCatalogComponent {
+export class NoLoadCatalogComponent implements OnInit {
+
+
+  settingsSub: Subscription;
+  settings: Settings;
+  selectedCompressorSub: Subscription;
+  form: UntypedFormGroup;
+  isFormChange: boolean = false;
+  noLoadLabel: string;
+  //validationMessages: ValidationMessageMap;
+  //warnings: PerformancePointWarnings;
+
+  showPressureCalc: boolean;
+  showAirflowCalc: boolean;
+  showPowerCalc: boolean;
+  selectedCompressor: CompressedAirItem;
+  atmosphericPressure: number;
+  constructor(private performancePointsCatalogService: PerformancePointsCatalogService,
+    private compressedAirCatalogService: CompressedAirCatalogService,
+    private compressedAirInventoryService: CompressedAirInventoryService,
+    private noLoadCatalogService: NoLoadCatalogService) { }
+
+
+
+  ngOnInit(): void {
+    //let compressedAirAssessment: CompressedAirItem = this.compressedAirCatalogService.selectedCompressedAirItem.getValue();
+    //TODO: CA Inventory get atmosphericPressure
+    this.atmosphericPressure = 14.7;
+
+
+    this.settingsSub = this.compressedAirInventoryService.settings.subscribe(val => {
+      this.settings = val;
+    });
+    //this.settings = this.compressedAirAssessmentService.settings.getValue();
+    this.selectedCompressorSub = this.compressedAirCatalogService.selectedCompressedAirItem.subscribe(compressor => {
+      if (compressor) {
+        this.selectedCompressor = compressor;
+        this.checkShowCalc();
+        //this.warnings = this.performancePointsFormService.checkMotorServiceFactorExceededWarning(compressor.performancePoints.fullLoad.power, compressor);
+        if (this.isFormChange == false) {
+          this.setNoLoadLabel(compressor.compressedAirControlsProperties.controlType);
+          this.form = this.performancePointsCatalogService.getPerformancePointFormFromObj(compressor.compressedAirPerformancePointsProperties.noLoad, compressor, 'noLoad')
+          //this.validationMessages = this.performancePointsFormService.validationMessageMap.getValue();
+        } else {
+          this.updateForm(compressor.compressedAirPerformancePointsProperties.noLoad);
+          this.isFormChange = false;
+        }
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.selectedCompressorSub.unsubscribe();
+  }
+
+  save() {
+    this.isFormChange = true;
+    let fullLoad: PerformancePoint = this.performancePointsCatalogService.getPerformancePointObjFromForm(this.form);
+    //TODO: CA Inventory
+    //this.compressedAirDataManagementService.updateFullLoad(fullLoad);
+  }
+
+  focusField(str: string) {
+    this.compressedAirInventoryService.focusedField.next(str);
+  }
+
+  setNoLoadLabel(controlType: number) {
+    if (controlType == 4 || controlType == 5 || controlType == 2 || controlType == 3 || controlType == 8 || controlType == 10) {
+      this.noLoadLabel = "(unloaded)";
+    } else if (controlType == 6) {
+      this.noLoadLabel = "(off)";
+    } else if (controlType == 1) {
+      this.noLoadLabel = "(modulated)";
+    }
+  }
+
+  saveDischargePressure() {
+    this.form.controls.isDefaultPressure.patchValue(false);
+    this.save();
+  }
+
+  saveAirFlow() {
+    this.form.controls.isDefaultAirFlow.patchValue(false);
+    this.save();
+  }
+
+  savePower() {
+    this.form.controls.isDefaultPower.patchValue(false);
+    this.save();
+  }
+
+  checkShowCalc() {
+    if (!this.selectedCompressor.compressedAirPerformancePointsProperties.noLoad.isDefaultAirFlow) {
+      let defaultValue: number = this.noLoadCatalogService.getNoLoadAirFlow(this.selectedCompressor, true);
+      this.showAirflowCalc = (this.selectedCompressor.compressedAirPerformancePointsProperties.noLoad.airflow != defaultValue);
+    } else {
+      this.showAirflowCalc = false;
+    }
+
+    if (!this.selectedCompressor.compressedAirPerformancePointsProperties.noLoad.isDefaultPower) {
+      let defaultValue: number = this.noLoadCatalogService.getNoLoadPower(this.selectedCompressor, true);
+      this.showPowerCalc = (this.selectedCompressor.compressedAirPerformancePointsProperties.noLoad.power != defaultValue);
+    } else {
+      this.showPowerCalc = false;
+    }
+
+    if (!this.selectedCompressor.compressedAirPerformancePointsProperties.noLoad.isDefaultPressure) {
+      let defaultValue: number = this.noLoadCatalogService.getNoLoadPressure(this.selectedCompressor, true, this.settings);
+      this.showPressureCalc = (this.selectedCompressor.compressedAirPerformancePointsProperties.noLoad.dischargePressure != defaultValue);
+    } else {
+      this.showPressureCalc = false;
+    }
+  }
+
+  setAirFlow() {
+    let defaultValue: number = this.noLoadCatalogService.getNoLoadAirFlow(this.selectedCompressor, true);
+    this.form.controls.airflow.patchValue(defaultValue);
+    this.form.controls.isDefaultAirFlow.patchValue(true);
+    this.save();
+  }
+
+  setPower() {
+    let defaultValue: number = this.noLoadCatalogService.getNoLoadPower(this.selectedCompressor, true);
+    this.form.controls.power.patchValue(defaultValue);
+    this.form.controls.isDefaultPower.patchValue(true);
+    this.save();
+  }
+
+  setPressure() {
+    let defaultValue: number = this.noLoadCatalogService.getNoLoadPressure(this.selectedCompressor, true, this.settings);
+    this.form.controls.dischargePressure.patchValue(defaultValue);
+    this.form.controls.isDefaultPressure.patchValue(true);
+    this.save();
+  }
+
+  updateForm(performancePoint: PerformancePoint) {
+    if (performancePoint.airflow != this.form.controls.airflow.value) {
+      this.form.controls.airflow.patchValue(performancePoint.airflow);
+    }
+    if (performancePoint.dischargePressure != this.form.controls.dischargePressure.value) {
+      this.form.controls.dischargePressure.patchValue(performancePoint.dischargePressure);
+    }
+    if (performancePoint.power != this.form.controls.power.value) {
+      this.form.controls.power.patchValue(performancePoint.power);
+    }
+  }
 
 }
--- src/app/compressed-air-inventory/compressed-air-inventory-setup/compressed-air-catalog/performance-points-catalog/no-load-catalog/no-load-catalog.service.ts
+++ src/app/compressed-air-inventory/compressed-air-inventory-setup/compressed-air-catalog/performance-points-catalog/no-load-catalog/no-load-catalog.service.ts
@@ -1,7 +1,75 @@
 import { Injectable } from '@angular/core';
+import { Settings } from '../../../../../shared/models/settings';
+import { CompressedAirItem } from '../../../../compressed-air-inventory';
 
 @Injectable()
 export class NoLoadCatalogService {
 
   constructor() { }
+
+  getNoLoadPressure(selectedCompressor: CompressedAirItem, isDefault: boolean, settings: Settings): number {
+    if (isDefault) {
+      let defaultPressure: number;
+      if (selectedCompressor.nameplateData.compressorType == 6 || selectedCompressor.compressedAirControlsProperties.controlType == 6
+        || selectedCompressor.nameplateData.compressorType == 4 || selectedCompressor.nameplateData.compressorType == 5 ||
+        selectedCompressor.nameplateData.compressorType == 3) {
+        //centrifugal or start/stop or reciprocating or lubricant free
+        defaultPressure = 0
+      } else if (selectedCompressor.compressedAirControlsProperties.controlType == 1) {
+        //without unloading
+        defaultPressure = selectedCompressor.compressedAirPerformancePointsProperties.fullLoad.dischargePressure + selectedCompressor.compressedAirDesignDetailsProperties.modulatingPressureRange;
+      } else {
+        //rest of options
+        defaultPressure = selectedCompressor.compressedAirControlsProperties.unloadSumpPressure;
+      }
+      //TODO: CA Inventory Conversion
+      //return this.convertCompressedAirService.roundPressureForPresentation(defaultPressure, settings);
+      return defaultPressure;
+    } else {
+      return selectedCompressor.compressedAirPerformancePointsProperties.noLoad.dischargePressure;
+    }
+  }
+
+  getNoLoadAirFlow(selectedCompressor: CompressedAirItem, isDefault: boolean): number {
+    if (isDefault) {
+      return 0;
+    } else {
+      return selectedCompressor.compressedAirPerformancePointsProperties.noLoad.airflow;
+    }
+  }
+
+  getNoLoadPower(selectedCompressor: CompressedAirItem, isDefault: boolean): number {
+    if (isDefault) {
+      let defaultPower: number;
+      if (selectedCompressor.compressedAirControlsProperties.controlType == 1) {
+        //without unloading
+        defaultPower = this.calculateNoLoadPowerWithoutUnloading(selectedCompressor);
+      } else if (selectedCompressor.compressedAirControlsProperties.controlType == 6) {
+        //start stop
+        defaultPower = 0
+      } else {
+        defaultPower = this.calculateNoLoadPower(selectedCompressor.compressedAirDesignDetailsProperties.noLoadPowerUL, selectedCompressor.nameplateData.totalPackageInputPower, selectedCompressor.compressedAirDesignDetailsProperties.designEfficiency);
+      }
+      //TODO: CA Inventory Conversion
+      //return this.convertCompressedAirService.roundPowerForPresentation(defaultPower);
+      return defaultPower;
+    } else {
+      return selectedCompressor.compressedAirPerformancePointsProperties.noLoad.power;
+    }
+  }
+
+  calculateNoLoadPower(NoLoadPowerUL: number, TotPackageInputPower: number, designEfficiency: number): number {
+    if (NoLoadPowerUL < 25) {
+      let noLoadPower: number = NoLoadPowerUL * TotPackageInputPower / (NoLoadPowerUL / (NoLoadPowerUL - 25 + 2521.834 / designEfficiency) / designEfficiency) / 10000;
+      return noLoadPower;
+    } else {
+      let noLoadPower: number = NoLoadPowerUL * TotPackageInputPower / 100;
+      return noLoadPower;
+    }
+  }
+
+  //Without unloading
+  calculateNoLoadPowerWithoutUnloading(selectedCompressor: CompressedAirItem): number {
+    return selectedCompressor.compressedAirDesignDetailsProperties.noLoadPowerFM / 100 * selectedCompressor.compressedAirPerformancePointsProperties.fullLoad.power;
+  }
 }
