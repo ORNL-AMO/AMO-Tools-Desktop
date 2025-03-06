cb2f897e560ecc22c7b6309ae7f233a085e8a8e9
nblondheim <nblondheim@intertech.com>
2025-03-06 10:46:57 -0600

    v1.5.10 changes to package, SW .wasm, app.module changes, release notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { SwUpdate } from '@angular/service-worker';
 import { ElectronService } from './electron/electron.service';
 import { AppErrorService } from './shared/errors/app-error.service';
 import { UpdateApplicationService } from './shared/update-application/update-application.service';
+import { MeasurAppError } from './shared/errors/errors';
 // declare ga as a function to access the JS code in TS
 declare let gtag: Function;
 
@@ -46,6 +47,25 @@ export class AppComponent {
     }
 
     if (!this.electronService.isElectron && environment.production) {
+
+      updates.versionUpdates.subscribe((evt) => {
+        switch (evt.type) {
+          case 'VERSION_DETECTED':
+            console.log(`SW VERSION_DETECTED - SW Downloading new app version: ${evt.version.hash}`);
+            break;
+          case 'VERSION_READY':
+            console.log(`SW VERSION READY - Current app version: ${evt.currentVersion.hash}`);
+            console.log(`SW VERSION READY - New app version ready for use: ${evt.latestVersion.hash}`);
+            break;
+          case 'NO_NEW_VERSION_DETECTED':
+            console.log(`SW NO_NEW_VERSION_DETECTED - Current app version: ${evt.version.hash}`);
+          break;
+          case 'VERSION_INSTALLATION_FAILED':
+            console.log(`SW VERSION_INSTALLATION_FAILED - Failed to install app version '${evt.version.hash}': ${evt.error}`);
+            break;
+        }
+      });
+
       const appIsStable = this.appRef.isStable.pipe(first((isStable) => isStable === true));
       const everySixHours = interval(6 * 60 * 60 * 1000);
       const everySixHoursOnceAppIsStable = concat(appIsStable, everySixHours);
@@ -53,6 +73,7 @@ export class AppComponent {
       everySixHoursOnceAppIsStable.subscribe(async () => {
         try {
           const updateFound = await updates.checkForUpdate();
+          console.log('SW checking for updates', updateFound);
           if (updateFound) {
             this.updateApplicationService.webUpdateAvailable.next(true);
           }
@@ -63,12 +84,14 @@ export class AppComponent {
       });
 
       updates.unrecoverable
-      .pipe(catchError(error => this.appErrorService.handleObservableAppError('An error occurred that MEASUR cannot recover from', error)
-      ))
-      .subscribe({
-        next: (resp) => {},
-        error: (error) => {}
-      });
+        .pipe(catchError(error => this.appErrorService.handleObservableAppError('SW version unrecoverable - reload application', error)
+        ))
+        .subscribe({
+          next: (resp) => {
+            new MeasurAppError('SW version unrecoverable - reload application', undefined);
+          },
+          error: (error) => { }
+        });
 
     }
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { environment } from '../environments/environment';
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { ServiceWorkerModule, SwUpdate } from '@angular/service-worker';
 import { MeasurErrorHandler } from './shared/errors/MeasurErrorHandler';
 import { MeasurAppError } from './shared/errors/errors';
 import { AppErrorModule } from './shared/errors/app-error.module';
+import { ElectronService } from './electron/electron.service';
 
 
 @NgModule({
@@ -30,7 +31,8 @@ import { AppErrorModule } from './shared/errors/app-error.module';
    {provide: ErrorHandler, useClass: MeasurErrorHandler},
    {
     provide: APP_INITIALIZER,
-    useFactory: () => initializeWasmScript,
+    useFactory: initializeAppFactory,
+    deps: [ElectronService, SwUpdate],
     multi: true
    },
   ]
@@ -60,27 +62,107 @@ export function loadScriptFromPath(id: string, url: string): Promise<void> {
 }
 
 
-export function initializeWasmScript(): Promise<any> {
-  return new Promise((resolve, reject) => {
-    // Prepare global objects
-    window['dbInstance'] = undefined;
-		window['Module'] = {
-      onRuntimeInitialized: function () {
-        window['dbInstance'] = new window['Module'].SQLite(":memory:", true);
-        console.log('AMO Tools Suite module initialized');
-        resolve(module);
-			},
-      onAbort: function () {
-        new MeasurAppError('Error occured in MEASUR Tools Suite', undefined)
-      } 
-		}
-    // load client.js, ensure module created before app init
-    loadScriptFromPath('wasmClient', `client.js`)
-      .then(() => {
-        const module = <any>{ locateFile: (file: string) => { } };
-      })
-      .catch(err => {
-        reject(new MeasurAppError('Unable to load MEASUR Tools Suite', err))
+
+export function initializeAppFactory(electronService: ElectronService, serviceWorkerUpdates: SwUpdate) {
+  console.log('MEASUR version', environment.version);
+  console.time('initializeAppFactory');
+  
+  return async () => {
+    const MAX_RELOAD_TRIES = 2;
+    const RELOAD_TRIES = 'measur_reload_tries';
+    const RELOAD_REASON = 'measur_reload_reason';
+    const LOAD_ERROR = 'measur_loading_error';
+
+    // * Use for debugging
+    // * prevent infinite reloads
+    // function checkReloadApp(reason: string, err?: any) {
+    //   console.log('--- checkReloadApp');
+    //   const reloads = Number(sessionStorage.getItem(RELOAD_TRIES) || '0');
+    //   console.log('--- reload tries', reloads);
+
+    //   if (reloads < MAX_RELOAD_TRIES) {
+    //     console.log('--- reloading app, setting tries to', reloads + 1);
+    //     sessionStorage.setItem(RELOAD_TRIES, (reloads + 1).toString());
+    //     sessionStorage.setItem(`${RELOAD_REASON}_${reloads + 1}`, reason);
+    //     if (err) {
+    //       sessionStorage.setItem(`${LOAD_ERROR}_${reloads + 1}`, err.toString());
+    //     }
+    //     window.location.reload();
+    //   }
+    // }
+
+    // * Use for debugging
+    // * Turned off due to reload here may be problematic during version change. Currently causing mismatched state/assets
+    // async function checkWebUpdate(): Promise<void> {
+    //   return Promise.resolve();
+    //   return serviceWorkerUpdates.checkForUpdate()
+    //     .then(updateFound => {
+    //       if (updateFound) {
+    //         console.log('SW Updates found, reloading');
+    //         console.log('--- reloading app after SW update found');
+    //         checkReloadApp('sw update'); 
+    //       } else {
+    //         console.log('NO SW Updates found, continue to load script');
+    //       }
+    //       return Promise.resolve();
+    //     })
+    //     .catch(err => {
+    //       console.error('Error checking for SW update', err);
+    //       checkReloadApp('err checking for sw update', err); 
+    //       return Promise.resolve();
+    //     });
+    // }
+
+    function initializeWASMScript() {
+      return new Promise((resolve, reject) => {
+        // * No changes
+        // Prepare global objects 
+        console.log('=== Initializing MEASUR Tools Suite module ===');
+        window['dbInstance'] = undefined;
+        window['Module'] = {
+          onRuntimeInitialized: function () {
+            window['dbInstance'] = new window['Module'].SQLite(":memory:", true);
+            console.log('=== MEASUR Tools Suite module initialized ===');
+            console.timeEnd('initializeAppFactory');
+            resolve(module);
+          },
+          onAbort: function (err) {
+            new MeasurAppError('Error occurred in MEASUR Tools Suite', undefined);
+            // checkReloadApp('error tools suite onabort', err); 
+          }
+        };
+
+        // Load client.js, ensure module created before app init
+        loadScriptFromPath('wasmClient', `client.js`)
+          .then(() => {
+            console.log('=== Loading MEASUR Tools Suite client.js ===');
+            const module = <any>{ locateFile: (file: string) => { } };
+          })
+          .catch(err => {
+            reject(new MeasurAppError('Unable to load MEASUR Tools Suite', err));
+            // checkReloadApp('unable to load tools suite clientjs', err);
+          });
       });
-  });
+    }
+
+    if ((!electronService.isElectron && serviceWorkerUpdates.isEnabled)) {
+      console.log('=== Initialize for production web');
+    } else {
+      console.log('=== Initialize for electron or dev web');
+    }
+    return initializeWASMScript();
+
+    // * Use for debugging
+    // * Turned off due to reload here may be problematic during version change. Currently causing mismatched state/assets
+    // if ((!electronService.isElectron && serviceWorkerUpdates.isEnabled)) {
+    //   console.log('=== Initialize for production web');
+    //   return checkWebUpdate().then(() => {
+    //     console.log('=== Web update checked')
+    //     return initializeWASMScript();
+    //   });
+    // } else {
+    //   console.log('=== Initialize for electron or dev web');
+    //   return initializeWASMScript();
+    // }
+  }
 }
