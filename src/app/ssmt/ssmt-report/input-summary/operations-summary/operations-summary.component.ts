71f7177f46460abc315c675a466647510a8937d7
Rachel Stone <rhernandez@intertech.com>
2025-06-18 11:23:30 -0500

    added copy table to Steam report tables

--- src/app/ssmt/ssmt-report/input-summary/operations-summary/operations-summary.component.ts
+++ src/app/ssmt/ssmt-report/input-summary/operations-summary/operations-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
 import { SSMTInputs, SsmtValid } from '../../../../shared/models/steam/ssmt';
 import { Settings } from '../../../../shared/models/settings';
 
@@ -17,6 +17,9 @@ export class OperationsSummaryComponent implements OnInit {
   settings: Settings;
   @Input()
   printView: boolean;
+
+  @ViewChild('copyTable', { static: false }) copyTable: ElementRef;  
+  copyTableString: any;
 
   collapse: boolean = true;
   numMods: number = 0;
@@ -76,5 +79,9 @@ export class OperationsSummaryComponent implements OnInit {
 
   toggleCollapse() {
     this.collapse = !this.collapse;
+  }
+
+  updateCopyTableString() {
+    this.copyTableString = this.copyTable.nativeElement.innerText;
   }
 }
