11f61abf7cb9ae56dec1fdd34efba2ad3cd715c7
Rachel Stone <rhernandez@intertech.com>
2025-06-24 13:26:32 -0500

    added system dropdown component

---
+++ src/app/compressed-air-inventory/compressed-air-inventory-banner/summary-filter/selected-options/selected-options.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selected-options',
+  templateUrl: './selected-options.component.html',
+  styleUrl: './selected-options.component.css',
+  standalone: false
+})
+export class SelectedOptionsComponent {
+
+}
---
+++ src/app/compressed-air-inventory/compressed-air-inventory-banner/summary-filter/system-dropdown/system-dropdown.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-system-dropdown',
+  templateUrl: './system-dropdown.component.html',
+  styleUrl: './system-dropdown.component.css',
+  standalone: false
+})
+export class SystemDropdownComponent {
+
+}
