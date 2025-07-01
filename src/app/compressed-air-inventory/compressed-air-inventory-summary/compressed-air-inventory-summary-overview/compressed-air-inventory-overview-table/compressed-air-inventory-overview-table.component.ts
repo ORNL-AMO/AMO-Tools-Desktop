24c0a85c8ba764c98793ab9dd00c887474fa9409
Rachel Stone <rhernandez@intertech.com>
2025-07-01 14:12:29 -0500

    CA Inventory sum overview table added w/out data

--- src/app/compressed-air-inventory/compressed-air-inventory-summary/compressed-air-inventory-summary-overview/compressed-air-inventory-overview-table/compressed-air-inventory-overview-table.component.ts
+++ src/app/compressed-air-inventory/compressed-air-inventory-summary/compressed-air-inventory-summary-overview/compressed-air-inventory-overview-table/compressed-air-inventory-overview-table.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Settings } from '../../../../shared/models/settings';
+import { Subscription } from 'rxjs';
+import { CompressedAirInventorySummaryOverviewService, InventorySummary } from '../compressed-air-inventory-summary-overview.service';
 
 @Component({
   selector: 'app-compressed-air-inventory-overview-table',
@@ -6,6 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './compressed-air-inventory-overview-table.component.css',
   standalone: false 
 })
-export class CompressedAirInventoryOverviewTableComponent {
+export class CompressedAirInventoryOverviewTableComponent implements OnInit {
+  @ViewChild('copyTable', { static: false }) copyTable: ElementRef;
+  @Input()
+  settings: Settings;
+  tableString: any;
+  inventorySummary: InventorySummary;
+  inventorySummarySub: Subscription;
+
+  constructor(private compressedAirInventorySummaryOverviewService: CompressedAirInventorySummaryOverviewService) { }
+  
+    ngOnInit(): void {
+      this.inventorySummarySub = this.compressedAirInventorySummaryOverviewService.inventorySummary.subscribe(val => {
+        this.inventorySummary = val;
+      })
+    }
+  
+    ngOnDestroy() {
+      this.inventorySummarySub.unsubscribe();
+    }
+  
+    updateTableString() {
+      this.tableString = this.copyTable.nativeElement.innerText;
+    }
 
 }
--- src/app/compressed-air-inventory/compressed-air-inventory-summary/compressed-air-inventory-summary-overview/compressed-air-inventory-summary-overview.service.ts
+++ src/app/compressed-air-inventory/compressed-air-inventory-summary/compressed-air-inventory-summary-overview/compressed-air-inventory-summary-overview.service.ts
@@ -1,7 +1,37 @@
 import { Injectable } from '@angular/core';
-
+import { BehaviorSubject } from 'rxjs';
+import { PumpInventoryService } from '../../../pump-inventory/pump-inventory.service';
+import { ConvertPumpInventoryService } from '../../../pump-inventory/convert-pump-inventory.service';
+import { PsatService } from '../../../psat/psat.service';
+import { CompressedAirInventoryService } from '../../compressed-air-inventory.service';
+import { ConvertCompressedAirInventoryService } from '../../convert-compressed-air-inventory.service';
+import { CompressedAirAssessmentService } from '../../../compressed-air-assessment/compressed-air-assessment.service';
+import { AssessmentCo2SavingsService } from '../../../shared/assessment-co2-savings/assessment-co2-savings.service';
 @Injectable()
 export class CompressedAirInventorySummaryOverviewService {
+  inventorySummary: BehaviorSubject<InventorySummary>;
 
-  constructor() { }
+  constructor(
+      private compressedAirInventoryService: CompressedAirInventoryService, 
+      private convertCompressedAirInventoryService: ConvertCompressedAirInventoryService,
+      private compressedAirAssessmentService: CompressedAirAssessmentService, 
+      private assessmentCo2SavingsService: AssessmentCo2SavingsService
+  ) {
+    this.inventorySummary = new BehaviorSubject<InventorySummary>({
+      totalEnergyUse: 0,
+      totalEnergyCost: 0,
+      totalCompressors: 0,
+      totalEmissions: 0,
+      //departmentSummaryItems: new Array()
+    });
+  }
+}
+
+
+export interface InventorySummary {
+  totalEnergyUse: number,
+  totalEnergyCost: number,
+  totalCompressors: number,
+  totalEmissions: number,
+  //departmentSummaryItems: Array<DepartmentSummaryItem>
 }
