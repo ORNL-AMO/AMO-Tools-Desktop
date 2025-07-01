c8780103c7691baa825bf6abc9a25fad5b4323ed
Rachel Stone <rhernandez@intertech.com>
2025-07-01 14:34:32 -0500

    CA Inventory sum overview pie chart w/out data

--- src/app/compressed-air-inventory/compressed-air-inventory-summary/compressed-air-inventory-summary-overview/compressed-air-inventory-overview-pie-chart/compressed-air-inventory-overview-pie-chart.component.ts
+++ src/app/compressed-air-inventory/compressed-air-inventory-summary/compressed-air-inventory-summary-overview/compressed-air-inventory-overview-pie-chart/compressed-air-inventory-overview-pie-chart.component.ts
@@ -1,11 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { PlotlyService } from 'angular-plotly.js';
+import { Subscription } from 'rxjs';
+import { CompressedAirInventorySummaryOverviewService, InventorySummary } from '../compressed-air-inventory-summary-overview.service';
 
 @Component({
   selector: 'app-compressed-air-inventory-overview-pie-chart',
   templateUrl: './compressed-air-inventory-overview-pie-chart.component.html',
   styleUrl: './compressed-air-inventory-overview-pie-chart.component.css',
-  standalone: false 
+  standalone: false
 })
-export class CompressedAirInventoryOverviewPieChartComponent {
+export class CompressedAirInventoryOverviewPieChartComponent implements OnInit {
 
+
+  @ViewChild('overviewPieChart', { static: false }) overviewPieChart: ElementRef;
+
+  invetorySummarySub: Subscription;
+  constructor(private inventorySummaryOverviewService: CompressedAirInventorySummaryOverviewService,
+    private plotlyService: PlotlyService) { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.invetorySummarySub = this.inventorySummaryOverviewService.inventorySummary.subscribe(inventorySummary => {
+      let pumpItemData: Array<{ energyCost: number, pumpName: string, color: string }> = this.getCompresedAirItemDataFromSummary(inventorySummary);
+      var data = [{
+        values: pumpItemData.map(val => { return val.energyCost }),
+        labels: pumpItemData.map(val => { return val.pumpName }),
+        marker: {
+          colors: pumpItemData.map(val => { return 'rgb(' + val.color + ')' }),
+          line: {
+            width: pumpItemData.map(val => { return 2 }),
+            color: '#fff'
+          }
+        },
+        type: 'pie',
+        textposition: 'auto',
+        insidetextorientation: "horizontal",
+        textinfo: 'label+value',
+        texttemplate: '%{label}<br>%{value:$,.0f}',
+        hoverinfo: 'label+percent',
+        hovertemplate: '%{percent:%,.2f} <extra></extra>',
+        sort: false,
+        automargin: true
+      }];
+      let layout = {
+        title: {
+          text: 'Annual Energy Cost'
+        },
+        showlegend: false,
+        font: {
+          size: 12,
+        },
+      };
+
+      let configOptions = {
+        modeBarButtonsToRemove: ['hoverClosestPie'],
+        displaylogo: false,
+        displayModeBar: true,
+        responsive: true
+      };
+      this.plotlyService.newPlot(this.overviewPieChart.nativeElement, data, layout, configOptions);
+    })
+  }
+
+  ngOnDestroy() {
+    this.invetorySummarySub.unsubscribe();
+  }
+
+
+  getCompresedAirItemDataFromSummary(inventorySummary: InventorySummary): Array<{ energyCost: number, pumpName: string, color: string }> {
+    let compressedAirItemData = new Array<{ energyCost: number, pumpName: string, color: string }>();
+    inventorySummary.systemSummaryItems.forEach(systemSummaryItem => {
+      systemSummaryItem.pumpItemResults.forEach(pumpItem => {
+        compressedAirItemData.push({
+          energyCost: pumpItem.results.energyCost,
+          pumpName: pumpItem.name,
+          color: systemSummaryItem.systemColor
+        })
+      })
+    });
+    return compressedAirItemData;
+  }
 }
--- src/app/compressed-air-inventory/compressed-air-inventory-summary/compressed-air-inventory-summary-overview/compressed-air-inventory-summary-overview.service.ts
+++ src/app/compressed-air-inventory/compressed-air-inventory-summary/compressed-air-inventory-summary-overview/compressed-air-inventory-summary-overview.service.ts
@@ -7,22 +7,23 @@ import { CompressedAirInventoryService } from '../../compressed-air-inventory.se
 import { ConvertCompressedAirInventoryService } from '../../convert-compressed-air-inventory.service';
 import { CompressedAirAssessmentService } from '../../../compressed-air-assessment/compressed-air-assessment.service';
 import { AssessmentCo2SavingsService } from '../../../shared/assessment-co2-savings/assessment-co2-savings.service';
+import { CompressedAirItem } from '../../compressed-air-inventory';
 @Injectable()
 export class CompressedAirInventorySummaryOverviewService {
   inventorySummary: BehaviorSubject<InventorySummary>;
 
   constructor(
-      private compressedAirInventoryService: CompressedAirInventoryService, 
-      private convertCompressedAirInventoryService: ConvertCompressedAirInventoryService,
-      private compressedAirAssessmentService: CompressedAirAssessmentService, 
-      private assessmentCo2SavingsService: AssessmentCo2SavingsService
+    private compressedAirInventoryService: CompressedAirInventoryService,
+    private convertCompressedAirInventoryService: ConvertCompressedAirInventoryService,
+    private compressedAirAssessmentService: CompressedAirAssessmentService,
+    private assessmentCo2SavingsService: AssessmentCo2SavingsService
   ) {
     this.inventorySummary = new BehaviorSubject<InventorySummary>({
       totalEnergyUse: 0,
       totalEnergyCost: 0,
       totalCompressors: 0,
       totalEmissions: 0,
-      //departmentSummaryItems: new Array()
+      systemSummaryItems: new Array()
     });
   }
 }
@@ -33,5 +34,35 @@ export interface InventorySummary {
   totalEnergyCost: number,
   totalCompressors: number,
   totalEmissions: number,
-  //departmentSummaryItems: Array<DepartmentSummaryItem>
+  systemSummaryItems: Array<SystemSummaryItem>
 }
+
+export interface SystemSummaryItem {
+  systemName: string;
+  numberOfPumps: number;
+  energyUse: number;
+  energyCost: number;
+  systemColor: string;
+  co2EmissionOutput: number,
+  pumpItemResults: Array<{ data: CompressedAirItem, results: PumpInventoryResults, name: string }>;
+}
+
+
+export interface PumpInventoryResults {
+  energyUse: number;
+  emissionOutput?: number;
+  energyCost: number;
+  emissionsOutput: number;
+}
+
+export const SystemRGBColors: Array<string> = [
+  '33, 97, 140',
+  '17, 122, 101',
+  '185, 119, 14',
+  '123, 125, 125',
+  '40, 55, 71',
+  '231, 76, 60',
+  '46, 204, 113',
+  '2, 136, 209',
+  '255, 87, 34',
+]
