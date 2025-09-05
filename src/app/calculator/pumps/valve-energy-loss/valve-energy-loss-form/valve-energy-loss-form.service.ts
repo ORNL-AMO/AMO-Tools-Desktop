de17c0da19f335a0bd647494dd033430e1cb275e
Rachel Stone <rhernandez@intertech.com>
2025-09-05 11:27:00 -0500

    Added form validation for Valve Energy Loss inputs

--- src/app/calculator/pumps/valve-energy-loss/valve-energy-loss-form/valve-energy-loss-form.service.ts
+++ src/app/calculator/pumps/valve-energy-loss/valve-energy-loss-form/valve-energy-loss-form.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { ValveEnergyLossInputs } from '../../../../shared/models/calculators';
 
 @Injectable()
@@ -9,19 +9,19 @@ export class ValveEnergyLossFormService {
 
   initForm(): UntypedFormGroup {
     let formGroup = this.formBuilder.group({
-      'hoursOperation': [0],
-      'electricalRate': [0],
-      'efficiencyPump': [0],
-      'efficiencyMotor': [0],
-      'SG': [0],
-      'flowRate': [0],
-      'upstreamPressure': [0],
+      'hoursOperation': [0, [Validators.required, Validators.min(0), Validators.max(8760)]],
+      'electricalRate': [0, [Validators.required, Validators.min(0)]],
+      'efficiencyPump': [0, [Validators.required, Validators.min(0), Validators.max(100)]],
+      'efficiencyMotor': [0, [Validators.required, Validators.min(0), Validators.max(100)]],
+      'SG': [0, [Validators.required, Validators.max(7), Validators.min(0.9999)]],
+      'flowRate': [0, [Validators.required, Validators.min(0)]],
+      'upstreamPressure': [0, [Validators.required, Validators.min(0), Validators.max(1000)]],
       'upstreamDiameter': [0],
-      'upstreamHeight': [0],
+      'upstreamHeight': [0, Validators.required],
       'valveDiameter': [0],
-      'downstreamPressure': [0],
+      'downstreamPressure': [0, [Validators.required, Validators.min(0), Validators.max(1000)]],
       'downstreamDiameter': [0],
-      'downstreamHeight': [0],
+      'downstreamHeight': [0, Validators.required],
       'pipeSizeFactor': [0],
     });
     return formGroup;
@@ -30,19 +30,19 @@ export class ValveEnergyLossFormService {
 
   getFormFromObj(inputs: ValveEnergyLossInputs): UntypedFormGroup {
     let formGroup = this.formBuilder.group({
-      'hoursOperation': [inputs.hoursOperation],
-      'electricalRate': [inputs.electricalRate],
-      'efficiencyPump': [inputs.efficiencyPump],
-      'efficiencyMotor': [inputs.efficiencyMotor],
-      'SG': [inputs.SG],
-      'flowRate': [inputs.flowRate],
-      'upstreamPressure': [inputs.upstreamPressure],
+      'hoursOperation': [inputs.hoursOperation, [Validators.required, Validators.min(0), Validators.max(8760)]],
+      'electricalRate': [inputs.electricalRate, [Validators.required, Validators.min(0)]],
+      'efficiencyPump': [inputs.efficiencyPump, [Validators.required, Validators.min(0), Validators.max(100)]],
+      'efficiencyMotor': [inputs.efficiencyMotor, [Validators.required, Validators.min(0), Validators.max(100)]],
+      'SG': [inputs.SG, [Validators.required, Validators.max(7), Validators.min(0.9999)]],
+      'flowRate': [inputs.flowRate, [Validators.required, Validators.min(0)]],
+      'upstreamPressure': [inputs.upstreamPressure, [Validators.required, Validators.min(0), Validators.max(1000)]],
       'upstreamDiameter': [inputs.upstreamDiameter],
-      'upstreamHeight': [inputs.upstreamHeight],
+      'upstreamHeight': [inputs.upstreamHeight, Validators.required],
       'valveDiameter': [inputs.valveDiameter],
-      'downstreamPressure': [inputs.downstreamPressure],
+      'downstreamPressure': [inputs.downstreamPressure, [Validators.required, Validators.min(0), Validators.max(inputs.upstreamDiameter)]],
       'downstreamDiameter': [inputs.downstreamDiameter],
-      'downstreamHeight': [inputs.downstreamHeight],
+      'downstreamHeight': [inputs.downstreamHeight, Validators.required],
       'pipeSizeFactor': [inputs.pipeSizeFactor],
     });
     return formGroup;
