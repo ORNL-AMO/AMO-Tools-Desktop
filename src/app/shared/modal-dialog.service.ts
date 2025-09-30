943a840b17ac7b4f8c78cb01bab020eda26524d3
nblondheim <nblondheim@intertech.com>
2025-09-30 14:45:32 -0500

    Update modal dialog service with usage and injector

--- src/app/shared/modal-dialog.service.ts
+++ src/app/shared/modal-dialog.service.ts
@@ -1,29 +1,58 @@
 import { Dialog, DialogConfig, DialogRef } from '@angular/cdk/dialog';
 import { ComponentType } from '@angular/cdk/portal';
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+
+/**
+ * App global modal - render any component as a child of core.component
+ * 
+ * @remarks
+ * Exposes observable to core.component, where it is rendered. 
+ * Pass an injector ref with the target component if it needs service providers
+ * 
+ * @example
+ * Close event in target component  :
+ * ```typescript
+ * import { DialogRef } from '@angular/cdk/dialog';
+ * 
+ *
+ * private dialogRef = inject(DialogRef<AddModificationComponent>);
+ *
+ *  close() {
+ *    this.dialogRef.close();
+ *  }
+ *
+ * 
+ * ```
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class ModalDialogService {
-  private currentModalRef: BehaviorSubject<DialogRef<any, any>> = new BehaviorSubject(undefined);
+  private currentModalRef: BehaviorSubject<DialogRef<any, any> | null> = new BehaviorSubject(null);
 
-  constructor(private dialog: Dialog) {}
+  constructor(private dialog: Dialog) { }
 
   get modalDialogRef$() {
     return this.currentModalRef.asObservable();
   }
 
-
+  /**
+   * Opens modal at core.component 
+   * @param injector - required if modal has services or other injections provided at the module level
+   */
   openModal<T>(
-    component: ComponentType<T>, 
-    config?: DialogConfig<any>
+    component: ComponentType<T>,
+    config?: DialogConfig<any>,
+    injector?: Injector
   ) {
+    // todo we should use a default flexible width
     const dialogRef = this.dialog.open(component, {
       panelClass: 'app-modal-dialog',
       hasBackdrop: true,
       disableClose: false,
+      injector: injector,
       ...config
     });
 
@@ -31,11 +60,12 @@ export class ModalDialogService {
 
     dialogRef.closed.subscribe(() => {
       if (this.currentModalRef.value === dialogRef) {
-        this.currentModalRef.next(undefined);
+        this.currentModalRef.next(null);
       }
     });
+
+    return dialogRef;
   }
 
 }
 
-  
