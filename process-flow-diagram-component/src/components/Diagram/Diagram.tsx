08f9731eeade33eea8c0761a14ba49bcda022c42
nbintertech <60401912+nbintertech@users.noreply.github.com>
2026-01-22 06:52:36 -0800

    Remove commented-out ResultsPanel in Diagram.tsx
    Removed commented-out ResultsPanel component for clarity.

--- process-flow-diagram-component/src/components/Diagram/Diagram.tsx
+++ process-flow-diagram-component/src/components/Diagram/Diagram.tsx
@@ -181,7 +181,6 @@ const Diagram = (props: DiagramProps) => {
       }
 
  {/* // * Only for development result checking */}
-
         {/* <ResultsPanel  style={{
                 left: isMenuDrawerOpen ? drawerOpenOffsetPx : drawerClosedOffsetPx,
                 transition: 'left 0.25s cubic-bezier(0.4, 0, 0.2, 1)'
