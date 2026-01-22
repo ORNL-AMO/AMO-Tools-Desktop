33868017be5ef77d9a1b3ef01c3fc61f191e494b
nbintertech <nblondheim@intertech.com>
2026-01-22 09:26:27 -0600

    Revert "Merge pull request #8106 from ORNL-AMO/hotfix-remove-debugging"
    This reverts commit 961cb3011a34ee18cfbdd3106ffa28f560cf4d72, reversing
    changes made to bcbed23dde9b95ea8f0eb983b4439bbebb2d998d.

--- process-flow-diagram-component/src/components/Diagram/Diagram.tsx
+++ process-flow-diagram-component/src/components/Diagram/Diagram.tsx
@@ -181,10 +181,11 @@ const Diagram = (props: DiagramProps) => {
       }
 
  {/* // * Only for development result checking */}
-        {/* <ResultsPanel  style={{
+
+        <ResultsPanel  style={{
                 left: isMenuDrawerOpen ? drawerOpenOffsetPx : drawerClosedOffsetPx,
                 transition: 'left 0.25s cubic-bezier(0.4, 0, 0.2, 1)'
-              }}></ResultsPanel> */}
+              }}></ResultsPanel>
       <ReactFlowProvider>
         <div className={'flow-wrapper'} style={{ height: props.height }}>
           <ReactFlow
