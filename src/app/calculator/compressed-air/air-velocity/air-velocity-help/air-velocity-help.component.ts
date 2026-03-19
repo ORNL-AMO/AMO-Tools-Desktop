22dbbff6996a94d4daa389a9fdc57a079007bc3b
Mark Root <rootrm@ornl.gov>
2026-03-19 08:41:58 -0500

    chore: added help text to pipe sizing and air velocity calculators

--- src/app/calculator/compressed-air/air-velocity/air-velocity-help/air-velocity-help.component.ts
+++ src/app/calculator/compressed-air/air-velocity/air-velocity-help/air-velocity-help.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Settings } from '../../../../shared/models/settings';
 
 @Component({
-    selector: 'app-air-velocity-help',
-    templateUrl: './air-velocity-help.component.html',
-    styleUrls: ['./air-velocity-help.component.css'],
-    standalone: false
+  selector: 'app-air-velocity-help',
+  templateUrl: './air-velocity-help.component.html',
+  styleUrls: ['./air-velocity-help.component.css'],
+  standalone: false
 })
 export class AirVelocityHelpComponent implements OnInit {
   @Input()
   currentField: string;
-  
+  @Input()
+  settings: Settings;
+
   constructor() { }
 
   ngOnInit() {
